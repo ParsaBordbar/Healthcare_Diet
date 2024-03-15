f8825ed083488e878da472a67db3189c0dde4bb9
ParsaBordbar <parsab71@gmail.com>
2024-03-15 18:28:59 +0330

    Add Starabeerys compoennt to page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,24 @@ import Hero from "@/components/landingPage_components/Hero";
 import Navbar from "@/components/landingPage_components/Navbar";
 import PlanBox from "@/components/landingPage_components/PlanBox";
 import WhyHealthySection from "@/components/landingPage_components/whyHealthySection";
+import Strawberries from "/public/svg/strawberries.svg"
 
 export default function Home() {
   return (
     <main>
-      <div className="grid grid-cols-12">
-        <div className="col-span-full sticky top-0 shadow-sm z-20">
+        <div className="sticky top-0 shadow-sm z-20">
           <Navbar />
         </div>
-        <div className="col-span-full bg-[var(--primary)] h-5/6">
+        <div className="bg-[var(--primary)] h-5/6">
           <Hero/>
         </div>
-        <div className="col-span-full flex justify-center items-center">
+        <div className="flex justify-center items-center">
           <WhyHealthySection />
         </div>
-        <div className="col-span-full flex justify-end items-center">
-          <PlanBox />
+        <div className="flex items-center my-9">
+        <Strawberries className="z-0"/>
+          <PlanBox/>
         </div>
-      </div>
     </main>
   );
 }
--- src/components/landingPage_components/PlanBox/index.tsx
+++ src/components/landingPage_components/PlanBox/index.tsx
@@ -2,30 +2,29 @@ import Icon1 from "/public/svg/icon1.svg";
 import Icon2 from "/public/svg/icon2.svg";
 import Icon3 from "/public/svg/icon3.svg";
 
-
 const PlanBox = () => {
     return(
-        <div className="w-5/6 bg-[var(--box-bg)] px-40 py-12 rounded-s-3xl border-4">
-            <div className="flex justify-center items-center flex-col ms-32 ">
-                <h3 className="text-xl font-bold">
+        <div className="w-5/6 bg-[var(--box-bg)] flex flex-col items-end pe-52 py-12 rounded-s-3xl shadow-2xl` px-16">
+            <div className="flex justify-center items-center flex-col pe-32">
+                <h3 className="text-2xl font-bold mb-4">
                     برای شما برنامه‌ داریم
                 </h3>
-                <p>
+                <p className="mb-8">
                     عادات سالم‌تری بسازید با برنامه‌های شخصی سازی شده ما      
                 </p>
             </div>
-            <hr />
+            <hr/>
             <ul className="flex flex-row justify-center items-center gap-14 m-3 ">
                 <li className="flex justify-center items-center text-xl">
-                حفظ وزن
+                    <p>حفظ وزن</p>
                     <Icon1 />
                 </li>
                 <li className="flex justify-center items-center text-xl">
-                اضافه کردن وزن
+                    <p>اضافه کردن وزن</p>
                     <Icon2 />
                 </li>
                 <li className="flex justify-center items-center text-xl">
-                کاهش وزن
+                    <p>کاهش وزن</p>
                     <Icon3 />
                 </li>
             </ul>
