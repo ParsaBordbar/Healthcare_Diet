da618fea25bfb66a60d5abea01715c20c17458c2
MohammadMosavat <42.octmst@gmail.com>
2024-03-16 14:56:44 +0330

    Add sections of landing page , qestion box , healthly stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,133 @@ import Hero from "@/components/landingPage_components/Hero";
 import Navbar from "@/components/landingPage_components/Navbar";
 import PlanBox from "@/components/landingPage_components/PlanBox";
 import WhyHealthySection from "@/components/landingPage_components/whyHealthySection";
-import Strawberries from "/public/svg/strawberries.svg"
+import Strawberries from "/public/svg/strawberries.svg";
 import TextBoxSection from "@/components/landingPage_components/TextBoxSection";
+import QuestionMark from "/public/svg/ques mark.svg";
+import MainButton from "@/components/LoginButton";
+import Orange from "/public/svg/oranges.svg";
+import PNGwing9 from "/public/svg/pngwing 9.svg";
+import LiIcon from "/public/svg/liIcon.svg";
+import ListItems from "@/components/ListItems";
+import Tick from "/public/svg/tttick.svg";
+import ProteinWhey from "/public/svg/Rectangle 27.svg";
 
 export default function Home() {
   return (
     <main>
-        <div className="grid grid-cols-1">
-          <div className="sticky top-0 shadow-sm z-20 col-span-full">
-            <Navbar />
-          </div>
-          <div className="bg-[var(--primary)] h-5/6 col-span-full">
-            <Hero/>
-          </div>
-          <div className="flex justify-center items-center col-span-full">
-            <WhyHealthySection />
-          </div>
-          <div className="flex items-center my-9 gap-14 justify-end col-span-full">
-            <Strawberries className="z-0"/>
-            <PlanBox/>
-          </div>
-          <div>
-            <TextBoxSection />
-          </div>  
+      <div className="grid grid-cols-1">
+        <div className="sticky top-0 shadow-sm z-20 col-span-full">
+          <Navbar />
         </div>
+        <div className="bg-[var(--primary)] h-5/6 col-span-full">
+          <Hero />
+        </div>
+        <div className="flex justify-center items-center col-span-full">
+          <WhyHealthySection />
+        </div>
+        <div className="flex items-center my-9 gap-14 justify-end col-span-full">
+          <Strawberries className="z-0" />
+          <PlanBox />
+        </div>
+        <div>
+          <TextBoxSection />
+        </div>
+        <div className="flex w-1/3 shadow-lg drop-shadow-md mx-auto py-9 px-14 rounded-3xl bg-[var(--box-bg)] justify-between">
+          <section className="w-9/12 flex flex-col justify-between">
+            <p className="text-xl font-normal text-right">
+              نمی‌دونید وعده غذایی درست و برنامه غذایی مناسب شما چی هست؟
+            </p>
+            <MainButton
+              standard
+              className="py-4 w-2/3"
+              value={"همین حالا ثبت نام کنید"}
+            />
+          </section>
+          <QuestionMark />
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col w-10/12 gap-24">
+            <h1 className="mx-auto text-3xl">
+              چه چیزی
+              <mark className="bg-transparent text-[var(--text-important)] text-6xl">
+                {" "}
+                گاز سلامتی{" "}
+              </mark>
+              شماست
+            </h1>
+            <section className="flex justify-around w-11/12">
+              <PNGwing9 />
+              <ul className="flex flex-col gap-9 ">
+                <ListItems
+                  icon={LiIcon}
+                  className="gap-6"
+                  desc="نظارت بر رژیم غذایی"
+                />
+                <ListItems
+                  icon={LiIcon}
+                  className="gap-6"
+                  desc="بهترین پیشنهادات تغذیه"
+                />
+                <ListItems
+                  icon={LiIcon}
+                  className="gap-6"
+                  desc="پروتال تمرینات"
+                />
+                <ListItems
+                  icon={LiIcon}
+                  className="gap-6"
+                  desc="برنامه ریز وعده‌های غذایی"
+                />
+                <ListItems
+                  icon={LiIcon}
+                  className="gap-6"
+                  desc="پایگاه داده‌ای از ریسپی‌ها"
+                />
+                <ListItems
+                  className="gap-6"
+                  icon={LiIcon}
+                  desc="خرید یک پایانه‌ای محصولات تعذیه‌ای  "
+                />
+                <ListItems className="gap-6" icon={LiIcon} desc="جامعه" />
+              </ul>
+            </section>
+          </div>
+          <Orange />
+        </div>
+        <div className="w-full py-10 bg-[var(--secondry-color)]">
+          <section className="flex w-1/2 flex-col mx-auto gap-8">
+            <h1 className="text-center text-3xl">
+              <mark className="bg-transparent text-[var(--text-important)] text-6xl">
+                {" "}
+                محصولات{" "}
+              </mark>
+              رژیمی
+            </h1>
+            <ul className="flex self-center flex-col  items-start gap-4">
+              <p>
+                <mark className="bg-transparent font-bold w-full">
+                  گاز سلامتی
+                </mark>{" "}
+                به شما این امکان را می‌دهد که عرضه محصولات رژیمی خود را از همان
+                پلتفرم نیز به راحتی دریافت کنید
+              </p>
+              <ListItems icon={Tick} desc="در یک رنج بزرگ جستجو کنید" />
+              <ListItems
+                icon={Tick}
+                desc="بهترین توصیه را از دکتر تغذیه دریافت کنید  "
+              />
+            </ul>
+          </section>
+          <main>
+            <div className="bg-white rounded-lg px-10 py-4">
+              <ProteinWhey />
+              <section>
+                p
+              </section>
+            </div>
+          </main>
+        </div>
+      </div>
     </main>
   );
 }
