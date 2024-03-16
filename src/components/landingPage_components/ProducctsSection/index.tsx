5b7d9a5c05fc0bce68252695830f15951db1c84b
ParsaBordbar <parsab71@gmail.com>
2024-03-16 19:40:00 +0330

    Add Background Svg to ProductsSection component. Fix Problems with Component, Ready for farther Devlopment.

--- src/components/landingPage_components/ProducctsSection/index.tsx
+++ src/components/landingPage_components/ProducctsSection/index.tsx
@@ -1,41 +1,48 @@
 import ListItems from "@/components/ListItems";
 import Tick from "/public/svg/tttick.svg";
 import ProteinWhey from "/public/svg/Rectangle 27.svg";
+import DietProductsBg from "/public/svg/DiietProductsBg.svg"
 
 const ProductsSection = () => {
     return(
-        <div className="w-full py-10 bg-[var(--secondry-color)]">
-          <section className="flex w-1/2 flex-col mx-auto gap-8">
-            <h1 className="text-center text-3xl">
-              <mark className="bg-transparent text-[var(--text-important)] text-6xl">
-                {" "}
-                محصولات{" "}
-              </mark>
-              رژیمی
-            </h1>
-            <ul className="flex self-center flex-col  items-start gap-4">
-              <p>
-                <mark className="bg-transparent font-bold w-full">
-                  گاز سلامتی
-                </mark>{" "}
-                به شما این امکان را می‌دهد که عرضه محصولات رژیمی خود را از همان
-                پلتفرم نیز به راحتی دریافت کنید
-              </p>
-              <ListItems icon={Tick} desc="در یک رنج بزرگ جستجو کنید" />
-              <ListItems
-                icon={Tick}
-                desc="بهترین توصیه را از دکتر تغذیه دریافت کنید  "
-              />
-            </ul>
-          </section>
-          <main>
-            <div className="bg-white rounded-lg px-10 py-4">
-              <ProteinWhey />
-              <section>
-                p
-              </section>
+        <div className="w-full py-10 flex justify-center">
+            <div className="flex justify-center">
+              <DietProductsBg className="w-full relative z-0"/>
+              <div className="absolute z-10 flex flex-row mt-60">
+                  <section className="flex w-1/2 flex-col mx-auto gap-8">
+                    <h1 className="text-center text-3xl">
+                      <mark className="bg-transparent text-[var(--text-important)] text-6xl">
+                        {" "}
+                        محصولات{" "}
+                      </mark>
+                      رژیمی
+                    </h1>
+                    <ul className="flex self-center flex-col  items-start gap-4">
+                      <p>
+                        <mark className="bg-transparent font-bold w-full">
+                          گاز سلامتی
+                        </mark>{" "}
+                        به شما این امکان را می‌دهد که عرضه محصولات رژیمی خود را از همان
+                        پلتفرم نیز به راحتی دریافت کنید
+                      </p>
+                      <ListItems icon={Tick} desc="در یک رنج بزرگ جستجو کنید" />
+                      <ListItems
+                        icon={Tick}
+                        desc="بهترین توصیه را از دکتر تغذیه دریافت کنید  "
+                      />
+                    </ul>
+                  </section>
+                  <main>
+                    <div className="bg-white rounded-lg px-10 py-4">
+                      <ProteinWhey />
+                      <section>
+                        p
+                      </section>
+                    </div>
+                  </main>
+              </div>
             </div>
-          </main>
+            
         </div>
     )
 };
