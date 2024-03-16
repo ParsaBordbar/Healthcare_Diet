5da07014ff1980282ec84eb46b23bd45563d01ad
MohammadMosavat <42.octmst@gmail.com>
2024-03-16 14:53:31 +0330

    Add a useable component call it MainButton for all of own button

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -1,9 +1,39 @@
-
-const LoginButton = () => {
-    return(
-        <button className="px-5 py-2 border border-[var(--tittle-box)] rounded-lg">
-            ثبت نام
-        </button>
-    )
+"use client";
+import {
+  ButtonHTMLAttributes,
+  FunctionComponent,
+  useCallback,
+  useMemo,
+} from "react";
+type TButton = ButtonHTMLAttributes<HTMLButtonElement> & {
+  value: string;
+  className?: string;
+  iconSrc?: FunctionComponent;
+  standard?: boolean;
+  simple?: boolean;
+  trasparent?: boolean;
 };
-export default LoginButton;
+
+const MainButton = (props: TButton) => {
+  const declearModleOfButton = useMemo(() => {
+    if (props.standard) {
+      return "bg-gradient-to-t to-[var(--hero-btn)] from-[var(--logo-bg)] shadow-lg bg-[var(--hero-btn)]";
+    } else if (props.trasparent) {
+      return " border  border-[var(--tittle-box)]";
+    }
+  }, []);
+  const checkIcon = useCallback(() => {
+    if (props.iconSrc) return <props.iconSrc />;
+  }, []);
+  return (
+    <button
+      className={`${props.className} ${declearModleOfButton} ${
+        props.iconSrc && "flex items-center gap-2"
+      } text-center rounded-lg`}
+    >
+      {checkIcon()}
+      <span className="!text-black font-medium  text-base ">{props.value}</span>
+    </button>
+  );
+};
+export default MainButton;
