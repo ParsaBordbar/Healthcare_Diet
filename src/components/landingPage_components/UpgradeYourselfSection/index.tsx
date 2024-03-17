7cc60c1a77de6723964e577810bf8ff5f689a33e
MohammadMosavat <42.octmst@gmail.com>
2024-03-17 19:37:04 +0330

    change the mode of MainButton to gradient in upgrateYourselfSection

--- src/components/landingPage_components/UpgradeYourselfSection/index.tsx
+++ src/components/landingPage_components/UpgradeYourselfSection/index.tsx
@@ -1,34 +1,33 @@
 import ListItems from "@/components/ListItems";
-import MainButton from "@/components/LoginButton";
+import MainButton from "@/components/MainButton";
 import Tick from "/public/svg/tttick.svg";
 
 const UpgradeYourselfSection = () => {
-    return(
-        <section className="py-12 my-10 flex justify-center items-center flex-col bg-[var(--upgrade-box)] gap-16">
-            <div className="flex flex-col justify-center items-center">
-                <p className="text-3xl">روی خودت سرمایه گذاری کن و</p>
-                <h3 className="text-4xl"> ارتقا بده سلامتی, بدن و اعتماد به نفس‌ت رو</h3>
-            </div>
-            <MainButton
-              standard
-              className="py-4 w-1/6"
-              value={"امروز شروع کنید"}
-            />
-            <ul className="flex gap-52">
-                <ListItems
-                icon={Tick}
-                desc="بهترین توصیه را از دکتر تغذیه دریافت کنید"
-                />
-                <ListItems
-                icon={Tick}
-                desc="بهترین توصیه را از دکتر تغذیه دریافت کنید"
-                />
-                <ListItems
-                icon={Tick}
-                desc="بهترین توصیه را از دکتر تغذیه دریافت کنید"
-                />
-            </ul>
-        </section>
-    )
+  return (
+    <section className="py-12 my-10 flex justify-center items-center flex-col bg-[var(--upgrade-box)] gap-16">
+      <div className="flex flex-col justify-center items-center">
+        <p className="text-3xl">روی خودت سرمایه گذاری کن و</p>
+        <h3 className="text-4xl">
+          {" "}
+          ارتقا بده سلامتی, بدن و اعتماد به نفس‌ت رو
+        </h3>
+      </div>
+      <MainButton gradient className="py-4 w-1/6" value={"امروز شروع کنید"} />
+      <ul className="flex gap-52">
+        <ListItems
+          icon={Tick}
+          desc="بهترین توصیه را از دکتر تغذیه دریافت کنید"
+        />
+        <ListItems
+          icon={Tick}
+          desc="بهترین توصیه را از دکتر تغذیه دریافت کنید"
+        />
+        <ListItems
+          icon={Tick}
+          desc="بهترین توصیه را از دکتر تغذیه دریافت کنید"
+        />
+      </ul>
+    </section>
+  );
 };
 export default UpgradeYourselfSection;
