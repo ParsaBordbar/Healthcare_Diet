0f14436d387429a592e547ed4222ac4cc918be57
MohammadMosavat <42.octmst@gmail.com>
2024-03-17 19:31:29 +0330

    Set slider for food story section its client component and we separete this from main section

---
+++ src/components/landingPage_components/FoodStorySection/slider/index.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { SwiperSlide } from "swiper/react";
+import StoryBox from "../../StoryBox ";
+import { Slider } from "@/components/slider";
+import StoryBoxSvg from "/public/svg/StoryBoxSvg.svg";
+
+const NewSlider = () => {
+  return (
+    <main className="flex gap-10 w-full justify-center">
+      <Slider className="!w-[22%]" anotherProp={1}>
+        <SwiperSlide className="!w-fit !m-0">
+          <StoryBox
+            tittle={"رژیم و فیتنس"}
+            bodyText={
+              "تمام پروتئین مورد نیاز خود را با یک رژیم غذایی گیاهی دریافت کنید"
+            }
+            img={<StoryBoxSvg />}
+          />
+        </SwiperSlide>
+        <SwiperSlide className="!w-fit !m-0">
+          <StoryBox
+            tittle={"رژیم و فیتنس"}
+            bodyText={
+              "تمام پروتئین مورد نیاز خود را با یک رژیم غذایی گیاهی دریافت کنید"
+            }
+            img={<StoryBoxSvg />}
+          />
+        </SwiperSlide>
+        <SwiperSlide className="!w-fit !m-0">
+          <StoryBox
+            tittle={"رژیم و فیتنس"}
+            bodyText={
+              "تمام پروتئین مورد نیاز خود را با یک رژیم غذایی گیاهی دریافت کنید"
+            }
+            img={<StoryBoxSvg />}
+          />
+        </SwiperSlide>
+      </Slider>
+    </main>
+  );
+};
+
+export default NewSlider;
