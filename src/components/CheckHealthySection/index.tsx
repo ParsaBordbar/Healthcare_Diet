edf565ccd88022601c4443a3c465b2e9d751ba6a
MohammadMosavat <42.octmst@gmail.com>
2024-03-17 19:29:47 +0330

    Add check health section and we have slider in this section component

---
+++ src/components/CheckHealthySection/index.tsx
@@ -0,0 +1,12 @@
+import SlideCheckHealthySection from "./slider";
+
+const CheckHealthySection = () => {
+  return (
+    <div className="w-2/3  mx-auto flex flex-col gap-5">
+      <h1 className="font-bold text-3xl">بررسی های گاز سلامتی</h1>
+      <SlideCheckHealthySection />
+    </div>
+  );
+};
+
+export default CheckHealthySection;
