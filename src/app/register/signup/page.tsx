786779860dd2c8f4c6d5b766963e9754e8b460a4
MohammadMosavat <42.octmst@gmail.com>
2024-04-02 19:41:07 +0330

    create signup page and import form signup

--- src/app/register/signup/page.tsx
+++ src/app/register/signup/page.tsx
@@ -1,26 +1,13 @@
 import Link from "next/link";
-import FormLogin from "@/components/FormRegisters/Login/login";
 import Social from "@/components/FormRegisters/Login/social";
+import FormSignUp from "@/components/FormRegisters/Signup/signup";
 
 const SignUpPage = () => {
   return (
     <>
       <div className="absolute flex flex-col items-center gap-5  w-1/3 top-[9.375rem] right-[7.5rem]">
-        <FormLogin />
-        <section className="flex gap-5 w-full flex-col items-center">
-          <p className="text-xl text-[var(--text)]">ورود با</p>
-          <Social/>
-          <Link
-            href={"/register/signup"}
-            className="text-xl mt-5 text-[var(--text)]"
-          >
-            آیا ثبت نام نکرده اید؟
-            <mark className="bg-transparent text-[var(--rating-color)]">
-              {" "}
-              ثبت نام
-            </mark>
-          </Link>
-        </section>
+        <FormSignUp />
+        <Link href={"/register/login"} className="text-[var(--text)] text-xl">آیا حساب دارید؟ ثبت نام</Link>
       </div>
     </>
   );
