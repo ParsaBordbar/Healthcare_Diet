323344a9fa35eaa4ffe072db505f6b3208a268b6
MohammadMosavat <42.octmst@gmail.com>
2024-04-02 20:15:35 +0330

    fix the text of navigate to login page from signup page

--- src/app/register/signup/page.tsx
+++ src/app/register/signup/page.tsx
@@ -7,7 +7,7 @@ const SignUpPage = () => {
     <>
       <div className="absolute flex flex-col items-center gap-5  w-1/3 top-[9.375rem] right-[7.5rem]">
         <FormSignUp />
-        <Link href={"/register/login"} className="text-[var(--text)] text-xl">آیا حساب دارید؟ ثبت نام</Link>
+        <Link href={"/register/login"} className="text-[var(--text)] text-xl">آیا حساب دارید؟ ورود</Link>
       </div>
     </>
   );
