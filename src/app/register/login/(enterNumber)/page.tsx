abacac01561b22cc8ad8d5625fc86a0c7d573723
MohammadMosavat <42.octmst@gmail.com>
2024-04-03 16:04:32 +0330

    remove the social buttons in login page

--- src/app/register/login/(enterNumber)/page.tsx
+++ src/app/register/login/(enterNumber)/page.tsx
@@ -8,8 +8,6 @@ const LoginPage = () => {
       <div className="absolute  lg:!shadow-none lg:p-0 py-5 px-5 lg:!bg-transparent flex flex-col items-center gap-5 w-4/5 lg:w-1/3  lg:right-[7.5rem]">
         <FormLogin />
         <section className="flex gap-5 w-full flex-col items-center">
-          <p className="text-xl text-[var(--text)]">ورود با</p>
-          <Social />
           <Link
             href={"/register/signup"}
             className="text-xl mt-5 text-[var(--text)]"
