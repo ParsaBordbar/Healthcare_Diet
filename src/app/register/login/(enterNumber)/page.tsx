ffa1e83b6e985fd0f7beca5f3e0eddcac5dd8d75
MohammadMosavat <42.octmst@gmail.com>
2024-04-03 15:47:30 +0330

    responsive the form of login or enterNumber section of login page

--- src/app/register/login/(enterNumber)/page.tsx
+++ src/app/register/login/(enterNumber)/page.tsx
@@ -5,11 +5,11 @@ import Social from "@/components/FormRegisters/Login/social";
 const LoginPage = () => {
   return (
     <>
-      <div className="absolute flex flex-col items-center gap-5  w-1/3  right-[7.5rem]">
+      <div className="absolute  lg:!shadow-none lg:p-0 py-5 px-5 lg:!bg-transparent flex flex-col items-center gap-5 w-4/5 lg:w-1/3  lg:right-[7.5rem]">
         <FormLogin />
         <section className="flex gap-5 w-full flex-col items-center">
           <p className="text-xl text-[var(--text)]">ورود با</p>
-          <Social/>
+          <Social />
           <Link
             href={"/register/signup"}
             className="text-xl mt-5 text-[var(--text)]"
