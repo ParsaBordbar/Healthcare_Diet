728152738a7b5ff0d332a0c7102de0a3e286c369
MohammadMosavat <42.octmst@gmail.com>
2024-04-03 15:59:31 +0330

    responsive the signup page and set the parent of this form in center of the whole page

--- src/app/register/signup/page.tsx
+++ src/app/register/signup/page.tsx
@@ -5,10 +5,13 @@ import FormSignUp from "@/components/FormRegisters/Signup/signup";
 const SignUpPage = () => {
   return (
     <>
-      <div className="absolute flex flex-col items-center gap-5  w-1/3 top-[9.375rem] right-[7.5rem]">
+      <div className="absolute flex flex-col justify-center lg:justify-end items-center gap-5 w-4/5 lg:w-1/3 top-[9.375rem] lg:right-[7.5rem]">
         <FormSignUp />
         <Link href={"/register/login"} className="text-[var(--text)] text-xl">
-          آیا حساب دارید؟ <mark className="bg-transparent text-[var(--rating-color)]">ورود</mark>
+          آیا حساب دارید؟{" "}
+          <mark className="bg-transparent text-[var(--rating-color)]">
+            ورود
+          </mark>
         </Link>
       </div>
     </>
