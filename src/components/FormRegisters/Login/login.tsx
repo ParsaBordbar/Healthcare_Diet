7a0424662a60762631ad7c088d5d132e4eee9189
MohammadMosavat <42.octmst@gmail.com>
2024-04-03 13:48:27 +0330

    use useCallBack for renter the type in login form

--- src/components/FormRegisters/Login/login.tsx
+++ src/components/FormRegisters/Login/login.tsx
@@ -3,42 +3,48 @@ import { LoginFormType } from "@/types";
 import MainButton from "../../MainButton";
 import MainInput from "../../MainInput";
 import Email from "/public/svgs/Email.svg";
+import { useCallback } from "react";
 
-const FormLogin = ({type}:LoginFormType) => {
-  if(type == 'getCode'){
-    return(
-      <form action="" className="flex w-full gap-10 flex-col justify-center">
-      <h1 className="text-center font-bold text-2xl">ورود</h1>
-      <section className="flex w-full flex-col items-center gap-[3.125rem]">
-        <MainInput
-          label="کد را وارد کنید"
-          iconFirst={Email}
-          placeholder="کد دریافتی را وراد کنید"
-        />
-        <MainButton
-          simple
-          className="bg-[var(--rating-color)] py-2.5 !text-xl !text-white w-full"
-          value={"ورود"}
-        />
-      </section>
-    </form>
-    )
-  }
+const FormLogin = ({ type }: LoginFormType) => {
+  const CheckTypeRender = useCallback(() => {
+    if (type == "getCode") {
+      return (
+        <>
+          <MainInput
+            label="کد را وارد کنید"
+            iconFirst={Email}
+            placeholder="کد دریافتی را وراد کنید"
+          />
+          <MainButton
+            simple
+            className="bg-[var(--rating-color)] py-2.5 !text-xl !text-white w-full"
+            value={"ورود"}
+          />
+        </>
+      );
+    } else {
+      return (
+        <>
+          <MainInput
+            label="لطفا شماره تماس خود را وارد کنید:"
+            iconFirst={Email}
+            placeholder="abc@gmail.com"
+          />
+          <MainButton
+            simple
+            className="bg-[var(--rating-color)] py-2.5 !text-xl !text-white w-full"
+            value={"ارسال کد ورود"}
+            url="./enterTheCode"
+          />
+        </>
+      );
+    }
+  }, []);
   return (
     <form action="" className="flex w-full gap-10 flex-col justify-center">
       <h1 className="text-center font-bold text-2xl">ورود</h1>
       <section className="flex w-full flex-col items-center gap-[3.125rem]">
-        <MainInput
-          label="لطفا شماره تماس خود را وارد کنید:"
-          iconFirst={Email}
-          placeholder="abc@gmail.com"
-        />
-        <MainButton
-          simple
-          className="bg-[var(--rating-color)] py-2.5 !text-xl !text-white w-full"
-          value={"ارسال کد ورود"}
-          url="./enterTheCode"
-        />
+        {CheckTypeRender()}
       </section>
     </form>
   );
