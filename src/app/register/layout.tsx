45cfb5990710145deea5c00b3cf409238588287c
MohammadMosavat <42.octmst@gmail.com>
2024-04-04 14:43:00 +0330

    set style for banner of header of login and signup page bg-cover

--- src/app/register/layout.tsx
+++ src/app/register/layout.tsx
@@ -9,7 +9,7 @@ const LayoutRegister = ({ children }: { children: ReactNode }) => {
   console.log(children);
   return (
     <div className="relative top-0 z-40">
-      <header className="absolute bg-no-repeat w-full h-28 bg-[url('/svgs/header-register.svg')] top-0 ">
+      <header className="absolute bg-no-repeat bg-cover w-full h-28 bg-[url('/svgs/header-register.svg')] top-0 ">
         {/* <HeaderSVG className="header opacity-100" width={1920} /> */}
         <nav className="flex flex-row-reverse left-0 py-7 absolute top-0 px-10 gap-16 items-center">
           <Logo />
