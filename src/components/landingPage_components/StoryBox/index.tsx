41d4cd82765783560e5d323fd49542d736a49c69
MohammadMosavat <42.octmst@gmail.com>
2024-04-08 21:16:59 +0330

    pass id and add img tag insted a component in story box

--- src/components/landingPage_components/StoryBox/index.tsx
+++ src/components/landingPage_components/StoryBox/index.tsx
@@ -2,13 +2,14 @@ import { TextBoxType } from "@/types";
 import Link from "next/link";
 import Arrow from "/public/svg/Arrow 1.svg";
 
-const StoryBox = ({ tittle, bodyText, img }: TextBoxType) => {
+const StoryBox = ({ title, bodyText, img  , id}: TextBoxType) => {
   return (
     <div className="flex bg-[var(--box-bg)] flex-col rounded-2xl justify-center items-center w-[394px]">
-      <div className="w-fit">{img}</div>
+      {/* <div className="w-fit">{img}</div> */}
+      <img src={img} alt="" />
       <div className="flex justify-center items-start flex-col m-2 p-4 border-b-2">
         <h3 className="text-2xl text-[var(--tittle-box)] font-semibold">
-          {tittle}
+          {title + id}
         </h3>
         <p className="text-xl ">{bodyText}</p>
       </div>
