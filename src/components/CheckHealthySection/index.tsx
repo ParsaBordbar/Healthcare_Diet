fdfd8444ec39975fd9a69989904c71db616cc421
ParsaBordbar <parsab71@gmail.com>
2024-04-08 19:06:02 +0330

    Add responsivness to CheckHealthySection compnent for mobile size.

--- src/components/CheckHealthySection/index.tsx
+++ src/components/CheckHealthySection/index.tsx
@@ -3,7 +3,7 @@ import SlideCheckHealthySection from "./slider";
 const CheckHealthySection = () => {
   return (
     <div className="w-2/3  mx-auto flex flex-col gap-5">
-      <h1 className="font-bold text-3xl">بررسی های گاز سلامتی</h1>
+      <h1 className="font-bold text-3xl md:text-right text-center ">بررسی های گاز سلامتی</h1>
       <SlideCheckHealthySection />
     </div>
   );
