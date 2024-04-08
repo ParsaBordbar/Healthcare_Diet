c120e39acbb476645121f578c9a41079c3d5d2f1
ParsaBordbar <parsab71@gmail.com>
2024-04-08 17:26:15 +0330

    Add Resoponsiveness for ListItems Component.

--- src/components/ListItems/index.tsx
+++ src/components/ListItems/index.tsx
@@ -9,7 +9,9 @@ type TListItems = {
 const ListItems = (props: TListItems) => {
   return (
     <li className={`${props.className} lg:text-xl flex md:text-2xl text-base`}>
-      {props.icon && <props.icon />}
+      <div className="scale-90 lg:scale-100">
+        {props.icon && <props.icon />}
+      </div>
       <p>{props.desc}</p>
     </li>
   );
