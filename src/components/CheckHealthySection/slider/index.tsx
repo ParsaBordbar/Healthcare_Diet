284fce44141788d9a714f56153b32b958bcfe33d
MohammadMosavat <42.octmst@gmail.com>
2024-04-08 21:21:41 +0330

    add slider for checkHealthSection and mapping the data check in data.js

--- src/components/CheckHealthySection/slider/index.tsx
+++ src/components/CheckHealthySection/slider/index.tsx
@@ -1,52 +1,27 @@
 "use client";
-import CheckHealthBox from "@/components/CheckHealthBox";
+import CheckHealthBox, { TCheckHealth } from "@/components/CheckHealthBox";
 import { Slider } from "@/components/slider";
 import { SwiperSlide } from "swiper/react";
 import CheckHealth from "/public/svg/Rectangle 17.svg";
+import { DataCheck } from "@/data";
 
 const SlideCheckHealthySection = () => {
   return (
     <main className="flex gap-10 w-full justify-center">
-      <Slider className="!w-full" anotherProp={1}>
-        <SwiperSlide className="!w-fit !m-0">
-          <CheckHealthBox
-            img={CheckHealth}
-            title="10 کیلوگرم در 3 هفته"
-            desc=" این پلتفرم مانند بهترین اتفاقی است که برای سلامتی من افتاده
-                است. من شوکه شده بودم که چگونه هوس هایم بعد از چند روز از بین
-                رفت و نخواستن بین وعده های غذایی واقعاً کمکم کرد. از دست دادن
-                6 کیلوگرم تنها در 3 هفته فوق العاده است، اما بهترین بخش بهبود
-                سلامتی است."
-            name="Anshuman Khuranna"
-            datePlan="برنامه غذایی 3 هفته ای کاهش وزن"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="!w-fit !m-0">
-          <CheckHealthBox
-            img={CheckHealth}
-            title="10 کیلوگرم در 3 هفته"
-            desc=" این پلتفرم مانند بهترین اتفاقی است که برای سلامتی من افتاده
-                است. من شوکه شده بودم که چگونه هوس هایم بعد از چند روز از بین
-                رفت و نخواستن بین وعده های غذایی واقعاً کمکم کرد. از دست دادن
-                6 کیلوگرم تنها در 3 هفته فوق العاده است، اما بهترین بخش بهبود
-                سلامتی است."
-            name="Anshuman Khuranna"
-            datePlan="برنامه غذایی 3 هفته ای کاهش وزن"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="!w-fit !m-0">
-          <CheckHealthBox
-            img={CheckHealth}
-            title="10 کیلوگرم در 3 هفته"
-            desc=" این پلتفرم مانند بهترین اتفاقی است که برای سلامتی من افتاده
-                است. من شوکه شده بودم که چگونه هوس هایم بعد از چند روز از بین
-                رفت و نخواستن بین وعده های غذایی واقعاً کمکم کرد. از دست دادن
-                6 کیلوگرم تنها در 3 هفته فوق العاده است، اما بهترین بخش بهبود
-                سلامتی است."
-            name="Anshuman Khuranna"
-            datePlan="برنامه غذایی 3 هفته ای کاهش وزن"
-          />
-        </SwiperSlide>
+      <Slider className="!w-full py-10" anotherProp={1}>
+        {DataCheck.map((items: TCheckHealth) => {
+          return (
+            <SwiperSlide className="!w-fit !m-0" {...items}>
+              <CheckHealthBox
+                img={items.img}
+                title={items.title}
+                desc={items.desc}
+                datePlan={items.datePlan}
+                name={items.name}
+              />
+            </SwiperSlide>
+          );
+        })}
       </Slider>
     </main>
   );
