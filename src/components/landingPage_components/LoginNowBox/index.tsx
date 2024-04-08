e931550a3908d8d453ad89eb4ccc20a7881965e0
ParsaBordbar <parsab71@gmail.com>
2024-04-08 17:19:29 +0330

    Add Responsiveness for LoginNowBox Component.

--- src/components/landingPage_components/LoginNowBox/index.tsx
+++ src/components/landingPage_components/LoginNowBox/index.tsx
@@ -5,16 +5,16 @@ const LoginNowBox = () => {
     return(
         <div className="flex xl:1/3 lg:w-2/3 shadow-lg drop-shadow-md py-9 px-14 xl:w-2/5 lg:rounded-3xl bg-[var(--box-bg)] w-full justify-center">
           <section className="w-9/12 flex flex-col justify-between pt-8">
-            <p className="lg:text-xl font-normal text-right text-xl">
+            <p className="lg:text-xl font-normal text-right md:text-xl text-base">
               نمی‌دونید وعده غذایی درست و برنامه غذایی مناسب شما چی هست؟
             </p>
             <MainButton
               standard
-              className="py-4 lg:w-2/3 w-1/3 mt-6"
+              className="py-4 lg:w-2/3 md:w-1/3 w-3/4 mt-6"
               value={"همین حالا ثبت نام کنید"}
             />
           </section>
-          <QuestionMark className="scale-90 lg:scale-100"/>
+          <QuestionMark className="scale-75 md:scale-90 lg:scale-100"/>
         </div>
     )
 };
