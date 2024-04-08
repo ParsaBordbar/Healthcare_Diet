fa0e5c5906d09844dc22013222b83a677f94a738
ParsaBordbar <parsab71@gmail.com>
2024-04-08 16:17:27 +0330

    Add Responsiveness for PlanBox component, for mobile sizes.

--- src/components/landingPage_components/PlanBox/index.tsx
+++ src/components/landingPage_components/PlanBox/index.tsx
@@ -4,8 +4,8 @@ import Icon3 from "/public/svg/icon3.svg";
 
 const PlanBox = () => {
     return(
-        <div className="w-full bg-[var(--box-bg)] flex flex-col items-end xl:pe-52 lg:py-12 rounded-s-3xl shadow-2xl lg:pe-28 py-16">
-            <div className="flex justify-center items-center flex-col xl:pe-52 pe-11">
+        <div className="w-full bg-[var(--box-bg)] flex flex-col md:items-end xl:pe-52 lg:py-12 md:rounded-s-3xl shadow-2xl lg:pe-28 py-16 gap-2">
+            <div className="flex justify-center items-center flex-col xl:pe-52 md:pe-11">
                 <h3 className="lg:text-2xl font-bold lg:mb-4 text-lg">
                     برای شما برنامه‌ داریم
                 </h3>
@@ -14,18 +14,18 @@ const PlanBox = () => {
                 </p>
             </div>
             <hr/>
-            <ul className="flex flex-row justify-center items-center xl:gap-32 m-3 gap-1">
-                <li className="flex justify-center items-center lg:text-xl text-base">
+            <ul className="flex flex-row justify-center items-center xl:gap-32 m-3 md:gap-1">
+                <li className="flex justify-center items-center lg:text-xl md:text-base text-xs">
                     <p>حفظ وزن</p>
-                    <Icon1 className="scale-75"/>
+                    <Icon1 className="md:scale-75 scale-50"/>
                 </li>
-                <li className="flex justify-center items-center lg:text-xl text-base">
+                <li className="flex justify-center items-center lg:text-xl md:text-base text-xs">
                     <p>اضافه کردن وزن</p>
-                    <Icon2 className="scale-75"/>
+                    <Icon2 className="md:scale-75 scale-50"/>
                 </li>
-                <li className="flex justify-center items-center lg:text-xl text-base">
+                <li className="flex justify-center items-center lg:text-xl md:text-base text-xs">
                     <p>کاهش وزن</p>
-                    <Icon3 className="scale-75"/>
+                    <Icon3 className="md:scale-75 scale-50"/>
                 </li>
             </ul>
         </div>
