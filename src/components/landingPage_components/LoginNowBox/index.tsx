381a04b68e4477acb6fdf212881b97be157dc4f6
ParsaBordbar <parsab71@gmail.com>
2024-04-10 11:49:38 +0330

    Add attributes for better Responsiveness for smaller sizes to LoginNowBox Component.

--- src/components/landingPage_components/LoginNowBox/index.tsx
+++ src/components/landingPage_components/LoginNowBox/index.tsx
@@ -5,7 +5,7 @@ const LoginNowBox = () => {
     return(
         <div className="flex xl:1/3 lg:w-2/3 shadow-lg drop-shadow-md py-9 px-14 xl:w-2/5 lg:rounded-3xl bg-[var(--box-bg)] w-full justify-center">
           <section className="w-9/12 flex flex-col justify-between pt-8">
-            <p className="lg:text-xl font-normal text-right md:text-xl sm:text-base text-sm">
+            <p className="lg:text-xl font-normal sm:text-right md:text-xl sm:text-base text-sm">
               نمی‌دونید وعده غذایی درست و برنامه غذایی مناسب شما چی هست؟
             </p>
             <MainButton
