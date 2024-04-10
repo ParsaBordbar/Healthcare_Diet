8dddab4ac8ac66d06ca9cd088ba18225a69bb0f5
ParsaBordbar <parsab71@gmail.com>
2024-04-10 11:23:53 +0330

    Add Responsiveness for UpgradeYourselfSection  for small size mobiles.

--- src/components/landingPage_components/UpgradeYourselfSection/index.tsx
+++ src/components/landingPage_components/UpgradeYourselfSection/index.tsx
@@ -6,14 +6,14 @@ const UpgradeYourselfSection = () => {
   return (
     <section className="py-12 my-10 flex justify-center items-center flex-col bg-[var(--upgrade-box)] gap-16">
       <div className="flex flex-col justify-center items-center px-10 md:px-0">
-        <p className="md:text-3xl text-xl">روی خودت سرمایه گذاری کن و</p>
-        <h3 className="md:text-4xl text-2xl text-center md:text-right">
+        <p className="md:text-3xl sm:text-xl text-base">روی خودت سرمایه گذاری کن و</p>
+        <h3 className="md:text-4xl sm:text-2xl text-xl text-center md:text-right">
           {" "}
           ارتقا بده سلامتی, بدن و اعتماد به نفس‌ت رو
         </h3>
       </div>
       <MainButton gradient className="py-4 md:w-1/6 w-2/5" value={"امروز شروع کنید"} />
-      <ul className="flex xl:gap-52 gap-11 flex-col lg:flex-row">
+      <ul className="flex xl:gap-52 gap-11 flex-col lg:flex-row p-8 sm:p-0">
         <ListItems
           icon={Tick}
           desc="بهترین توصیه را از دکتر تغذیه دریافت کنید"
