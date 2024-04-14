4ca1d156a3300aeebabef874474a29172f6353be
ParsaBordbar <parsab71@gmail.com>
2024-04-14 18:43:32 +0330

    Fix Buggs for running on server.

--- src/components/landingPage_components/TextBox/index.tsx
+++ src/components/landingPage_components/TextBox/index.tsx
@@ -1,7 +1,7 @@
 import { TextBoxType } from "@/types";
 import TheUnderLineThing from '/public/svg/TheLine.svg'
 
-const TextBox = ({tittle, bodyText, img}:TextBoxType) => {
+const TextBox = ({title, bodyText, img}:TextBoxType) => {
     return(
         <div className="flex flex-col justify-center items-center gap-4">
             <div className="flex flex-row justify-center items-center gap-6">
