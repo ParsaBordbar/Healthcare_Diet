fced7e079b103fe7cfd7233b9cf96e25e1b407ea
MohammadMosavat <42.octmst@gmail.com>
2024-04-15 13:24:07 +0330

    responsive the product box in mobile size and tablet

--- src/components/ProductBox/index.tsx
+++ src/components/ProductBox/index.tsx
@@ -11,13 +11,13 @@ export type TProduct = {
 
 const ProductBox = (props: TProduct) => {
   return (
-    <div className="bg-white shadow-xl drop-shadow-lg flex md:justify-between justify-center rounded-3xl xl:px-10 py-4 sm:scale-75 md:scale-100 w-fit">
-      <ProteinWhey className="md:w-1/2 sm:w-1/4 w-1/5"/>
+    <div className="bg-white shadow-xl drop-shadow-lg flex flex-col md:flex-row md:justify-between justify-center rounded-3xl xl:px-10 py-4 scale-100 w-fit">
+      <ProteinWhey className="md:w-1/2 sm:w-3/4 w-3/4"/>
       {/* <props.img /> */}
-      <div className="flex justify-between items-end flex-col py-[2.125rem] xl:pr-12 pr-4 border-r  border-[var(--border-color)]">
+      <div className="flex justify-between items-end flex-col py-[2.125rem] xl:pr-12  p-4 border-r px-8 xl:pl-0 border-[var(--border-color)]">
         <section className="flex xl:gap-4 flex-col justify-start">
-          <h1 className="xl:text-2xl">{props.title}</h1>
-          <p className="xl:text-xl">{props.desc}</p>
+          <h1 className="text-2xl">{props.title}</h1>
+          <p className="text-xl">{props.desc}</p>
         </section>
         {/* <Rating/> */}
       </div>
