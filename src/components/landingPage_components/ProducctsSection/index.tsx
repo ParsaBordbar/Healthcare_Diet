380d5288c9be40485ebde7a2e7952a47b5edb206
ParsaBordbar <parsab71@gmail.com>
2024-04-20 16:20:38 +0330

    Fix Slider in Mobile sizes.

--- src/components/landingPage_components/ProducctsSection/index.tsx
+++ src/components/landingPage_components/ProducctsSection/index.tsx
@@ -9,7 +9,7 @@ const ProductsSection = () => {
     <div className="w-full py-10 flex justify-center items-center">
       <div className="flex w-full justify-center">
         <DietProductsBg className="lg:w-full lg:relative lg:z-0 lg:flex hidden" />
-        <div className="lg:absolute w-full gap-10 z-10 flex flex-col mt-60 bg-[var(--primary)] py-10">
+        <div className="lg:absolute w-full gap-10 z-10 flex flex-col sm:mt-52 bg-[var(--primary)] py-10">
           <section className="flex w-[-webkit-fill-available] flex-col mx-auto gap-8 justify-center items-center">
             <h1 className="text-center text-3xl sm:text-6xl">
               <mark className="bg-transparent text-[var(--text-important)] sm:text-6xl text-2xl">
--- src/components/landingPage_components/ProducctsSection/slider/index.tsx
+++ src/components/landingPage_components/ProducctsSection/slider/index.tsx
@@ -1,17 +1,11 @@
 "use client";
-import ListItems from "@/components/ListItems";
-import Tick from "/public/svg/tttick.svg";
-import DietProductsBg from "/public/svg/DiietProductsBg.svg";
 import ProductBox, { TProduct } from "@/components/ProductBox";
-import ProteinWhey from "/public/svg/Rectangle 27.svg";
-import LineSvg from "/public/svg/NutLiine.svg";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Slider } from "@/components/slider";
 import { DataProduct } from "@/data";
-import { useEffect, useState } from "react";
 
 const NewSlider = () => {
   return (
