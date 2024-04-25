96c62023d61d6dfb791624f7e78913b916584cfa
MohammadMosavat <42.octmst@gmail.com>
2024-04-25 15:46:26 +0330

    Add register diet page and use BMI form in it and have layout and its responsive

---
+++ client/src/app/register/layout.tsx
@@ -0,0 +1,34 @@
+// "use client";
+import { ReactNode } from "react";
+import Logo from "/public/svg/Logo.svg";
+import ListItems from "@/components/ListItems";
+
+const LayoutRegister = ({ children }: { children: ReactNode }) => {
+  console.log(children);
+  return (
+    <div className="relative top-0 z-40">
+      <header className="absolute bg-no-repeat bg-cover w-full h-28 bg-[url('/svg/header-register.svg')] top-0 ">
+        {/* <HeaderSVG className="header opacity-100" width={1920} /> */}
+        <nav className="flex flex-row-reverse left-0 py-7 absolute top-0 px-10 gap-16 items-center">
+          <Logo />
+          <ListItems
+            desc="خدمات"
+            className="cursor-pointer !text-[var(--text)]"
+          />
+          <ListItems
+            desc="بلاگ ها"
+            className="cursor-pointer !text-[var(--text)]"
+          />
+        </nav>
+      </header>
+      <main className="flex !bg-cover md:!bg-contain bg-no-repeat !bg-none lg:!bg-[url('/svg/loginBanner.svg')] h-screen items-center w-full justify-center lg:justify-end">
+        {/* <div className="absolute -z-20 left-0  h-screen bottom-0 top-0">
+          <LoginBanner className="bg-repeat-y" />
+        </div> */}
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default LayoutRegister;
