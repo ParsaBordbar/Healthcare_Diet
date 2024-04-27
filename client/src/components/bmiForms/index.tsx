ef821df7b08a441c733a79eee18b19b8e43ec633
MohammadMosavat <42.octmst@gmail.com>
2024-04-27 19:06:21 +0330

    use bmiForm component as client component

--- client/src/components/bmiForms/index.tsx
+++ client/src/components/bmiForms/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import MainButton from "../MainButton";
 import MainInput from "../MainInput";
 
@@ -7,34 +8,46 @@ const BmiFormDiet = () => {
       <h1 className="text-center font-bold text-2xl">
         اطلاعات خود را وارد کنید
       </h1>
-      <section className="grid w-full grid-cols-2 gap-8">
+      <section className="grid w-full grid-cols-4 gap-8">
         <MainInput
-          parentClassName="col-span-2 md:col-span-1 lg:col-span-2"
+          parentClassName="col-span-4 md:col-span-2 lg:col-span-4"
           label="نام"
           placeholder="نام خود را وارد کنید"
         />
         <MainInput
-          parentClassName="col-span-2 md:col-span-1 lg:col-span-2"
+          parentClassName="col-span-4 md:col-span-2 lg:col-span-4"
           label="نام خانوادگی"
           placeholder="نام خانوادگی خود را وارد کنید"
         />
         <MainInput
-          parentClassName="lg:col-span-2"
+          parentClassName="col-span-2 lg:col-span-4"
           label="شماره تماس"
           placeholder="شماره تماس خود را وارد کنید"
         />
         <MainInput
-          parentClassName="lg:col-span-2"
+          parentClassName="col-span-2 lg:col-span-2"
           label="جنسیت"
           placeholder="جنسیت خود را وارد کنید"
         />
-        <MainInput label="وزن" placeholder="وزن خود را وارد کنید" />
-        <MainInput label="قد" placeholder="قد خود را وارد کنید" />
+        <MainInput
+          label="وزن"
+          parentClassName="col-span-2"
+          placeholder="وزن خود را وارد کنید"
+        />
+        <MainInput
+          label="قد"
+          parentClassName="col-span-2"
+          placeholder="قد خود را وارد کنید"
+        />
+        <MainInput
+          label="سن"
+          parentClassName="col-span-2"
+          placeholder="سن خود را وارد کنید"
+        />
+      <MainButton gradient className="w-full h-fit self-end py-3 col-span-2 lg:col-span-4 !text-white" value={"ورود"} />
       </section>
-      <MainButton gradient className="w-full py-3 !text-white" value={"ورود"} />
     </form>
   );
 };
 
-
-export default BmiFormDiet
+export default BmiFormDiet;
