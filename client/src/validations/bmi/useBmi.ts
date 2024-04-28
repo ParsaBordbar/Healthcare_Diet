23428737bd8cbe6e24a875fb5c6d4fef1896b986
MohammadMosavat <42.octmst@gmail.com>
2024-04-28 19:32:22 +0330

    add two object for schema of bmi form in validation

--- client/src/validations/bmi/useBmi.ts
+++ client/src/validations/bmi/useBmi.ts
@@ -8,7 +8,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { useRouter } from "next/navigation";
 import { BmiDataType } from "@/types";
 
-export const loginSchema = yup.object({
+export const BmiSchema = yup.object({
   firstName: yup
     .string()
     .min(6, "نام کاربری باید حداقل ۴ حرف داشته باشد")
@@ -18,6 +18,23 @@ export const loginSchema = yup.object({
       "نام کاربری باید با حروف شروع شود و شامل حروف و اعداد می‌شود"
     )
     .required("نام کاربری الزامی است"),
+    lastName: yup
+    .string()
+    .min(6, "نام کاربری باید حداقل ۴ حرف داشته باشد")
+    .max(16, "نام کاربری بیشتر از ۱۶ حرف نمی‌تواند باشد")
+    .matches(
+      /^[a-zA-Z][a-zA-Z0-9]/,
+      "نام کاربری باید با حروف شروع شود و شامل حروف و اعداد می‌شود"
+    )
+    .required("نام کاربری الزامی است"),
+    phoneNumber: yup
+    .string()
+    .length(11, "شماره تماس باید ۱۱ عدد داشته باشد")
+    .matches(
+      /(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/gi,
+      "شماره تماس را به درستی وارد کنید"
+    )
+    .required("شماره تماس الزامی است"),
 
 });
 const useLogin = () => {
@@ -27,7 +44,7 @@ const useLogin = () => {
     register,
     formState: { errors },
   } = useForm<BmiDataType>({
-    resolver: yupResolver(loginSchema),
+    resolver: yupResolver(BmiSchema),
   });
 
   if (errors.firstName) {
