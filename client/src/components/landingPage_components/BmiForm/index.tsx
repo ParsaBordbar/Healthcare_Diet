cad08935e1e1cb2f47cb98f52c486e7b7a799e51
ParsaBordbar <parsab71@gmail.com>
2024-04-28 11:50:45 +0330

    Add Missing Field for age.

--- client/src/components/landingPage_components/BmiForm/index.tsx
+++ client/src/components/landingPage_components/BmiForm/index.tsx
@@ -3,7 +3,7 @@ import MainInput from "@/components/MainInput";
 
 const BmiForm = () => {
     return(
-        <div className="flex justify-center items-center flex-col bg-[var(--primary)] rounded-2xl w-4/5">
+        <div className="flex justify-center items-center flex-col bg-[var(--primary)] rounded-2xl w-4/5 my-12">
             <h3 className="text-3xl my-7">همین حالا شروع کنید!</h3>
             <form action="submit" className="flex justify-center items-center flex-col">
                 <div className="flex justify-center items-center flex-row gap-14 w-full">
@@ -22,29 +22,31 @@ const BmiForm = () => {
                         />
                         </li>
                         <li>
-                        <MainInput
-                        parentClassName="lg:col-span-2"
-                        label="شماره تماس"
-                        placeholder="شماره تماس خود را وارد کنید"
-                        />
-                        </li>
-                    </ul>
-                    <ul className="flex flex-col gap-2 md:w-96">
-                        <li>
                             <MainInput
                             parentClassName="lg:col-span-2"
                             label="جنسیت"
                             placeholder="جنسیت خود را وارد کنید"
                             />
+                        
                         </li>
+                    </ul>
+                    <ul className="flex flex-col gap-2 xl:w-96">
                         <li>
                         <MainInput label="وزن" placeholder="وزن خود را وارد کنید" />
                         </li>
                         <li>
                             <MainInput label="قد" placeholder="قد خود را وارد کنید" />
                         </li>
+                        <li>
+                            <MainInput label="سن" placeholder="قد خود را وارد کنید" />
+                        </li>
                     </ul>
                 </div>
+                <MainInput
+                        parentClassName="lg:col-span-2 py-3"
+                        label="شماره تماس"
+                        placeholder="شماره تماس خود را وارد کنید"
+                        /> 
                 <MainButton className="w-3/5 my-10 py-3 !text-white bg-[var(--rating-color)] font-bold text-2xl" value={"امروز شروع کنید"} />
             </form>
         </div>
