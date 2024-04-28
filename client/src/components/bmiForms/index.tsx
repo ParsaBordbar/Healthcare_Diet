0f073ca37e58d0820fd922c81586de6511bef88e
MohammadMosavat <42.octmst@gmail.com>
2024-04-28 19:31:12 +0330

    use select tag for selecting gender in bmi forms

--- client/src/components/bmiForms/index.tsx
+++ client/src/components/bmiForms/index.tsx
@@ -24,11 +24,21 @@ const BmiFormDiet = () => {
           label="شماره تماس"
           placeholder="شماره تماس خود را وارد کنید"
         />
-        <MainInput
-          parentClassName="col-span-2 lg:col-span-2"
-          label="جنسیت"
-          placeholder="جنسیت خود را وارد کنید"
-        />
+        <section className="col-span-2 flex flex-col gap-1">
+          <label htmlFor="gender" className="block  text-lg font-medium ">
+            جنسیت
+          </label>
+          <select
+            id="gender"
+            className="rounded-lg w-full outline-none   py-3 px-4 border border-[var(--border-color)] flex items-center gap-1  bg-white "
+          >
+            <option value="gender" selected>
+              جنسیت
+            </option>
+            <option value="male">مرد</option>
+            <option value="female">زن</option>
+          </select>
+        </section>
         <MainInput
           label="وزن"
           parentClassName="col-span-2"
@@ -44,7 +54,11 @@ const BmiFormDiet = () => {
           parentClassName="col-span-2"
           placeholder="سن خود را وارد کنید"
         />
-      <MainButton gradient className="w-full h-fit self-end py-3 col-span-2 lg:col-span-4 !text-white" value={"ورود"} />
+        <MainButton
+          gradient
+          className="w-full h-fit self-end py-3 col-span-2 lg:col-span-4 !text-white"
+          value={"ورود"}
+        />
       </section>
     </form>
   );
