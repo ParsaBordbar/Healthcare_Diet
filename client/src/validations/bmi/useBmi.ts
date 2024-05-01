26e7d92df8df59a51c5b7f0ffc7ec8777a5076fb
ParsaBordbar <parsab71@gmail.com>
2024-05-02 00:16:34 +0330

    Make it Ready for deploying on the server. NOTE: Steal in Devlopment.

--- client/src/validations/bmi/useBmi.ts
+++ client/src/validations/bmi/useBmi.ts
@@ -1,75 +1,75 @@
-import * as yup from "yup";
-// import { toast } from 'react-toastify';
+// import * as yup from "yup";
+// // import { toast } from 'react-toastify';
 
-import { useCallback } from "react";
-import { toast } from "react-toastify";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useRouter } from "next/navigation";
-import { BmiDataType } from "@/types";
+// import { useCallback } from "react";
+// import { toast } from "react-toastify";
+// import { useForm } from "react-hook-form";
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import { useRouter } from "next/navigation";
+// import { BmiDataType } from "@/types";
 
-export const BmiSchema = yup.object({
-  firstName: yup
-    .string()
-    .min(6, "نام کاربری باید حداقل ۴ حرف داشته باشد")
-    .max(16, "نام کاربری بیشتر از ۱۶ حرف نمی‌تواند باشد")
-    .matches(
-      /^[a-zA-Z][a-zA-Z0-9]/,
-      "نام کاربری باید با حروف شروع شود و شامل حروف و اعداد می‌شود"
-    )
-    .required("نام کاربری الزامی است"),
-    lastName: yup
-    .string()
-    .min(6, "نام کاربری باید حداقل ۴ حرف داشته باشد")
-    .max(16, "نام کاربری بیشتر از ۱۶ حرف نمی‌تواند باشد")
-    .matches(
-      /^[a-zA-Z][a-zA-Z0-9]/,
-      "نام کاربری باید با حروف شروع شود و شامل حروف و اعداد می‌شود"
-    )
-    .required("نام کاربری الزامی است"),
-    phoneNumber: yup
-    .string()
-    .length(11, "شماره تماس باید ۱۱ عدد داشته باشد")
-    .matches(
-      /(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/gi,
-      "شماره تماس را به درستی وارد کنید"
-    )
-    .required("شماره تماس الزامی است"),
+// export const BmiSchema = yup.object({
+//   firstName: yup
+//     .string()
+//     .min(6, "نام کاربری باید حداقل ۴ حرف داشته باشد")
+//     .max(16, "نام کاربری بیشتر از ۱۶ حرف نمی‌تواند باشد")
+//     .matches(
+//       /^[a-zA-Z][a-zA-Z0-9]/,
+//       "نام کاربری باید با حروف شروع شود و شامل حروف و اعداد می‌شود"
+//     )
+//     .required("نام کاربری الزامی است"),
+//     lastName: yup
+//     .string()
+//     .min(6, "نام کاربری باید حداقل ۴ حرف داشته باشد")
+//     .max(16, "نام کاربری بیشتر از ۱۶ حرف نمی‌تواند باشد")
+//     .matches(
+//       /^[a-zA-Z][a-zA-Z0-9]/,
+//       "نام کاربری باید با حروف شروع شود و شامل حروف و اعداد می‌شود"
+//     )
+//     .required("نام کاربری الزامی است"),
+//     phoneNumber: yup
+//     .string()
+//     .length(11, "شماره تماس باید ۱۱ عدد داشته باشد")
+//     .matches(
+//       /(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/gi,
+//       "شماره تماس را به درستی وارد کنید"
+//     )
+//     .required("شماره تماس الزامی است"),
     
-});
-const useLogin = () => {
-  const {
-    control,
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm<BmiDataType>({
-    resolver: yupResolver(BmiSchema),
-  });
+// });
+// const useLogin = () => {
+//   const {
+//     control,
+//     handleSubmit,
+//     register,
+//     formState: { errors },
+//   } = useForm<BmiDataType>({
+//     resolver: yupResolver(BmiSchema),
+//   });
 
-  if (errors.firstName) {
-    console.log("error");
-    toast.error(errors.firstName.message);
-  }
-  if (!errors.firstName && errors.lastName) {
-    console.log("error");
-    toast.error(errors.lastName.message);
-  }
+//   if (errors.firstName) {
+//     console.log("error");
+//     toast.error(errors.firstName.message);
+//   }
+//   if (!errors.firstName && errors.lastName) {
+//     console.log("error");
+//     toast.error(errors.lastName.message);
+//   }
 
-  const { push } = useRouter();
-  const handelValueInputs = useCallback((data: BmiDataType) => {
-    console.log(data);
-    push("/");
-    toast.success("خوش آمدید");
-  }, []);
+//   const { push } = useRouter();
+//   const handelValueInputs = useCallback((data: BmiDataType) => {
+//     console.log(data);
+//     push("/");
+//     toast.success("خوش آمدید");
+//   }, []);
 
-  return {
-    control,
-    handelValueInputs,
-    register,
-    errors,
-    handleSubmit,
-  };
-};
+//   return {
+//     control,
+//     handelValueInputs,
+//     register,
+//     errors,
+//     handleSubmit,
+//   };
+// };
 
-export default useLogin;
+// export default useLogin;
