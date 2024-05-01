e69e13e77b111ac6b08020e2b38ecd0b6c1669d1
ParsaBordbar <parsab71@gmail.com>
2024-05-01 11:48:50 +0330

    Add the url for Link component, in artSvg.

--- client/src/components/landingPage_components/Navbar/index.tsx
+++ client/src/components/landingPage_components/Navbar/index.tsx
@@ -8,17 +8,17 @@ const Navbar = () => {
     <nav className="flex px-36 justify-between items-center flex-row p-11 bg-[var(--primary)]">
       <ul className="lg:flex hidden justify-between items-center flex-row gap-12 ">
         <li>
-          <Link href={"./"}>
+          <Link href={"/"}>
             <LogoSvg />
           </Link>
         </li>
         <li>
           {" "}
-          <NavElement link="./" text="خدمات" />{" "}
+          <NavElement link="/" text="خدمات" />{" "}
         </li>
         <li>
           {" "}
-          <NavElement link="./" text="بلاگ‌ها" />{" "}
+          <NavElement link="/" text="بلاگ‌ها" />{" "}
         </li>
       </ul>
       <div className="lg:flex hidden flex-row gap-12 justify-center items-center">
