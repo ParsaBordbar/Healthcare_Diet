c13e66d3319111ccdf6657ac7c666c92d6c5d29b
MohammadMosavat <42.octmst@gmail.com>
2024-05-02 20:17:07 +0330

    use fragmentTag

--- client/src/app/register/layout.tsx
+++ client/src/app/register/layout.tsx
@@ -5,10 +5,11 @@ import Navbar from "@/components/landingPage_components/Navbar";
 const LayoutRegister = ({ children }: { children: ReactNode }) => {
   console.log(children);
   return (
-    <div className="relative top-0 z-40">
-      <header className="absolute bg-no-repeat bg-cover w-full h-28 bg-[url('/svg/header-register.svg')] top-0 ">
-        {/* <HeaderSVG className="header opacity-100" width={1920} /> */}
-        {/* <nav className="flex flex-row-reverse left-0 py-7 absolute top-0 px-10 gap-16 items-center">
+    <>
+      <div className="relative top-0 z-40">
+        <header className="absolute bg-no-repeat bg-cover w-full h-28 bg-[url('/svg/header-register.svg')] top-0 ">
+          {/* <HeaderSVG className="header opacity-100" width={1920} /> */}
+          {/* <nav className="flex flex-row-reverse left-0 py-7 absolute top-0 px-10 gap-16 items-center">
           <Logo />
           <ListItems
             desc="خدمات"
@@ -19,15 +20,16 @@ const LayoutRegister = ({ children }: { children: ReactNode }) => {
             className="cursor-pointer !text-[var(--text)]"
           />
         </nav> */}
-        <Navbar />
-      </header>
-      <main className="flex !bg-cover md:!bg-contain bg-no-repeat !bg-none lg:!bg-[url('/svg/loginBanner.svg')] h-screen items-center w-full justify-center lg:justify-end">
-        {/* <div className="absolute -z-20 left-0  h-screen bottom-0 top-0">
+          <Navbar />
+        </header>
+        <main className="flex !bg-cover md:!bg-contain bg-no-repeat !bg-none lg:!bg-[url('/svg/loginBanner.svg')] h-screen items-center w-full justify-center lg:justify-end">
+          {/* <div className="absolute -z-20 left-0  h-screen bottom-0 top-0">
           <LoginBanner className="bg-repeat-y" />
         </div> */}
-        {children}
-      </main>
-    </div>
+          {children}
+        </main>
+      </div>
+    </>
   );
 };
 
