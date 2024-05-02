2e8e42a97d5a0491c6b7727f5498f6b3ed0f2dc6
MohammadMosavat <42.octmst@gmail.com>
2024-05-02 20:14:11 +0330

    this hook folder is for create context it is comment for now

---
+++ client/src/hooks/useContext/Context.ts
@@ -0,0 +1,17 @@
+// // ThemeContext.tsx
+// import React, { useContext } from "react";
+
+// export interface AllContextType {
+//   Bmi: any;
+//   setBmi: any;
+// }
+
+// export const AllContext = React.createContext<AllContextType | undefined>(undefined);
+
+// export const useMyContext = () => {
+//   const context = useContext(AllContext);
+//   if (!context) {
+//     throw new Error("useMyContext must be used within a MyContextProvider");
+//   }
+//   return context;
+// };
---
+++ client/src/hooks/useContext/ContextProvider.tsx
@@ -0,0 +1,13 @@
+// 'use client'
+// import React, { ReactNode, useState } from "react";
+// import { AllContext } from "./Context";
+
+// const ContextProvider = ({ children }: { children: ReactNode }) => {
+//   const [Bmi, setBmi] = useState({});
+
+//   return (
+//     <AllContext.Provider value={{ Bmi, setBmi }}>{children}</AllContext.Provider>
+//   );
+// };
+
+// export default ContextProvider;
