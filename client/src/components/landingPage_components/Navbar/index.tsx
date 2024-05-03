783acf0306c62d16ed8afa37bb043570c5705c55
MohammadMosavat <42.octmst@gmail.com>
2024-05-03 18:52:13 +0330

    add the correct url to link of component in Navbar component

--- client/src/components/landingPage_components/Navbar/index.tsx
+++ client/src/components/landingPage_components/Navbar/index.tsx
@@ -22,14 +22,15 @@ const Navbar = () => {
         </li>
       </ul>
       <div className="lg:flex hidden flex-row gap-12 justify-center items-center">
-        <Link href={'register/login/enterNumber'}>        
+        <Link href={'/register/signup'}>        
           <MainButton trasparent className="py-2 px-5 " value={"ثبت نام"} />
         </Link>
-        <Link href={'register/login/enterNumber'}>
+        <Link href={'/register/login/enterNumber'}>
           <MainButton simple value={"ورود"} />
         </Link>
       </div>
     </nav>
   );
 };
+
 export default Navbar;
