ad6996212fe3467db055ef7afa730d275e95413f
MohammadMosavat <42.octmst@gmail.com>
2024-05-03 18:55:29 +0330

    fix the url in signup page

--- client/src/app/register/signup/page.tsx
+++ client/src/app/register/signup/page.tsx
@@ -7,7 +7,7 @@ const SignUpPage = () => {
     <>
       <div className="absolute flex flex-col justify-center lg:justify-end items-center gap-5 w-4/5 lg:w-1/3 top-[9.375rem] lg:right-[7.5rem]">
         <FormSignUp />
-        <Link href={"/register/login"} className="text-[var(--text)] text-xl">
+        <Link href={"/register/login/enterNumber"} className="text-[var(--text)] text-xl">
           آیا حساب دارید؟{" "}
           <mark className="bg-transparent text-[var(--rating-color)]">
             ورود
