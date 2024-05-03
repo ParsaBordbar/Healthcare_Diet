0c60e065390c303426cf93d3a6342898a69b1ba1
MohammadMosavat <42.octmst@gmail.com>
2024-05-03 18:54:53 +0330

    import useEnterNumber in login of enterNumber section to validate and shows erros and handle it

--- client/src/components/FormRegisters/Login/login.tsx
+++ client/src/components/FormRegisters/Login/login.tsx
@@ -3,19 +3,43 @@ import { LoginFormType } from "@/types";
 import MainButton from "../../MainButton";
 import MainInput from "../../MainInput";
 import Email from "/public/svg/email.svg";
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
+import { Controller } from "react-hook-form";
+import useEnterNumber from "@/validations/login/enterNumber/useEnterNumber";
+import Phone from "/public/svg/Phone.svg";
 
 const FormLogin = ({ type }: LoginFormType) => {
+  const phoneNumberRef = useRef<HTMLInputElement | null>(null);
+  const { control, errors, handelValueInputs, handleSubmit, register } =
+    useEnterNumber();
+  useEffect(() => {
+    phoneNumberRef.current?.focus();
+  }, [errors.phoneNumber]);
   const CheckTypeRender = useCallback(() => {
     if (type == "getCode") {
       return (
         <>
-          <MainInput
-            label="کد را وارد کنید"
-            iconFirst={Email}
-            placeholder="کد دریافتی را وراد کنید"
+          <Controller
+            control={control}
+            name="phoneNumber"
+            render={({ field }) => (
+              <MainInput
+                {...field}
+                error={!!errors.phoneNumber}
+                ref={phoneNumberRef}
+                iconFirst={Phone}
+                // onChange={() => {
+                // setBmi(phoneNumberRef.current?.value);
+                // }}
+                // value={Bmi.phoneNumber}
+                parentClassName="col-span-2 lg:col-span-4"
+                label="شماره تماس"
+                placeholder="شماره تماس خود را وارد کنید"
+              />
+            )}
           />
           <MainButton
+            type="submit"
             simple
             className="bg-[var(--rating-color)] py-2.5 !text-xl !text-white w-full"
             value={"ورود"}
@@ -25,23 +49,41 @@ const FormLogin = ({ type }: LoginFormType) => {
     } else {
       return (
         <>
-          <MainInput
-            label="لطفا شماره تماس خود را وارد کنید:"
-            iconFirst={Email}
-            placeholder="abc@gmail.com"
+          <Controller
+            control={control}
+            name="phoneNumber"
+            render={({ field }) => (
+              <MainInput
+                {...field}
+                iconFirst={Phone}
+                error={!!errors.phoneNumber}
+                ref={phoneNumberRef}
+                // onChange={() => {
+                // setBmi(phoneNumberRef.current?.value);
+                // }}
+                // value={Bmi.phoneNumber}
+                parentClassName="col-span-2 lg:col-span-4"
+                label="شماره تماس"
+                placeholder="شماره تماس خود را وارد کنید"
+              />
+            )}
           />
           <MainButton
+            type="submit"
             simple
             className="bg-[var(--rating-color)] py-2.5 !text-xl !text-white w-full"
             value={"ارسال کد ورود"}
-            url="/register/login/enterTheCode"
           />
         </>
       );
     }
-  }, []);
+  }, [errors.phoneNumber]);
   return (
-    <form action="" className="flex w-full gap-10 flex-col justify-center">
+    <form
+      action=""
+      onSubmit={handleSubmit(handelValueInputs)}
+      className="flex w-full gap-10 flex-col justify-center"
+    >
       <h1 className="text-center font-bold text-2xl">ورود</h1>
       <section className="flex w-full flex-col items-center gap-[3.125rem]">
         {CheckTypeRender()}
