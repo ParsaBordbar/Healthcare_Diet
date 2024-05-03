7c051a2d9f3a0904d73793e5f282ebccee7399c5
ParsaBordbar <parsab71@gmail.com>
2024-05-03 13:03:13 +0330

    Add Lines of code to add a collection to db to make it if there is no any.

--- server/server.js
+++ server/server.js
@@ -8,9 +8,22 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/bmi', bmi);
 
-mongoose.connect('mongodb://localhost/healthcareDiet')
-    .then(() => console.log("Connected to Mongodb :)"))
-    .catch(() => (console.error("Could not connect to Mongodb.")));
+mongoose.connect('mongodb://localhost/healthcareDiet', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to MongoDB");
+        const db = mongoose.connection;
+        db.listCollections({name: 'bmi_forms'})
+            .next(function(err, collinfo) {
+                if (!collinfo) {
+                    db.createCollection('bmi_forms', function(err, res) {
+                        if (err) throw err;
+                        console.log("Collection created!");
+                    });
+                }
+            });
+    })
+    .catch((err) => console.error("Could not connect to MongoDB.", err));
+
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on ${port}`));
