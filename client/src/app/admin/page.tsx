395d6b5a22ec8ced8b9ba20b51c6ff5c2db09eea
ParsaBordbar <parsab71@gmail.com>
2024-05-05 18:59:45 +0330

    Add route for Admin.

---
+++ client/src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import AdminLogin from "@/components/AdminLogin";
+
+const Admin = () => {
+    return(
+        <section className="flex justify-center items-center h-screen">
+            <AdminLogin />
+        </section>
+    )
+};
+
+export default Admin;
