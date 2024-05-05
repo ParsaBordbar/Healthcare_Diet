1e12365312f19f3e171e89873b6d58572d9f025b
MohammadMosavat <42.octmst@gmail.com>
2024-05-05 18:56:30 +0330

    create useContext component in hook folder

--- client/src/hooks/useContext/Context.ts
+++ client/src/hooks/useContext/Context.ts
@@ -1,17 +1,17 @@
-// // ThemeContext.tsx
-// import React, { useContext } from "react";
+// ThemeContext.tsx
+import React, { useContext } from "react";
 
-// export interface AllContextType {
-//   Bmi: any;
-//   setBmi: any;
-// }
+export interface AllContextType {
+  Bmi: any;
+  setBmi: any;
+}
 
-// export const AllContext = React.createContext<AllContextType | undefined>(undefined);
+export const AllContext = React.createContext<AllContextType | undefined>(undefined);
 
-// export const useMyContext = () => {
-//   const context = useContext(AllContext);
-//   if (!context) {
-//     throw new Error("useMyContext must be used within a MyContextProvider");
-//   }
-//   return context;
-// };
+export const useMyContext = () => {
+  const context = useContext(AllContext);
+  if (!context) {
+    throw new Error("useMyContext must be used within a MyContextProvider");
+  }
+  return context;
+};
