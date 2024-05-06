bc127e3bf6d989662c323f24102c6166b59dba4c
ParsaBordbar <parsab71@gmail.com>
2024-05-06 15:15:10 +0330

    Add schema field for mediterraneanForm in it's Model in backend.

--- server/models/mediterraneanForm.js
+++ server/models/mediterraneanForm.js
@@ -6,7 +6,7 @@ const mediterraneanForm = mongoose.model('mediterranean_form', mongoose.Schema({
         required: true,
         enum: ['۱-۲ واحد', '۲-۳ واحد','۳-۴ واحد', 'هیچ'],
     },
-    dailyVegtable : {
+    dailyVegetable : {
         required: true,
         enum: ['۱-۲ واحد', '۲-۳ واحد','۳-۴ واحد', 'هیچ'],
     },
@@ -22,6 +22,66 @@ const mediterraneanForm = mongoose.model('mediterranean_form', mongoose.Schema({
         required: true,
         enum: ['۱-۲ بار', '۲-۳ بار','۳-۴ بار', 'هیچ']
     },
-    
+    potatoAndStarchWeekly: {
+        required: true,
+        enum: ['۱-۲ بار', '۲-۳ بار', 'هر روز' , 'هیچ']
+    }
+    ,
+    oliveAndOliveOilDaily: {
+        required: true,
+        enum: ['۱ واحد', '۲ واحد','۳ واحد یا بیشتر ', 'هیچ']    
+    },
+    nutsDaily: {
+        required: true,
+        enum: ['۱-۲ بار', '۲-۳ بار','۳-۴ بار', 'هیچ']
+    },
+    dairyDaily: {
+        required: true,
+        enum: ['۱-۲ بار', '۲-۳ بار','بیشتر از ۲ بار', 'هیچ']        
+    },
+    beans: {
+        required: true,
+        enum: ['روزانه', 'هفتگی', 'ماهیانه', 'هیچ']
+    },
+    eggWeekly: {
+        required: true,
+        enum: ['۱-۲ بار', '۴-۳ بار','هر روز', 'هیچ']  
+    },
+    fishWeekly: {
+        required: true,
+        enum: ['۱ بار', '۲ بار','۳ بار', 'هیچ']  
+    },
+    chickensWeekly: {
+        required: true,
+        enum: ['۱-۲ بار','۳-۴ بار', 'هر روز', 'هیچ']  
+    },
+    chickensWeekly: {
+        required: true,
+        enum: ['۱-۲ بار','۳-۴ بار', 'هر روز', 'هیچ']  
+    },
+    sugarWeekly: {
+        required: true,
+        enum: ['۱ بار','۲-۳ بار', 'هر روز', 'هیچ'] 
+    },
+    alcoholWeekly: {
+        required: true,
+        enum: ['۱ بار','۲-۳ بار', 'هر روز', 'هیچ'] 
+    },
+    fermentationWeekly: {
+        required: true,
+        enum: ['۱-۲ بار', '۴-۳ بار','هر روز', 'هیچ']  
+    },
+    supplements: { //This is a multi select 
+        required: true,
+
+    },
+    physicalActivity: {
+        required: true,
+        enum: ['۱-۲ بار', '۴-۳ بار','هر روز', 'هیچ']  
+    },
+    diabetes: {
+        required: true,
+        enum: ['۱دیابت', 'دیابت۲','دیابت بارداری']  
+    }
 }))
 module.exports = mediterraneanForm;
