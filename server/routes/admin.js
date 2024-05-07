11f32f4d5693c990069002826707b051975f7186
ParsaBordbar <parsab71@gmail.com>
2024-05-07 10:06:15 +0330

    Add admin route in backend.

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async(req, res) => {
-    if (req.body.password === "admin" && req.body.userName ){
+    if (req.body.password === "admin" && req.body.userName == 'admin' ){
         res.status(200)
         res.send("It's OK!")
     }
