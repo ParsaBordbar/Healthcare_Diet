e50e3651794dc023dc9b00e0269767f26ff53c3d
MohammadMosavat <42.octmst@gmail.com>
2024-05-07 12:01:22 +0330

    create a accordion for every diets in user panel

---
+++ client/src/components/Accordion/index.tsx
@@ -0,0 +1,27 @@
+// Accordion.js 
+
+const Accordion = (props:any) => { 
+	return ( 
+		<div className="border rounded-lg mb-1"> 
+			<button 
+				className="w-full rounded-t-lg flex items-center justify-between p-4 text-left hover:bg-green-800 
+						bg-[var(--rating-color)] text-white transition duration-300"
+				onClick={props.toggleAccordion} 
+			> 
+				{props.title} 
+				<span className={`float-right transform ${props.isOpen ? 
+								'rotate-180' : 'rotate-0'} 
+								transition-transform duration-300`}> 
+					&#9660; 
+				</span> 
+			</button> 
+			{props.isOpen && ( 
+				<div className="p-4 bg-green-100"> 
+					{props.data} 
+				</div> 
+			)} 
+		</div> 
+	); 
+}; 
+
+export default Accordion
