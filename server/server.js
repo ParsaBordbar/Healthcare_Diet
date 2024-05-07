725117e50e8e3b0216cbe30aa187f4b36beaa148
ParsaBordbar <parsab71@gmail.com>
2024-05-07 10:07:15 +0330

    Add new routes to backend, server.js

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-const bmi = require('./routes/bmi')
+const bmi = require('./routes/bmi');
+const mediterranean = require('./routes/mediterranean');
+const admin = require('./models/admin');
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/bmi', bmi);
+app.use('/api/mediterranean', mediterranean);
+app.use('/api/admin', admin);
+
 
 mongoose.connect(process.env.DB_CONNECTION_STRING)
     .then(() => console.log("Connected to Mongodb :)"))
