dc227898a4f586cafea948e4c6e97a18ad5360ed
MohammadMosavat <42.octmst@gmail.com>
2024-05-07 12:00:24 +0330

    create group radio component for handle the questions of mediterraneal foorm in user panel

---
+++ client/src/components/GroupRadio/index.tsx
@@ -0,0 +1,26 @@
+import { InputHTMLAttributes } from "react";
+import { Formik, Field, Form } from "formik";
+
+export type TGroupRadio = InputHTMLAttributes<HTMLInputElement> & {
+  title?: string;
+  value: string;
+  header: Boolean;
+};
+const GroupRadio = (props: TGroupRadio) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <section
+        className="flex items-center justify-between"
+        role="group"
+        aria-labelledby="my-radio-group"
+      >
+        <label>
+          <Field {...props} type="radio" value={props.value} />
+          {props.value}
+        </label>
+      </section>
+    </div>
+  );
+};
+
+export default GroupRadio;
