26ad872fd4eb4c2867dc8b9e971e24ccbe192e5b
ParsaBordbar <parsab71@gmail.com>
2024-05-07 10:06:54 +0330

    Add mediterranean route im the backend.

--- server/routes/mediterranean.js
+++ server/routes/mediterranean.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router()
+const { MediterraneanForm } = require('../models/mediterraneanForm');
 
 router.get('/', async (req, res) => {
-    const mediterraneanForms = await mediterraneanForm.find().sort(req.params.id);
-    res.send(mediterraneanForms)
+    if(MediterraneanForm) {
+        const mediterraneanForms = await MediterraneanForm.find().sort(req.params.id);
+        res.send(mediterraneanForms)
+    }
+    else res.send({"DataBase": "NO mediterraneanForms yet!"});
+});
+
+router.get('/:id', async(req, res) => {
+    const mediterraneanForm = await MediterraneanForm.findById(req.params.id);
+    if(!mediterraneanForm) res.status(404).send("Mediterranean with this ID was not found.")
+    res.send(mediterraneanForm)
 })
+
+module.exports = router;
