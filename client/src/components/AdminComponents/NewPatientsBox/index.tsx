9774f267031a042b123b026495862339e29bb375
ParsaBordbar <parsab71@gmail.com>
2024-05-09 00:25:19 +0330

    Add new styles to admin panel component.

--- client/src/components/AdminComponents/NewPatientsBox/index.tsx
+++ client/src/components/AdminComponents/NewPatientsBox/index.tsx
@@ -1,20 +1,24 @@
 'use client'
-import PatientBmiForm from '../PatientBmiForm';
+import Link from 'next/link';
 import useFetchBmi from '../../../hooks/useFetchBmi'
+import PatientBox from '../PatientBox';
 
 function NewPatientsBox() {
   const  bmiForms = useFetchBmi();    
   return (
-    <section className='flex bg-[var(--primary)] flex-col'>
-        {bmiForms.map((form) => {
-            return <PatientBmiForm 
+    <section className='flex flex-col items-center rounded-2xl my-4'>
+      <h2 className='text-lg'>جدیدترین بیمارها</h2>
+        {bmiForms.slice(-3).map((form) => {
+            return <PatientBox
             name={form.name}
             lastName={form.lastName}
             phoneNumber={form.phoneNumber}
-            bmi={form.bmi}
             key={form.phoneNumber}
+            bmi=''
           />
-          })}
+          })
+          }
+      <Link className='pt-3 text-lg text-blue-600 ' href={'./panel/patients'}>دیدن همه بیمارها</Link>
     </section>
   )
 }
--- client/src/components/AdminComponents/PatientBox/index.tsx
+++ client/src/components/AdminComponents/PatientBox/index.tsx
@@ -1,9 +1,13 @@
+import { BmiType } from '@/types'
 import React from 'react'
 
-function Patients() {
+function PatientBox({name, lastName, phoneNumber, bmi}: BmiType) {
   return (
-    <div>Patients</div>
+    <div className=" border-[var(--tittle-box)] shadow-md flex gap-7 justify-center my-3 bg-[var(--primary)] py-4 items-center w-3/5 rounded-2xl text-lg">
+      <h2 className='flex justify-center text-[var(--text)]'>نام و نام‌خانوادگی:{name}{" "}{lastName}</h2>
+      <span className='flex justify-center text-[var(--text)]'>شماره تماس:{" "}{phoneNumber}</span>
+    </div>
   )
 }
 
-export default Patients
+export default PatientBox
