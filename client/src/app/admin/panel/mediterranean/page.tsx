a72e8d73a221b96cbe4571017d9ac3547a9f4140
ParsaBordbar <parsab71@gmail.com>
2024-05-10 00:47:58 +0330

    Add MediterraneanForm component to the page with the same name.

--- client/src/app/admin/panel/mediterranean/page.tsx
+++ client/src/app/admin/panel/mediterranean/page.tsx
@@ -1,6 +1,11 @@
+import MediterraneanForm from "@/components/AdminComponents/MediterraneanForm";
+
 const MediterraneanFormsPage = () => {
     return(
-        <h1>Mediterranean panel</h1>
+        <section>
+            <h1 className='text-4xl mb-6'>فرم‌های رژیم مدیترانه‌ای:</h1>
+            <MediterraneanForm dailyFruit={"parsa"} dailyVegetable={undefined} Cereals={undefined} dailyCereals={undefined} potatoAndStarchWeekly={undefined} oliveAndOliveOilDaily={undefined} nutsDaily={' ko? ni! khhhhhhh'} dairyDaily={undefined} beans={undefined} eggWeekly={undefined} fishWeekly={undefined} chickensWeekly={undefined} sugarWeekly={undefined} alcoholWeekly={undefined} fermentationWeekly={undefined} supplements={undefined} physicalActivity={undefined} diabetes={undefined} anemia={undefined} bloodPressure={undefined} digestiveProblems={undefined} selfSafety={undefined} stroke={undefined} fattyLiver={undefined} kidneyProblems={undefined} thyroid={undefined} cancer={false} Migraine={false} otherSickness={undefined} medicine={undefined} phoneNumber={undefined} />
+        </section>
     )
 }
 
