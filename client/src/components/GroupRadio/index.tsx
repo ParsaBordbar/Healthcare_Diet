26f7add947eb5242916b93a1fb744d3a7d103065
MohammadMosavat <42.octmst@gmail.com>
2024-05-09 20:01:37 +0330

    change the group radion and its just simple input not Field from Formik

--- client/src/components/GroupRadio/index.tsx
+++ client/src/components/GroupRadio/index.tsx
@@ -1,5 +1,4 @@
 import { InputHTMLAttributes } from "react";
-import { Formik, Field, Form } from "formik";
 
 export type TGroupRadio = InputHTMLAttributes<HTMLInputElement> & {
   value: string;
@@ -7,18 +6,12 @@ export type TGroupRadio = InputHTMLAttributes<HTMLInputElement> & {
 };
 const GroupRadio = (props: TGroupRadio) => {
   return (
-    <div className="flex flex-col gap-2">
-      <section
-        className="flex items-center justify-between"
-        role="group"
-        aria-labelledby="my-radio-group"
-      >
-        <label>
-          <Field {...props} type="radio" value={props.value} />
-          {props.value}
-        </label>
-      </section>
-    </div>
+
+
+    <label>
+      <input {...props} />
+      {props.value}
+    </label>
   );
 };
 
