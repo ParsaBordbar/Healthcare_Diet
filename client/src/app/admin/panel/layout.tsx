171ec645a0b02f94c81991961a80b3d75081b258
MohammadMosavat <42.octmst@gmail.com>
2024-05-10 10:12:30 +0330

    fix the layout of links in admin panel now is good

--- client/src/app/admin/panel/layout.tsx
+++ client/src/app/admin/panel/layout.tsx
@@ -9,29 +9,38 @@ const LayoutAdmin = ({ children }: { children: ReactNode }) => {
       <div className="sticky flex w-full top-0 z-40">
         <div className="bg-[var(--rating-color)] gap-16 flex flex-col h-screen w-1/6 py-4 ">
           <div className="flex items-center py-2 rounded-lg justify-around ">
-           
             <h1 className="text-white text-2xl font-semibold">نام دکتر</h1>
           </div>
           <ul className="flex flex-col rounded-lg h-screen py-4 ps-4 gap-4 ">
-          <li className="hover:text-[var(--rating-color)] text-xl cursor-pointer hover:text-black text-white hover:bg-white hover:bg-full ease-in-out transition-all duration-150 p-4 rounded-s-md">
-                  <Link href={"/admin/panel/"}> پنل کاربری</Link>                
-            </li>
-            <li className="hover:text-[var(--rating-color)] text-xl cursor-pointer hover:text-black text-white hover:bg-white hover:bg-full ease-in-out transition-all duration-150 p-4 rounded-s-md">
-                  <Link href={"/admin/panel/patients/"}> بیمارها</Link>                
-            </li>
-            <li className="hover:text-[var(--rating-color)] text-xl cursor-pointer hover:text-black text-white hover:bg-white hover:bg-full ease-in-out transition-all duration-150 p-4 rounded-s-md">
-                  <Link href={"/admin/panel/bmi/"}> فرم‌های BMI</Link>                
-            </li>
-            <li className="hover:text-[var(--rating-color)] text-xl cursor-pointer hover:text-black text-white hover:bg-white hover:bg-full ease-in-out transition-all duration-150 p-4 rounded-s-md">
-                  <Link href={"/admin/panel/mediterranean/"}> فرم‌های رژیم مدیترانه‌ای </Link>                
-            </li>
+            <Link
+              href={"/admin/panel/"}
+              className="hover:text-[var(--rating-color)] text-xl cursor-pointer hover:text-black text-white hover:bg-white hover:bg-full ease-in-out transition-all duration-150 p-4 rounded-s-md"
+            >
+              <li> پنل کاربری</li>
+            </Link>
+            <Link
+              href={"/admin/panel/patients/"}
+              className="hover:text-[var(--rating-color)] text-xl cursor-pointer hover:text-black text-white hover:bg-white hover:bg-full ease-in-out transition-all duration-150 p-4 rounded-s-md"
+            >
+              <li> بیمارها</li>
+            </Link>
+            <Link
+              href={"/admin/panel/bmi/"}
+              className="hover:text-[var(--rating-color)] text-xl cursor-pointer hover:text-black text-white hover:bg-white hover:bg-full ease-in-out transition-all duration-150 p-4 rounded-s-md"
+            >
+              <li> فرم‌های BMI</li>
+            </Link>
+            <Link
+              href={"/admin/panel/mediterranean/"}
+              className="hover:text-[var(--rating-color)] text-xl cursor-pointer hover:text-black text-white hover:bg-white hover:bg-full ease-in-out transition-all duration-150 p-4 rounded-s-md"
+            >
+              <li>فرم‌های رژیم مدیترانه‌ای</li>
+            </Link>
           </ul>
         </div>
         <main className="w-5/6 px-28 py-16 bg-white">
-          <div className="">
-          {children}
-          </div>
-          </main>
+          <div className="">{children}</div>
+        </main>
       </div>
     </>
   );
