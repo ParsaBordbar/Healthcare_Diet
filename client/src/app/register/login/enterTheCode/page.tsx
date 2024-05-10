450c772163f4e5fb8d2d18f79cdff829616c404d
MohammadMosavat <42.octmst@gmail.com>
2024-05-10 16:38:20 +0330

    change the name of enter code pag

--- client/src/app/register/login/enterTheCode/page.tsx
+++ client/src/app/register/login/enterTheCode/page.tsx
@@ -3,7 +3,7 @@ import FormLogin from "@/components/FormRegisters/Login/login";
 import Social from "@/components/FormRegisters/Login/social";
 import UserIdentifyForm from "@/components/UserIdentifierForm";
 
-const LoginPage = () => {
+const EnterTheCodePage = () => {
   return (
     <>
       <div className="absolute  lg:!shadow-none lg:p-0 py-5 px-5 lg:!bg-transparent flex flex-col items-center gap-5 w-4/5 lg:w-1/3  lg:right-[7.5rem] ">
@@ -13,4 +13,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default EnterTheCodePage;
