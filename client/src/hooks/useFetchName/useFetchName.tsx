c6eae5e2141ea0d21e893787dcc6783b6b74f016
ParsaBordbar <parsab71@gmail.com>
2024-05-11 00:21:36 +0330

    Update the useFetchName hook. Now It request for a single collection in db.

--- client/src/hooks/useFetchName/useFetchName.tsx
+++ client/src/hooks/useFetchName/useFetchName.tsx
@@ -2,16 +2,17 @@ import api from "@/apis";
 import { BmiType } from "@/types";
 import { useState, useEffect } from "react";
 
-const useFetchName = (phoneNumber :string) => {
+const useSpecificFetchBmi = (phoneNumber: string) => {
 
-    const [bmiForms, setBmiForms] = useState<BmiType[]>([]);
+    const [bmiForms, setBmiForms] = useState<BmiType>();
     
     useEffect(()=>{
-        fetchBmiPatientForms(phoneNumber)
-    }, [phoneNumber])  
+        fetchBmiPatientForms()
+    }, [bmiForms])
   
-    const fetchBmiPatientForms = async (phoneNumber :string) => {
+    const fetchBmiPatientForms = async () => {
       try{
+        console.log(bmiForms);
           const response = await api.get(`/bmi/${phoneNumber}`);
           setBmiForms(response.data);
       }
@@ -26,4 +27,4 @@ const useFetchName = (phoneNumber :string) => {
   }
   return bmiForms;
 }
-export default useFetchName;
+export default useSpecificFetchBmi;
