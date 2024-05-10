a35df4dce51a686fb9108fbc13c8912c835294ed
ParsaBordbar <parsab71@gmail.com>
2024-05-10 12:56:45 +0330

    Fix positon for pnael sidebar.

--- client/src/app/admin/panel/layout.tsx
+++ client/src/app/admin/panel/layout.tsx
@@ -5,18 +5,12 @@ import Link from "next/link";
 const LayoutAdmin = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <div className="sticky flex w-full top-0 z-40">
-        <div className="bg-[var(--rating-color)] gap-16 flex flex-col h-screen w-1/6 py-4 ">
-          <div className="flex items-center py-2 rounded-lg justify-around ">
-            <h1 className="text-white text-2xl font-semibold">نام دکتر</h1>
+      <div className="sticky flex justify-end w-full top-0 z-40">
+        <div className="fixed top-0 right-0 h-screen bg-[var(--rating-color)] gap-16 flex flex-col w-1/6 ">
+          <div className="flex items-center p-4rounded-lg justify-around ">
+          <h1 className="text-white text-2xl font-semibold my-10">نام دکتر</h1>
           </div>
           <ul className="flex flex-col rounded-lg h-screen py-4 ps-4 gap-4 ">
-          <Link
-              href={"/"}
-              className="hover:text-[var(--rating-color)] text-xl cursor-pointer hover:text-black text-white hover:bg-white hover:bg-full ease-in-out transition-all duration-150 p-4 rounded-s-md"
-            >
-              <li>خانه</li>
-            </Link>
             <Link
               href={"/admin/panel/"}
               className="hover:text-[var(--rating-color)] text-xl cursor-pointer hover:text-black text-white hover:bg-white hover:bg-full ease-in-out transition-all duration-150 p-4 rounded-s-md"
@@ -43,7 +37,7 @@ const LayoutAdmin = ({ children }: { children: ReactNode }) => {
             </Link>
           </ul>
         </div>
-        <main className="w-5/6 px-28 py-16 bg-white">
+        <main className="w-5/6 p-28 bg-white">
           <div className="">{children}</div>
         </main>
       </div>
