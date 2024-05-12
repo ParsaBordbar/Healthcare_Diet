d96c3aceba445e751e940d3343e5ecfbaaf6e7f5
ParsaBordbar <parsab71@gmail.com>
2024-05-12 18:14:19 +0330

    Update the useUpdateIsChecked hook.

--- client/src/hooks/useUpdateIsChecked/index.tsx
+++ client/src/hooks/useUpdateIsChecked/index.tsx
@@ -1,9 +1,17 @@
 import api from "@/apis"
+import { useEffect } from "react";
 
-const useUpdateIsChecked = async (phoneNumber: string, checked: boolean) => {
-    const response = await api.put(`mediterranean/${phoneNumber}`, {
-        isChecked: checked
-    })
-    console.log(response);
-    return response;
+const useUpdateIsChecked = (phoneNumber: string, checked: boolean) => {
+    useEffect(() => {
+        const update = async() => {
+            const response = await api.put(`mediterranean/${phoneNumber}`, {
+                isChecked: checked
+            })
+            console.log(response);
+            return response;
+        }
+        update();
+    }, [checked]); // Add 'checked' to the dependency array
 }
+
+export default useUpdateIsChecked;
