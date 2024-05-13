8ffa7eb951dd0b117bd7d8057fc6eb00931c07ae
ParsaBordbar <parsab71@gmail.com>
2024-05-13 11:23:52 +0330

    Add custom hook 'useFeetchOneMediterranean'. this hook gets the data for a spicific data set.

---
+++ client/src/hooks/useFetchOneMediterranean/index.tsx
@@ -0,0 +1,28 @@
+import api from "@/apis"
+import { MediterraneanFormType } from "@/types"
+import { useEffect, useState } from "react"
+
+const useFetchOneMediterranean = (phoneNumber: string) => {
+    
+    const [medForm, setMedForm] = useState<MediterraneanFormType>();
+    useEffect(()=> {
+        fetchForm()
+    }, [])
+
+    const fetchForm = async () => {
+        try{
+            const response = await api.get(`/mediterranean/${phoneNumber}`);
+            setMedForm(response.data);
+        }
+        catch(err : any){
+           if (err.response){
+            console.log(err.response.data)
+           }
+           else{
+            console.log(`Error: ${err.message}`);
+           }
+        }
+    }
+    return medForm;
+}
+export default useFetchOneMediterranean;
