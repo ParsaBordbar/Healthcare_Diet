1d882c656098edda22b37060f45bb107ef3debba
ParsaBordbar <parsab71@gmail.com>
2024-05-13 12:12:54 +0330

    Add Code for Linking to dynamic pages.

--- client/src/app/admin/panel/patients/page.tsx
+++ client/src/app/admin/panel/patients/page.tsx
@@ -1,11 +1,19 @@
+'use client'
 import PatientBox from "@/components/AdminComponents/PatientBox"
+import useFetchBmi from "@/hooks/useFetchBmi"
+import Link from "next/link"
 
 const PatientsPage = () => {
+    const data = useFetchBmi()
     return(
         <section>
-            <PatientBox name={"s"} lastName={"s"} phoneNumber={"890890890"} bmi={""} />
+            {data.map(d => {
+                return <Link href={`/admin/panel/patients/${d.phoneNumber}`} key={d.phoneNumber}>
+                    <PatientBox name={d.name} lastName={d.lastName} phoneNumber={d.phoneNumber} />
+                </Link>
+            })}
         </section>
     )
 }
 
-export default PatientsPage
+export default PatientsPage;
