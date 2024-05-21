1e89cd39bf787295a3a26e8133d4d9a2ac3c2358
ParsaBordbar <parsab71@gmail.com>
2024-05-22 01:47:55 +0330

    Add PatientCard Component to Admin Panel.

---
+++ client/src/components/AdminComponents/PatientCard/index.tsx
@@ -0,0 +1,40 @@
+import { BmiType } from "@/types"
+import FemaleAvatar from '/public/svg/adminPanelSvgs/Group 107.svg'
+import MaleAvatar from '/public/svg/adminPanelSvgs/Group 108.svg'
+import PhoneIcon from '/public/svg/phone.svg'
+
+const PatientCard = ({name, lastName, gender, phoneNumber }: BmiType) => {
+
+    {
+        if(gender ==="male"){
+            return(
+                <div className="flex m-4 col-span-full bg-[var(--primary)] gap-4 justify-center py-5 rounded-lg shadow-md">
+                    <MaleAvatar />
+                    <div className="flex flex-col gap-4 pt-10 px-8 w-1/2">
+                        <h2 className="text-4xl">{name} {lastName}</h2>
+                        <div className="flex gap-2">
+                            <PhoneIcon />
+                            <p className="text-lg text-[var(--text)]">{phoneNumber}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+
+        if(gender ==="female"){
+            return(
+                <div className="flex m-4 col-span-full bg-[var(--primary)] gap-4 justify-center py-5 rounded-lg shadow-md">
+                    <FemaleAvatar />
+                    <div className="flex flex-col gap-4 pt-10 px-8 w-1/2">
+                        <h2 className="text-4xl">{name} {lastName}</h2>
+                        <div className="flex gap-2">
+                            <PhoneIcon />
+                            <p className="text-lg text-[var(--text)]">{phoneNumber}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        }
+}
+export default PatientCard
