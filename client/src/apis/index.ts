5d7d7189c8e09e089c7fd575c0ae389cf6f24eb3
ParsaBordbar <parsab71@gmail.com>
2024-05-22 01:49:26 +0330

    Update apis and ecosystem config for server side.

--- client/src/apis/index.ts
+++ client/src/apis/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://70.34.217.65:8080//api'
+    baseURL: 'http://localhost:8080/api'
 });
 export default api;
--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
     env: {
       NODE_ENV: 'development',
       PORT: 8080,
-      DB_CONNECTION_STRING: 'mongodb://localhost:8080/'
+      DB_CONNECTION_STRING: 'mongodb://localhost:8080/healthcareDiet'
     },
     env_production: {
       NODE_ENV: 'production',
