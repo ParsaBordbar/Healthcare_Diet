553bcb668ddc8ac22df545469b4880a7e508965f
ParsaBordbar <parsab71@gmail.com>
2024-05-22 01:48:46 +0330

    Add paitentCard component to admin page.

--- client/src/app/admin/panel/patients/page.tsx
+++ client/src/app/admin/panel/patients/page.tsx
@@ -1,15 +1,17 @@
 'use client'
 import PatientBox from "@/components/AdminComponents/PatientBox"
+import PatientCard from "@/components/AdminComponents/PatientCard"
 import useFetchBmi from "@/hooks/useFetchBmi"
 import Link from "next/link"
 
 const PatientsPage = () => {
     const data = useFetchBmi()
     return(
-        <section>
+        <section className="grid grid-cols-2"> 
+            <h1 className="col-span-full text-3xl mb-3">بیمارها:</h1>
             {data.map(d => {
                 return <Link href={`/admin/panel/patients/${d.phoneNumber}`} key={d.phoneNumber}>
-                    <PatientBox name={d.name} lastName={d.lastName} phoneNumber={d.phoneNumber} />
+                    <PatientCard name={d.name} lastName={d.lastName} phoneNumber={d.phoneNumber} gender={d.gender}/>
                 </Link>
             })}
         </section>
