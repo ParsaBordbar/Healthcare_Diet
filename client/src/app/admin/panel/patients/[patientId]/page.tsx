461f68fa9ff4c4622ed9fc9240c4965dc80faab7
ParsaBordbar <parsab71@gmail.com>
2024-05-22 13:09:21 +0330

    Fix a major bug in the backend. The Bug sends multipul reqs with the same headers so the server shutdowns!

--- client/src/app/admin/panel/patients/[patientId]/page.tsx
+++ client/src/app/admin/panel/patients/[patientId]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import MediterraneanForm from '@/components/AdminComponents/MediterraneanForm'
 import PatientBmiForm from '@/components/AdminComponents/PatientBmiForm'
+import PatientCard from '@/components/AdminComponents/PatientCard'
 import useSpecificFetchBmi from '@/hooks/useFetchName/useFetchName'
 import useFetchOneMediterranean from '@/hooks/useFetchOneMediterranean'
 import React from 'react'
@@ -16,9 +17,12 @@ function PatientId({
    
   return (
     <main>
-        <section className='flex flex-col gap-2'>
-            {bmiData?<PatientBmiForm name={bmiData?.name} lastName={bmiData?.lastName} age={bmiData?.age} gender={bmiData?.gender} phoneNumber={params.patientId} bmi={bmiData?.bmi} wight={bmiData?.wight} height={bmiData?.height}/>: <h2 className='flex justify-center items-center'>Loading</h2>}
-            <h2 className='text-3xl mt-10'>رژیم‌ها:</h2>
+        <section className='flex flex-col gap-2'>            
+            {bmiData?<> <PatientCard name={bmiData.name} lastName={bmiData.lastName} phoneNumber={bmiData.phoneNumber} gender={bmiData.gender}/>
+            <h2 className='text-3xl mt-8'>فرم BMI:</h2>
+            <PatientBmiForm name={bmiData?.name} lastName={bmiData?.lastName} age={bmiData?.age} gender={bmiData?.gender} phoneNumber={params.patientId} bmi={bmiData?.bmi} wight={bmiData?.wight} height={bmiData?.height}/></>: <h2 className='flex justify-center items-center'>Loading</h2>}
+
+            <h2 className='text-3xl mt-8'>رژیم‌ها:</h2>
             {(medData)?<MediterraneanForm phoneNumber={params.patientId} isChecked={medData?.isChecked}  dailyFruit={medData.dailyFruit} dailyVegetable={medData.dailyVegetable} Cereals={medData.Cereals} dailyCereals={medData.dailyCereals} potatoAndStarchWeekly={medData.potatoAndStarchWeekly} oliveAndOliveOilDaily={medData.oliveAndOliveOilDaily} nutsDaily={medData.nutsDaily} dairyDaily={medData.dairyDaily} beans={medData.beans} eggWeekly={medData.eggWeekly} fishWeekly={medData.fishWeekly} chickensWeekly={medData.chickensWeekly} sugarWeekly={medData.sugarWeekly} alcoholWeekly={medData.alcoholWeekly} fermentationWeekly={medData.fermentationWeekly} supplements={medData.sugarWeekly} physicalActivity={medData.physicalActivity} diabetes={medData.diabetes} anemia={medData.anemia} bloodPressure={medData.bloodPressure} digestiveProblems={medData.digestiveProblems} selfSafety={medData.selfSafety} stroke={medData.stroke} fattyLiver={medData.fattyLiver} kidneyProblems={medData.kidneyProblems} thyroid={medData.thyroid} cancer={medData.cancer} Migraine={medData.Migraine} otherSickness={medData.otherSickness} medicine={medData.medicine}/>: <h2 className='flex justify-center items-center'>Loading</h2>}
         </section>
     </main>
--- server/routes/bmi.js
+++ server/routes/bmi.js
@@ -1,40 +1,43 @@
 const express = require('express');
 const router = express.Router();
-const {validateBmi, BmiForm } = require('../models/bmiForm')
+const { validateBmi, BmiForm } = require('../models/bmiForm');
 
-router.get('/', async(req, res) => {
-    if(BmiForm){
-        const bmiForms = await BmiForm.find().sort(req.params.id)
-        res.send(bmiForms)
+router.get('/', async (req, res) => {
+    if (BmiForm) {
+        const bmiForms = await BmiForm.find().sort(req.params.id);
+        return res.send(bmiForms);
+    } else {
+        return res.send({ "DataBase": "Empty!!!" });
     }
-    else res.send({"DataBase": "Empty!!!"}); 
-})
+});
 
-//This will check for the Users phoneNumber from the frontEnd, and Gives back a Response
-router.get('/:phoneNumber', async(req, res)=>{
+// This will check for the User's phoneNumber from the frontEnd, and Gives back a Response
+router.get('/:phoneNumber', async (req, res) => {
     try {
-        const result = await BmiForm.findOne({phoneNumber: req.params.phoneNumber});
-        if(result){
-            res.status(200).send(result);
+        const result = await BmiForm.findOne({ phoneNumber: req.params.phoneNumber });
+        if (result) {
             console.log(result);
+            return res.status(200).send(result);
         } else {
-            res.status(404).send('Not Found');
+            return res.status(404).send('Not Found');
         }
-    } catch(err) {
+    } catch (err) {
         console.error(err);
-        res.status(500).send('Server Error');
+        return res.status(500).send('Server Error');
     }
-})
+});
 
-
-router.post('/' ,async(req, res) => {
-    const {error} = validateBmi(req.body);
-    if(error){
-        return res.status(400).send(error.details[0].message)
+router.post('/', async (req, res) => {
+    const { error } = validateBmi(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
     }
-    //This Checks if the number is already in db or not
-    let phoneNumberCheck = await BmiForm.findOne({ phoneNumber:req.body.phoneNumber });
-    if (phoneNumberCheck) return res.status(400).send("such phone number is already registered.")
+
+    // This Checks if the number is already in db or not
+    let phoneNumberCheck = await BmiForm.findOne({ phoneNumber: req.body.phoneNumber });
+    if (phoneNumberCheck) {
+        return res.status(400).send("Such phone number is already registered.");
+    }
 
     let form = new BmiForm({
         name: req.body.name,
@@ -44,13 +47,13 @@ router.post('/' ,async(req, res) => {
         age: req.body.age,
         height: req.body.height,
         wight: req.body.wight,
-        bmi: (req.body.wight/(req.body.height * req.body.height)).toFixed(4), //bmi = wight/(height * height)
-    })
+        bmi: (req.body.wight / (req.body.height * req.body.height)).toFixed(4), // bmi = wight/(height * height)
+    });
     form = await form.save();
-    res.send(form);
-})
+    return res.send(form);
+});
 
-router.put('/:phoneNumber', async(req, res) => {
+router.put('/:phoneNumber', async (req, res) => {
     const bmiForm = await BmiForm.findByIdAndUpdate(req.params.phoneNumber, {
         name: req.body.name,
         lastName: req.body.lastName,
@@ -59,14 +62,18 @@ router.put('/:phoneNumber', async(req, res) => {
         age: req.body.age,
         height: req.body.height,
         wight: req.body.wight,
-        bmi: (req.body.wight/(req.body.height * req.body.height)).toFixed(4),
-    }, {new: true})
-    if(!bmiForm) res.status(404).send("Such BMI form was not found.")
-    res.send(bmiForm);
-})
+        bmi: (req.body.wight / (req.body.height * req.body.height)).toFixed(4),
+    }, { new: true });
 
-router.delete('/:phoneNumber',async(req, res) => {
-    const bmiForm = await BmiForm.findByIdAndDelete(req.params.id)
-    res.send(bmiForm);
-})
+    if (!bmiForm) {
+        return res.status(404).send("Such BMI form was not found.");
+    }
+    return res.send(bmiForm);
+});
+
+router.delete('/:phoneNumber', async (req, res) => {
+    const bmiForm = await BmiForm.findByIdAndDelete(req.params.phoneNumber);
+    return res.send(bmiForm);
+});
+
 module.exports = router;
