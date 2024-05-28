3b685c4c6b25bf301cc9d24365367071f8274904
ParsaBordbar <parsab71@gmail.com>
2024-05-28 22:40:57 +0330

    Add doctorComment model in the backend.

---
+++ server/models/doctorComment.js
@@ -0,0 +1,30 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const doctorsComment = mongoose.model('doctors_comment' ,new mongoose.Schema({
+    sender: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+    receiver:{
+        type: mongoose.type.ObjectId,
+        ref: "Bmi_form",
+        required: true,
+    },
+}))
+
+const validateDoctorsComment = (doctorsComment) => {
+    const schema = Joi.object({
+        sender: Joi.string().required(),
+        body: Joi.string().required(),
+        receiver: Joi.string().required(),
+    })
+    return schema.validate(doctorsComment)
+}
+
+exports.doctorsComment = doctorsComment;
+exports.validateDoctorsComment = validateDoctorsComment;
