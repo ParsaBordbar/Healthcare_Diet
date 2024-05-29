ddf44f0694ab793f4216541478f5922a645f620e
MohammadMosavat <42.octmst@gmail.com>
2024-05-29 16:58:53 +0330

    Add hover background color for comments button in admin panel , the green color

--- client/src/components/AdminComponents/ReplyBox/index.tsx
+++ client/src/components/AdminComponents/ReplyBox/index.tsx
@@ -36,7 +36,7 @@ const ReplyBox: React.FC<ReplyBoxProps> = ({ receiver }) => {
           {...register('receiver')}
         />
         
-        <button type="submit" className="bg-[var(--orange)] text-white text-3xl px-4 py-2 rounded-2xl w-1/6 hover:bg-[var(--new-green)] ease-in-out delay-100">
+        <button type="submit" className="bg-[var(--orange)] text-white text-3xl px-4 py-2 rounded-2xl w-1/6 transition-all duration-200 hover:bg-[var(--new-green)] ease-in-out ">
           ثبت
         </button>
       </form>
