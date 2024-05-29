8315d5d56265bd4631d1a7ec485fef52ca47b642
ParsaBordbar <parsab71@gmail.com>
2024-05-30 02:11:37 +0330

    Update useComment custome hook to handle dates. Make a new object with spread oprator, to include dates.

--- client/src/hooks/useComment/useComment.ts
+++ client/src/hooks/useComment/useComment.ts
@@ -5,13 +5,14 @@ import { toast } from 'react-toastify';
 import * as yup from 'yup';
 import api from '@/apis'; 
 import { CommentType } from '@/types';
+import moment from 'moment';
+import momentJalaali from 'moment-jalaali';
 
 const CommentSchema = yup.object().shape({
   sender: yup.string().required('Sender is required'),
   body: yup.string().required('لطفا متن پیام را اضافه کنید'),
   receiver: yup.string().required('Receiver is required'),
 });
-
 
 const useComment = () => {
   const {
@@ -26,11 +27,19 @@ const useComment = () => {
 
   const handleValueInputs: SubmitHandler<CommentType> = useCallback(async (data) => {
     try {
-      const response = await api.post('/doctorsComment', data);
+      const createdAtGregorian = moment().toISOString();
+      const createdAtJalali = momentJalaali().format('jYYYY/jM/jD HH:mm:ss');
+
+      const response = await api.post('/doctorsComment', {
+        ...data,
+        createdAtGregorian,
+        createdAtJalali,
+      });
+
       toast.success('پیام با موفقیت ارسال شد');
     } catch (error) {
       console.error('مشکلی در ارسال پیام پیش آمد', error);
-      toast.error('An error occurred while submitting the comment');
+      toast.error('مشکلی در ارسال پیام پیش آمد');
     }
   }, []);
 
