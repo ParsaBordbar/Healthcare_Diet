83008ba1b0658a8877199e6f1057b5fe71eef8f1
ParsaBordbar <parsab71@gmail.com>
2024-05-30 02:13:07 +0330

    Add updated useComment, now data can include date of created, with Time! Dates are both in latin and Persion!

--- client/src/components/AdminComponents/ReplyBox/index.tsx
+++ client/src/components/AdminComponents/ReplyBox/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import useComment from '@/hooks/useComment/useComment'; 
-import SendIcon from '/public/svg/adminPanelSvgs/send-alt-1-svgrepo-com.svg'
-import AttachmentIcon from '/public/svg/adminPanelSvgs/attachment-svgrepo-com.svg'
+import SendIcon from '/public/svg/adminPanelSvgs/send-alt-1-svgrepo-com.svg';
+import AttachmentIcon from '/public/svg/adminPanelSvgs/attachment-svgrepo-com.svg';
 
 interface ReplyBoxProps {
   receiver: string;
@@ -21,13 +21,14 @@ const ReplyBox: React.FC<ReplyBoxProps> = ({ receiver }) => {
     <div className="flex flex-col bg-[var(--primary)] my-10 rounded-lg text-lg shadow-md justify-center">
       <h2 className="text-3xl px-20 pt-8 text-[var(--new-green)]">ثبت پیام جدید</h2>
       <form className="flex w-full flex-col m-3 gap-4 p-10" onSubmit={handleSubmit(handleValueInputs)}>
+        {errors.body && <p className="text-red-500">{errors.body.message}</p>}
+
         <textarea
           placeholder="پیام خود را وارد کنید..."
           className={`bg-[var(--milky-white)] border ${errors.body ? 'border-red-500' : 'border-var(--rating-color)'} px-6 py-5 rounded-xl w-full`}
           rows={10}
           {...register('body')}
         />
-        {errors.body && <p className="text-red-500">{errors.body.message}</p>}
         
         <input
           type="hidden"
