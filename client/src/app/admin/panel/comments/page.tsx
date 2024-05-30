b950ff48750e9223185bc4a3ba03a2f01bdb064f
ParsaBordbar <parsab71@gmail.com>
2024-05-30 21:53:54 +0330

    Add new icon and styles to comments in admin panel.

---
+++ client/src/app/admin/panel/comments/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import CommentBox from '@/components/AdminComponents/CommentBox'
+import MainInput from '@/components/MainInput'
+import useFetchComments from '@/hooks/useFetchComments/useFetchComments'
+
+
+const CommentsPage = () => {
+  const allComments = useFetchComments()
+
+  console.log(allComments);
+  return (
+    <main>
+        <div className='flex gap-3 flex-col'>
+            <div className='bg-red'>
+                <MainInput parentClassName="!w-1/2 mx-auto" type="search" placeholder="نام بیمار مورد نظر خود را وارد کنید"/>
+            </div>
+                {allComments? allComments.map((data): React.ReactNode => {
+                    return <CommentBox key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali}/>
+                }): null
+                }
+        </div>
+    </main>
+  )
+}
+
+export default CommentsPage
