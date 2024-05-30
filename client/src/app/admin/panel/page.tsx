30ae3b44340e1efb6ef98b0134bbe6f6f3b365e2
ParsaBordbar <parsab71@gmail.com>
2024-05-30 18:30:40 +0330

    Add new sections for comments.

--- client/src/app/admin/panel/page.tsx
+++ client/src/app/admin/panel/page.tsx
@@ -1,3 +1,4 @@
+import NewCommentsBox from "@/components/AdminComponents/NewCommentsBox "
 import NewPatientsBox from "@/components/AdminComponents/NewPatientsBox"
 import WelcomeBox from "@/components/AdminComponents/WelcomeBox"
 import Link from "next/link"
@@ -6,10 +7,14 @@ const MainPageAdminPanel = () => {
     return(
         <section className="grid grid-cols-7 gap-4">
             <div className="col-span-7">
-                <WelcomeBox doctorName="X"/>    
+                <WelcomeBox doctorName="نام "/>    
             </div>
             <div className="col-span-7">
                 <NewPatientsBox />
+            </div>
+            <div className="col-span-7">
+                <NewCommentsBox />
+                <Link href={""}> دیدن‌ همه‌ی پیام‌ها</Link>
             </div>
         </section>
     )   
