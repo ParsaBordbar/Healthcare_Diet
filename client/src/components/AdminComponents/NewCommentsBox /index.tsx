9f74b9fad89c8ed4b98e63b4b93b80b199db7de3
ParsaBordbar <parsab71@gmail.com>
2024-05-30 21:52:06 +0330

    Add new icon and fetch to  NewCommentsBox.

--- client/src/components/AdminComponents/NewCommentsBox /index.tsx
+++ client/src/components/AdminComponents/NewCommentsBox /index.tsx
@@ -1,9 +1,21 @@
+'use client'
+import useFetchComments from '@/hooks/useFetchComments/useFetchComments'
+import Link from 'next/link'
 import React from 'react'
+import CommentBox from '../CommentBox'
 
 function NewCommentsBox () {
+
+  const allComments = useFetchComments()
+
   return (
-    <div>
-        <h2>جدیدترین پیام‌ها</h2>
+    <div className='my-3 flex justify-center flex-col mx-1'>
+        <h2 className="text-lg px-10">جدیدترین پیام‌ها</h2>
+                {allComments? allComments.slice(-3).map((data): React.ReactNode => {
+                    return <CommentBox key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali}/>
+                }): null
+                }
+        <Link className="pt-3 mx-2 text-lg px-10 text-blue-600 " href={"/admin/panel/comments"}> دیدن‌ همه‌ی پیام‌ها</Link>
     </div>
   )
 }
