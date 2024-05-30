f9fb2c8ce1a373b484ea50d7362118ea48ac430b
MohammadMosavat <42.octmst@gmail.com>
2024-05-30 12:36:43 +0330

    Add col-span-7 to parent of new patieints box to shows well it

--- client/src/components/AdminComponents/NewPatientsBox/index.tsx
+++ client/src/components/AdminComponents/NewPatientsBox/index.tsx
@@ -1,26 +1,34 @@
-'use client'
-import Link from 'next/link';
-import useFetchBmi from '../../../hooks/useFetchBmi'
-import PatientBox from '../PatientBox';
+"use client";
+import Link from "next/link";
+import useFetchBmi from "../../../hooks/useFetchBmi";
+import PatientBox from "../PatientBox";
+import PatientCard from "../PatientCard";
 
 function NewPatientsBox() {
-  const  bmiForms = useFetchBmi();    
+  const bmiForms = useFetchBmi();
   return (
-    <section className='flex flex-col items-start rounded-2xl my-4'>
-      <h2 className='text-lg'>جدیدترین بیمارها</h2>
-        {bmiForms.slice(-3).map((form) => {
-            return <PatientBox
-            name={form.name}
-            lastName={form.lastName}
-            phoneNumber={form.phoneNumber}
-            key={form.phoneNumber}
-            bmi=''
-          />
-          })
-          }
-      <Link className='pt-3 text-lg text-blue-600 ' href={'./panel/patients'}>دیدن همه بیمارها</Link>
+    <section className="flex flex-col items-start rounded-2xl my-4">
+      <h2 className="text-lg">جدیدترین بیمارها</h2>
+      <main className="grid grid-cols-2 w-full justify-between">
+        {bmiForms.slice(-4).map((form) => {
+          return (
+            <div className="col-span-1 " key={form.phoneNumber}>
+              <PatientCard
+                linkTo={`/admin/panel/patients/${form.phoneNumber}`}
+                name={form.name}
+                lastName={form.lastName}
+                phoneNumber={form.phoneNumber}
+                gender={form.gender}
+              />
+            </div>
+          );
+        })}
+      </main>
+      <Link className="pt-3 text-lg text-blue-600 " href={"./panel/patients"}>
+        دیدن همه بیمارها
+      </Link>
     </section>
-  )
+  );
 }
 
-export default NewPatientsBox
+export default NewPatientsBox;
