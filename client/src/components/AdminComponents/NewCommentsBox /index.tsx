24dfe0025b01bad2cee96c0fc4738ce6f9c90665
ParsaBordbar <parsab71@gmail.com>
2024-05-30 22:34:31 +0330

    Change style to show new element of NewComment Higher.

--- client/src/components/AdminComponents/NewCommentsBox /index.tsx
+++ client/src/components/AdminComponents/NewCommentsBox /index.tsx
@@ -9,12 +9,14 @@ function NewCommentsBox () {
   const allComments = useFetchComments()
 
   return (
-    <div className='my-3 flex justify-center flex-col mx-1'>
+    <div className='my-3 flex justify-center flex-col mx-1 '>
         <h2 className="text-lg px-10">جدیدترین پیام‌ها</h2>
-                {allComments? allComments.slice(-3).map((data): React.ReactNode => {
-                    return <CommentBox key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali}/>
-                }): null
-                }
+                <div className='flex flex-col-reverse'>
+                  {allComments? allComments.slice(-3).map((data): React.ReactNode => {
+                      return <CommentBox key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali}/>
+                  }): null
+                  }
+                </div>
         <Link className="pt-3 mx-2 text-lg px-10 text-blue-600 " href={"/admin/panel/comments"}> دیدن‌ همه‌ی پیام‌ها</Link>
     </div>
   )
