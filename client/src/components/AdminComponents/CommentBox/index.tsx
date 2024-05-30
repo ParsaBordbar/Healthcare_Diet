6a7a43f4ffcbcf013ff50226bf04b9d8779e8ec6
MohammadMosavat <42.octmst@gmail.com>
2024-05-31 02:07:37 +0330

    responsive totaly the comment box component

--- client/src/components/AdminComponents/CommentBox/index.tsx
+++ client/src/components/AdminComponents/CommentBox/index.tsx
@@ -1,37 +1,62 @@
-import { CommentType } from '@/types'
-import React from 'react'
-import AttachmentIcon from '/public/svg/adminPanelSvgs/attachment-svgrepo-com.svg';
-import DateSvg from '/public/svg/adminPanelSvgs/date-range-svgrepo-com.svg'
-import Link from 'next/link';
-import useSpecificFetchBmi from '@/hooks/useFetchName/useFetchName';
+import { CommentType } from "@/types";
+import React from "react";
+import AttachmentIcon from "/public/svg/adminPanelSvgs/attachment-svgrepo-com.svg";
+import DateSvg from "/public/svg/adminPanelSvgs/date-range-svgrepo-com.svg";
+import Link from "next/link";
+import useSpecificFetchBmi from "@/hooks/useFetchName/useFetchName";
 
-const CommentBox = ({sender, body, receiver, createdAtJalali}: CommentType) => {
-    
-    const userData = useSpecificFetchBmi(receiver);
-    
-    // Convert newlines to <br> tags
-    const formatBody = (text: string) => {
-        return { __html: text.replace(/\n/g, '<br/>') };
-    }
+const CommentBox = ({
+    className,
+  sender,
+  body,
+  receiver,
+  createdAtJalali,
+}: CommentType) => {
+  const userData = useSpecificFetchBmi(receiver);
+
+  // Convert newlines to <br> tags
+  const formatBody = (text: string) => {
+    return { __html: text.replace(/\n/g, "<br/>") };
+  };
 
   return (
-    <div className='flex flex-col m-3 col-span-full bg-[var(--milky-white)] mx-4 sm:mx-8 gap-2 my-4 rounded-lg shadow-md'>
-        <div className='ps-4 py-3 flex justify-start gap-5 pb-2 bg-[var(--new-green)] text-white rounded-e-2xl w-fit pe-8 items-center'>
-            <div className='flex gap-2 items-center'>
-                <p>ارسال کننده:</p>
-                <p className='text-xl'>{sender} </p>
+    <div className="flex flex-col ml-3 col-span-full bg-[var(--milky-white)]  gap-2 rounded-lg shadow-md">
+      <div className="ps-4 py-3  flex items-start  min-[1320px]:gap-5 gap-2 pb-2 bg-[var(--new-green)] text-white rounded-lg rounded-br-none justify-between pe-4">
+        <section className="flex items-start flex-col gap-2">
+          <div className="flex gap-2 items-center">
+            <p>ارسال کننده:</p>
+            <p className="text-xl text-ellipsis overflow-hidden whitespace-nowrap">{sender} </p>
+          </div>
+          {receiver ? (
+            <div className="flex gap-2 items-center">
+              <p className="w-fit">دریافت کننده:</p>
+              <p className="text-lg text-ellipsis overflow-hidden whitespace-nowrap">
+                {userData?.name} {userData?.lastName}{" "}
+              </p>
             </div>
-            {receiver? <div className='flex gap-2 items-center'>
-                <p>دریافت کننده:</p>
-                <p className='text-lg'>{userData?.name} {userData?.lastName} </p>
-            </div>: null}
-            <div className='flex gap-2 justify-center items-center flex-row'> <DateSvg />تاریخ ارسال:{" "} {createdAtJalali} </div>
-            
-            <Link href={`/admin/panel/patients/${receiver}`}><AttachmentIcon width={24}/></Link>
-        </div>
-        <p className='p-5 w-full pb-12' dangerouslySetInnerHTML={formatBody(body)} />
+          ) : null}
+          <div className="flex gap-2 justify-start min-[1320px]:justify-center items-center flex-row">
+            {" "}
+            <DateSvg />
+            {createdAtJalali}
+          </div>
+        </section>
+        <Link href={`/admin/panel/patients/${receiver}`}>
+          <AttachmentIcon width={24} />
+        </Link>
+      </div>
+      <Link
+        className="min-[1320px]:hidden inline-block"
+        href={`/admin/panel/patients/${receiver}`}
+      >
+        <AttachmentIcon width={24} />
+      </Link>
+      <p
+        className="p-5 w-full pb-12"
+        dangerouslySetInnerHTML={formatBody(body)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default CommentBox
+export default CommentBox;
