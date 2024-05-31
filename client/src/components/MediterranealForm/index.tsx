3ae1b75274d69b52bb085d0396192dd9f7f5e5f4
MohammadMosavat <42.octmst@gmail.com>
2024-05-31 16:28:13 +0330

    handle errors and shows them and set new schema of yup to get errors

--- client/src/components/MediterranealForm/index.tsx
+++ client/src/components/MediterranealForm/index.tsx
@@ -6,33 +6,38 @@ import GroupRadio from "../GroupRadio";
 import MainButton from "../MainButton";
 
 const MediterranealForm = () => {
-  const schema = yup.object().shape({
-    // Add other fields as needed
-    selectedOption: yup.string().required("Please select an option."),
-  });
-
   const MediterrealnSchema = yup.object().shape({
     dailyFruit: yup.string().required("Please select an option."),
-    questionOne: yup.string().required("جواب به این سوال الزامی است"),
-    questionTwo: yup.string().required("جواب به این سوال الزامی است"),
-    questionThree: yup.string().required("جواب به این سوال الزامی است"),
-    questionFour: yup.string().required("جواب به این سوال الزامی است"),
-    questionFive: yup.string().required("جواب به این سوال الزامی است"),
-    questionSix: yup.string().required("جواب به این سوال الزامی است"),
-    questionSeven: yup.string().required("جواب به این سوال الزامی است"),
-    questionEight: yup.string().required("جواب به این سوال الزامی است"),
-    questionNine: yup.string().required("جواب به این سوال الزامی است"),
-    questionTen: yup.string().required("جواب به این سوال الزامی است"),
-    questionTwelve: yup.string().required("جواب به این سوال الزامی است"),
-    questionThirteen: yup.string().required("جواب به این سوال الزامی است"),
-    questionFourteen: yup.string().required("جواب به این سوال الزامی است"),
-    questionFifteen: yup.string().required("جواب به این سوال الزامی است"),
-    questionSixteen: yup.string().required("جواب به این سوال الزامی است"),
-    questionSeventeen: yup.string().required("جواب به این سوال الزامی است"),
-    questionEighteen: yup.string().required("جواب به این سوال الزامی است"),
-    questionNineteen: yup.string().required("جواب به این سوال الزامی است"),
-    questionTwenty: yup.string().required("جواب به این سوال الزامی است"),
-    questionTwentyOne: yup.string().required("جواب به این سوال الزامی است"),
+    dailyVegetable: yup.string().required("جواب به این سوال الزامی است"),
+    vegetables: yup.string().required("جواب به این سوال الزامی است"),
+    dailyCereals: yup.string().required("جواب به این سوال الزامی است"),
+    potatoAndStarchWeekly: yup.string().required("جواب به این سوال الزامی است"),
+    oliveAndOliveOilDaily: yup.string().required("جواب به این سوال الزامی است"),
+    nutsDaily: yup.string().required("جواب به این سوال الزامی است"),
+    dairyDaily: yup.string().required("جواب به این سوال الزامی است"),
+    beans: yup.string().required("جواب به این سوال الزامی است"),
+    eggWeekly: yup.string().required("جواب به این سوال الزامی است"),
+    fishWeekly: yup.string().required("جواب به این سوال الزامی است"),
+    chickensWeekly: yup.string().required("جواب به این سوال الزامی است"),
+    redMeatWeekly: yup.string().required("جواب به این سوال الزامی است"),
+    sugarWeekly: yup.string().required("جواب به این سوال الزامی است"),
+    alcoholWeekly: yup.string().required("جواب به این سوال الزامی است"),
+    fermentationWeekly: yup.string().required("جواب به این سوال الزامی است"),
+    physicalActivity: yup.string().required("جواب به این سوال الزامی است"),
+    diabetes: yup.string().required("جواب به این سوال الزامی است"),
+    anemia: yup.string().required("جواب به این سوال الزامی است"),
+    bloodPressure: yup.string().required("جواب به این سوال الزامی است"),
+    digestiveProblems: yup.string().required("جواب به این سوال الزامی است"),
+    selfSafety: yup.string().required("جواب به این سوال الزامی است"),
+    stroke: yup.string().required("جواب به این سوال الزامی است"),
+    fattyLiver: yup.string().required("جواب به این سوال الزامی است"),
+    kidneyProblems: yup.string().required("جواب به این سوال الزامی است"),
+    thyroid: yup.string().required("جواب به این سوال الزامی است"),
+    cancer: yup.string().required("جواب به این سوال الزامی است"),
+    supplements: yup.string().required("جواب به این سوال الزامی است"),
+    Migraine: yup.string().required("جواب به این سوال الزامی است"),
+    otherSickness: yup.string().required("جواب به این سوال الزامی است"),
+    medicine: yup.string().required("جواب به این سوال الزامی است"),
   });
 
   const formik = useFormik({
@@ -81,6 +86,7 @@ const MediterranealForm = () => {
       questionTwenty: "کدام یک از مشکلات زیر را دارید یا قبلا داشتید؟",
       questionTwentyOne: "سایر بیماری ها یا جراحی قبلی را اینجا بنویسید .",
       anemia: "",
+      supplements:'',
       bloodPressure: "",
       digestiveProblems: "",
       selfSafety: "",
@@ -92,7 +98,7 @@ const MediterranealForm = () => {
       Migraine: "",
       otherSickness: "",
       medicine: "",
-      phoneNumber:''
+      phoneNumber: "",
     },
     validationSchema: MediterrealnSchema,
     onSubmit: async (data) => {
@@ -103,19 +109,14 @@ const MediterranealForm = () => {
         data.selectedOptionSeventeenThree,
         data.selectedOptionSeventeenFour
       );
-      Object.assign(data , {supplements:selectedOptionSeventeen})
+      Object.assign(data, { supplements: selectedOptionSeventeen });
       // Handle form submission (e.g., make a request to your backend)
-      console.log("Form submitted successfully! Selected option:", data);
-      console.log('this is answer of question severnteen ' , selectedOptionSeventeen)
-      
+      console.log(data);
     },
   });
 
   const { errors, touched, values, handleChange, handleSubmit } = formik;
-  console.log(values);
-  console.log(values.selectedOptionSeventeenFour, "this is log");
-  console.log(errors)
-
+  console.log(errors);
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-10">
       <div className="flex flex-col gap-4">
@@ -203,7 +204,7 @@ const MediterranealForm = () => {
             header
           />
         </section>
-        {errors.questionTwo ? (
+        {errors.dailyVegetable ? (
           <p className="text-red-600">جواب دادن به سوال بالا الزامی است</p>
         ) : null}
       </div>
@@ -845,7 +846,6 @@ const MediterranealForm = () => {
             }
             onChange={handleChange}
             onClick={() => {
-              console.log(values.selectedOptionSeventeenFour, "this is log");
               values.selectedOptionSeventeenFour == "هیچکدام";
               values.selectedOptionSeventeenOne = "null";
               values.selectedOptionSeventeenTwo = "null";
@@ -856,9 +856,9 @@ const MediterranealForm = () => {
             header
           />
         </section>
-        {/* {errors.questionOne ? (
+        {errors.supplements ? (
           <p className="text-red-600">جواب دادن به سوال بالا الزامی است</p>
-        ) : null} */}
+        ) : null}
       </div>
 
       <div className="flex flex-col gap-4">
@@ -912,10 +912,7 @@ const MediterranealForm = () => {
             className={`col-span-full sm:col-span-3 lg:col-span-2 rounded-lg w-full outline-none   py-3 px-4 border   flex items-center gap-1  bg-white `}
           >
             <option
-              selected={
-                values.diabetes === "null" ||
-                values.diabetes === ""
-              }
+              selected={values.diabetes === "null" || values.diabetes === ""}
               value="null"
             >
               دیابت
@@ -938,10 +935,7 @@ const MediterranealForm = () => {
             >
               دیابت بارداری
             </option>
-            <option
-              selected={values.diabetes === "ندارم"}
-              value="ندارم"
-            >
+            <option selected={values.diabetes === "ندارم"} value="ندارم">
               ندارم
             </option>
           </select>
@@ -951,10 +945,7 @@ const MediterranealForm = () => {
             name="anemia"
             className={`col-span-full sm:col-span-3 lg:col-span-2 rounded-lg w-full outline-none   py-3 px-4 border   flex items-center gap-1  bg-white `}
           >
-            <option
-              selected={values.anemia === "null"}
-              value="null"
-            >
+            <option selected={values.anemia === "null"} value="null">
               فشار خون
             </option>
             <option
@@ -969,10 +960,7 @@ const MediterranealForm = () => {
             >
               پایین بودن فشار خون
             </option>
-            <option
-              selected={values.anemia === "ندارم"}
-              value="ندارم"
-            >
+            <option selected={values.anemia === "ندارم"} value="ندارم">
               ندارم
             </option>
           </select>
@@ -982,10 +970,7 @@ const MediterranealForm = () => {
             name="bloodPressure"
             className={`col-span-full sm:col-span-3 lg:col-span-2 rounded-lg w-full outline-none   py-3 px-4 border   flex items-center gap-1  bg-white `}
           >
-            <option
-              selected={values.bloodPressure === "null"}
-              value="null"
-            >
+            <option selected={values.bloodPressure === "null"} value="null">
               مشکلات گوارشی
             </option>
             <option
@@ -994,22 +979,13 @@ const MediterranealForm = () => {
             >
               ریفالکس{"(ترش کردن)"}
             </option>
-            <option
-              selected={values.bloodPressure === "نفخ"}
-              value="نفخ"
-            >
+            <option selected={values.bloodPressure === "نفخ"} value="نفخ">
               نفخ
             </option>
-            <option
-              selected={values.bloodPressure === "یبوست"}
-              value="یبوست"
-            >
+            <option selected={values.bloodPressure === "یبوست"} value="یبوست">
               یبوست
             </option>
-            <option
-              selected={values.bloodPressure === "ندارم"}
-              value="ندارم"
-            >
+            <option selected={values.bloodPressure === "ندارم"} value="ندارم">
               ندارم
             </option>
           </select>
@@ -1019,10 +995,7 @@ const MediterranealForm = () => {
             name="digestiveProblems"
             className={`col-span-full sm:col-span-3 lg:col-span-2 rounded-lg w-full outline-none   py-3 px-4 border   flex items-center gap-1  bg-white `}
           >
-            <option
-              selected={values.digestiveProblems === "null"}
-              value="null"
-            >
+            <option selected={values.digestiveProblems === "null"} value="null">
               خودایمنی
             </option>
             <option
@@ -1050,9 +1023,7 @@ const MediterranealForm = () => {
               آرتریت روماتوئید
             </option>
             <option
-              selected={
-                values.digestiveProblems === "سایر بیماری های خودایمنی"
-              }
+              selected={values.digestiveProblems === "سایر بیماری های خودایمنی"}
               value="سایر بیماری های خودایمنی"
             >
               سایر بیماری های خودایمنی
@@ -1070,10 +1041,7 @@ const MediterranealForm = () => {
             name="selfSafety"
             className={`col-span-full sm:col-span-3 lg:col-span-2 rounded-lg w-full outline-none   py-3 px-4 border   flex items-center gap-1  bg-white `}
           >
-            <option
-              selected={values.selfSafety === "null"}
-              value="null"
-            >
+            <option selected={values.selfSafety === "null"} value="null">
               سکته
             </option>
             <option
@@ -1088,10 +1056,7 @@ const MediterranealForm = () => {
             >
               سکته مغزی
             </option>
-            <option
-              selected={values.selfSafety === "ندارم"}
-              value="ندارم"
-            >
+            <option selected={values.selfSafety === "ندارم"} value="ندارم">
               ندارم
             </option>
           </select>
@@ -1101,10 +1066,7 @@ const MediterranealForm = () => {
             name="stroke"
             className={`col-span-full sm:col-span-3 lg:col-span-2 rounded-lg w-full outline-none   py-3 px-4 border   flex items-center gap-1  bg-white `}
           >
-            <option
-              selected={values.stroke === "null"}
-              value="null"
-            >
+            <option selected={values.stroke === "null"} value="null">
               کبد چرب
             </option>
             <option
@@ -1125,10 +1087,7 @@ const MediterranealForm = () => {
             >
               کبد چرب گرید ۳
             </option>
-            <option
-              selected={values.stroke === "ندارم"}
-              value="ندارم"
-            >
+            <option selected={values.stroke === "ندارم"} value="ندارم">
               ندارم
             </option>
           </select>
@@ -1138,10 +1097,7 @@ const MediterranealForm = () => {
             name="fattyLiver"
             className={`col-span-full sm:col-span-3 lg:col-span-2 rounded-lg w-full outline-none   py-3 px-4 border   flex items-center gap-1  bg-white `}
           >
-            <option
-              selected={values.fattyLiver === "null"}
-              value="null"
-            >
+            <option selected={values.fattyLiver === "null"} value="null">
               مشکلات کلیوی
             </option>
             <option
@@ -1157,9 +1113,7 @@ const MediterranealForm = () => {
               نارسایی کلیه
             </option>
             <option
-              selected={
-                values.fattyLiver === "عفونت مجاری ادرار "
-              }
+              selected={values.fattyLiver === "عفونت مجاری ادرار "}
               value="عفونت مجاری ادرار "
             >
               عفونت مجاری ادرار
@@ -1171,10 +1125,7 @@ const MediterranealForm = () => {
             name="kidneyProblems"
             className={`col-span-full sm:col-span-3 lg:col-span-2 rounded-lg w-full outline-none   py-3 px-4 border   flex items-center gap-1  bg-white `}
           >
-            <option
-              selected={values.kidneyProblems === "null"}
-              value="null"
-            >
+            <option selected={values.kidneyProblems === "null"} value="null">
               تیروئید
             </option>
             <option
@@ -1183,24 +1134,16 @@ const MediterranealForm = () => {
             >
               کم کار
             </option>
-            <option
-              selected={values.kidneyProblems === "پرکار"}
-              value="پرکار"
-            >
+            <option selected={values.kidneyProblems === "پرکار"} value="پرکار">
               پرکار
             </option>
             <option
-              selected={
-                values.kidneyProblems === "گره و التهاب تیروئید"
-              }
+              selected={values.kidneyProblems === "گره و التهاب تیروئید"}
               value="گره و التهاب تیروئید"
             >
               گره و التهاب تیروئید
             </option>
-            <option
-              selected={values.kidneyProblems === "ندارم"}
-              value="ندارم"
-            >
+            <option selected={values.kidneyProblems === "ندارم"} value="ندارم">
               ندارم
             </option>
           </select>
@@ -1251,7 +1194,7 @@ const MediterranealForm = () => {
           حتما این فایل را دانلود کنید
         </a> */}
 
-        {errors.questionTwenty ? (
+        {errors.digestiveProblems ? (
           <p className="text-red-600">جواب دادن به سوال بالا الزامی است</p>
         ) : null}
       </div>
