0cf187fdf8234c750f8ba50993af8342a7da109b
MohammadMosavat <42.octmst@gmail.com>
2024-05-31 16:11:18 +0330

    responsive the main page of user panel

--- client/src/app/user/[user]/panel/(main)/page.tsx
+++ client/src/app/user/[user]/panel/(main)/page.tsx
@@ -1,8 +1,8 @@
 // app/user/[user]/panel/main.tsx
-"use client"
-import CommentBox from '@/components/AdminComponents/CommentBox';
-import useFetchPatientComments from '@/hooks/useFetchPatientComments/useFetchPatientComments';
-import { FC } from 'react';
+"use client";
+import CommentBox from "@/components/AdminComponents/CommentBox";
+import useFetchPatientComments from "@/hooks/useFetchPatientComments/useFetchPatientComments";
+import { FC } from "react";
 
 interface UserPanelProps {
   params: {
@@ -13,13 +13,25 @@ interface UserPanelProps {
 const UserPanel: FC<UserPanelProps> = ({ params }) => {
   const userID = params.user;
   console.log(userID);
-  const comments = useFetchPatientComments(userID)
+  const comments = useFetchPatientComments(userID);
 
   return (
     <section>
       <h1>پیام‌های شما:</h1>
-      {comments ? comments.map((comment) => 
-        <CommentBox key={comment.receiver} sender={comment.sender} body={comment.body} receiver={comment.receiver} createdAtJalali={comment.createdAtJalali} /> ) :null}
+      <main className="grid grid-cols-2 ">
+        {comments
+          ? comments.map((comment) => (
+              <CommentBox
+              className="md:col-span-1 col-span-2"
+                key={comment.receiver}
+                sender={comment.sender}
+                body={comment.body}
+                receiver={comment.receiver}
+                createdAtJalali={comment.createdAtJalali}
+              />
+            ))
+          : null}
+      </main>
     </section>
   );
 };
