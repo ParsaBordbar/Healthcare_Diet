46e71c6289c3d2a5dc1927c1ccf95162ed191707
ParsaBordbar <parsab71@gmail.com>
2024-06-01 17:09:52 +0330

    Add new style and sizes for PatientCardLazy.

--- client/src/components/AdminComponents/lazyLoadingComponents/PatientCardLazy/index.tsx
+++ client/src/components/AdminComponents/lazyLoadingComponents/PatientCardLazy/index.tsx
@@ -4,7 +4,7 @@ const PatientCardLazy = () => {
     
         <div className="flex flex-col gap-4 w-full">
           <div className="flex justify-start items-center pt-2.5">
-          <div className="bg-white rounded-full p-12"></div>
+          <div className="bg-white rounded-full p-10"></div>
           <div className="flex flex-col w-full gap-7">
             <h2 className="bg-gray-200 w-1/6 h-4 mx-10 mt-1"></h2>
             <div className="bg-gray-200 w-1/12 h-3 mx-10 mt-1"></div>
