71f22ca2b7b20b8a3c8c5c347474589e78a89418
ParsaBordbar <parsab71@gmail.com>
2024-06-04 20:09:21 +0330

    Add new Fetch data and conditional rendering to the page.

--- client/src/app/admin/panel/comments/page.tsx
+++ client/src/app/admin/panel/comments/page.tsx
@@ -4,24 +4,49 @@ import MainInput from '@/components/MainInput'
 import useFetchComments from '@/hooks/useFetchComments/useFetchComments'
 import useFetchPatientComments from '@/hooks/useFetchPatientComments/useFetchPatientComments'
 import SearchIcon from "/public/svg/search-normal.svg";
+import { useEffect, useState } from 'react'
+import useFilterComments from '@/hooks/useFilterComments/useFilterComments'
+import { date } from 'yup'
 
 
 const CommentsPage = () => {
+
   const allComments = useFetchComments()
-  // const handleSubmit = () => {
-  //   useFetchPatientComments() //Pass the input value in here.
-  // }
+  const [filter, setFilter] = useState(allComments)
+  const oldest = useFilterComments('/doctorsComment/comments?sort=newest')
+  const newest = useFilterComments('/doctorsComment/comments?sort=oldest')
+  
+  const newestFilterHandler = () => {
+    setFilter(newest)
+  }
+
+  const oldestFilterHandler = () => {
+    setFilter(oldest)
+  }
+
+  useEffect(() => {
+    setFilter(allComments);
+  }, [allComments]);
 
   return (
     <section className='flex gap-3 flex-col'>
-      <MainInput
+      <header className='flex gap-10 mb-10'>
+        <MainInput
         iconFirst={SearchIcon}
         parentClassName="!w-1/2 "
         type="search"
         placeholder="نام بیمار مورد نظر خود را وارد کنید"
-      />
-      {allComments? allComments.map((data): React.ReactNode => {
-          return <CommentBox key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali} isDoctor={true}/>
+        />
+        <p className='ps-10'>فیلتر کردن براساس:</p>
+        <button onClick={newestFilterHandler} className='bg-[var(--orange)] rounded-lg px-4 hover:bg-[var(--new-green)] ease-in-out duration-100 hover:text-white text-sm'>
+          قدیمی‌ترین  
+        </button>
+        <button onClick={oldestFilterHandler} className='bg-[var(--orange)] rounded-lg px-4 hover:bg-[var(--new-green)] ease-in-out duration-100 hover:text-white text-sm'>
+          جدیدترین
+        </button>
+      </header>
+      {filter? filter.map((data): React.ReactNode => {
+          return <CommentBox key={data.receiver+data.createdAtJalali} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali} isDoctor={true}/>
       }): null
       }
       </section>
