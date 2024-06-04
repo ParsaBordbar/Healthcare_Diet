07158208cb6f24ba0d56f1bb315b68c0c4667e39
ParsaBordbar <parsab71@gmail.com>
2024-06-04 12:35:58 +0330

    Fetch counts for Admin pabel QuickLinkBox in QuickLinkSection.

--- client/src/components/AdminComponents/QuickLinkSection/index.tsx
+++ client/src/components/AdminComponents/QuickLinkSection/index.tsx
@@ -1,20 +1,32 @@
+'use client'
 import React from 'react'
 import QuickLinkBox from '../QuickLinkBox'
+import useFetchComments from '@/hooks/useFetchComments/useFetchComments'
+import useFetchMediterranean from '@/hooks/useFetchMediterranean'
+import useFetchBmi from '@/hooks/useFetchBmi'
 
 function QuickLinSection() {
+   const allComments = useFetchComments()
+   const commentCount = allComments.length
+
+   const mediterraneanForms = useFetchMediterranean('/checking/isChecked');
+   const unvisitedMediterraneanFormsCount = mediterraneanForms.length
+   const bmiForms = useFetchBmi();
+   const bmiCount = bmiForms.length
+
   return (
     <ul className='flex justify-center items-center gap-20'>
         <li>
-            <QuickLinkBox tittle={'پیام‌ها'} desc={'لیست تمامی پیام‌های ارسالی'} url={"/admin/panel/comments"} counter={'6'} />
+            <QuickLinkBox tittle={'پیام‌ها'} desc={'لیست تمامی پیام‌های ارسالی'} url={"/admin/panel/comments"} counter={commentCount} />
         </li>
         <li>
-            <QuickLinkBox tittle={' ویزیت نشده'} desc={'لیست رژیم‌های ویزیت نشده '} url={"/admin/panel/comments"} counter={''} />
+            <QuickLinkBox tittle={' ویزیت نشده'} desc={'لیست رژیم‌های ویزیت نشده '} url={"/admin/panel/comments"} counter={unvisitedMediterraneanFormsCount} />
         </li>
         <li>
-            <QuickLinkBox tittle={'بیمارها'} desc={'سوابق و مشخصات تمام بیمارها'} url={"/admin/panel/comments"} counter={''} /> 
+            <QuickLinkBox tittle={'بیمارها'} desc={'سوابق و مشخصات تمام بیمارها'} url={"/admin/panel/comments"} counter={bmiCount} /> 
         </li>
         <li>
-            <QuickLinkBox tittle={'فایل‌ها'} desc={'فایل‌های آپلود شده شما'} url={"/admin/panel/comments"} counter={''} />
+            <QuickLinkBox tittle={'فایل‌ها'} desc={'فایل‌های آپلود شده شما'} url={"/admin/panel/comments"} counter={0} />
         </li>
     </ul>
   )
