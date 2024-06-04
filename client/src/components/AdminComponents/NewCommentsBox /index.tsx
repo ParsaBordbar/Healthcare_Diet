4dd282c92a8216eac68ede460788e92408e18b59
MohammadMosavat <42.octmst@gmail.com>
2024-06-04 13:32:49 +0330

    Add width in different size to show better the information of comments in newcomment box components

--- client/src/components/AdminComponents/NewCommentsBox /index.tsx
+++ client/src/components/AdminComponents/NewCommentsBox /index.tsx
@@ -13,7 +13,7 @@ function NewCommentsBox () {
         <h2 className="text-lg mb-6">جدیدترین پیام‌ها</h2>
                 <div className='flex max-h-[590px] gap-3 custom-scroll flex-col-reverse'>
                   {allComments? allComments.slice(-3).map((data): React.ReactNode => {
-                      return <CommentBox className='xl:min-w-[98%] lg:min-w-[75%] lg:w-min w-full' key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali} isDoctor={true}/>
+                      return <CommentBox className=' min-[1320px]:w-[96%] w-11/12' key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali} isDoctor={true}/>
                   }): null
                   }
                 </div>
