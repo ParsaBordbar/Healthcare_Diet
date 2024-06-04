0bfbc724ffe1eb148718a3dc524eb21b549c5b46
MohammadMosavat <42.octmst@gmail.com>
2024-06-04 14:40:43 +0330

    responsive the quicklink box in admin panel

--- client/src/components/AdminComponents/QuickLinkSection/index.tsx
+++ client/src/components/AdminComponents/QuickLinkSection/index.tsx
@@ -15,17 +15,17 @@ function QuickLinSection() {
    const bmiCount = bmiForms.length
 
   return (
-    <ul className='flex justify-center items-center gap-20'>
-        <li>
+    <ul className='grid grid-cols-4 gap-8'>
+        <li className='lg:col-span-1 md:col-span-2 col-span-full' >
             <QuickLinkBox tittle={'پیام‌ها'} desc={'لیست تمامی پیام‌های ارسالی'} url={"/admin/panel/comments"} counter={commentCount} />
         </li>
-        <li>
+        <li className='lg:col-span-1 md:col-span-2 col-span-full' >
             <QuickLinkBox tittle={' ویزیت نشده'} desc={'لیست رژیم‌های ویزیت نشده '} url={"/admin/panel/unvisited_diets"} counter={unvisitedMediterraneanFormsCount} />
         </li>
-        <li>
+        <li className='lg:col-span-1 md:col-span-2 col-span-full' >
             <QuickLinkBox tittle={'بیمارها'} desc={'سوابق و مشخصات تمام بیمارها'} url={"/admin/panel/patients"} counter={bmiCount} /> 
         </li>
-        <li>
+        <li className='lg:col-span-1 md:col-span-2 col-span-full' >
             <QuickLinkBox tittle={'فایل‌ها'} desc={'فایل‌های آپلود شده شما'} url={"/admin/panel/comments"} counter={0} />
         </li>
     </ul>
