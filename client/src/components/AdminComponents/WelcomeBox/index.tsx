10a5959cbb3bcbfd006e2e95be08bea9cfaaf0bb
MohammadMosavat <42.octmst@gmail.com>
2024-06-04 15:21:59 +0330

    Add search bar into welcome box in admin panel

--- client/src/components/AdminComponents/WelcomeBox/index.tsx
+++ client/src/components/AdminComponents/WelcomeBox/index.tsx
@@ -1,12 +1,22 @@
-import { WelcomeBoxType } from '@/types'
-import React from 'react'
-
-function WelcomeBox({doctorName}: WelcomeBoxType) {
+'use client'
+import MainInput from "@/components/MainInput";
+import { WelcomeBoxType } from "@/types";
+import React from "react";
+import SearchIcon from "/public/svg/search-normal.svg";
+function WelcomeBox({ doctorName }: WelcomeBoxType) {
   return (
-    <div className='flex justify-start w-full'>
-        <h1 className='text-[var(--new-green)] text-2xl font-semibold  '>خوش آمدید  مشاور تغذیه {doctorName} </h1>
+    <div className="flex items-center justify-between w-full">
+      <h1 className="text-[var(--new-green)] text-2xl font-semibold  ">
+        خوش آمدید مشاور تغذیه {doctorName}{" "}
+      </h1>
+      <MainInput
+        iconFirst={SearchIcon}
+        parentClassName="!w-1/2 "
+        type="search"
+        placeholder="نام بیمار مورد نظر خود را وارد کنید"
+      />
     </div>
-  )
+  );
 }
 
-export default WelcomeBox
+export default WelcomeBox;
