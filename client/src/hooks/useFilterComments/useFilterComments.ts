083efaf0161ca3b897d769edba1cf51dd9ad1329
ParsaBordbar <parsab71@gmail.com>
2024-06-04 20:09:59 +0330

    Add new custome hook for fetching filrered data.

---
+++ client/src/hooks/useFilterComments/useFilterComments.ts
@@ -0,0 +1,30 @@
+import api from "@/apis";
+import { BmiType, CommentType } from "@/types";
+import { useState, useEffect } from "react";
+
+const useFilterComments = (endpoint: string) => {
+
+    const [data, setData] = useState<CommentType[]>([]);
+    
+    useEffect(()=>{
+        fetchBmiPatientForms()
+    }, [])
+  
+    const fetchBmiPatientForms = async () => {
+      try{
+        console.log(data);
+          const response = await api.get(endpoint);
+          setData(response.data);
+      }
+      catch(err : any){
+         if (err.response){
+          console.log(err.response.data)
+         }
+         else{
+          console.log(`Error: ${err.message}`);
+         }
+      }
+  }
+  return data;
+}
+export default useFilterComments;
