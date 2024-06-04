4cf26bdf5e10ef0f716be5931d08f4f921000416
ParsaBordbar <parsab71@gmail.com>
2024-06-04 12:52:49 +0330

    Delete the layout file for Comments Page, in favor of one page rendering for comments.

--- client/src/app/admin/panel/comments/page.tsx
+++ client/src/app/admin/panel/comments/page.tsx
@@ -7,11 +7,12 @@ import useFetchComments from '@/hooks/useFetchComments/useFetchComments'
 const CommentsPage = () => {
   const allComments = useFetchComments()
   return (
-      <section className='flex gap-3 flex-col'>
-              {allComments? allComments.map((data): React.ReactNode => {
-                  return <CommentBox key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali} isDoctor={true}/>
-              }): null
-              }
+    <section className='flex gap-3 flex-col'>
+      <MainInput parentClassName="!w-1/2 mx-auto mb-10" type="search" placeholder="نام بیمار مورد نظر خود را وارد کنید"/>
+      {allComments? allComments.map((data): React.ReactNode => {
+          return <CommentBox key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali} isDoctor={true}/>
+      }): null
+      }
       </section>
   )
 }
