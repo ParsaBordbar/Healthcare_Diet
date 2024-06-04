7c9be1bcffb33c7b799ab392647046a9e49aec78
MohammadMosavat <42.octmst@gmail.com>
2024-06-04 13:30:36 +0330

    change the layout of informations of comments and responsive it

--- client/src/components/AdminComponents/CommentBox/index.tsx
+++ client/src/components/AdminComponents/CommentBox/index.tsx
@@ -21,24 +21,24 @@ const CommentBox = ({
   };
 
   return (
-    <div className={`flex ${className} flex-col ml-3 col-span-full bg-[var(--milky-white)]  gap-2 rounded-lg shadow-md my-2`}>
-      <div className="ps-4 py-3 flex items-start  min-[1320px]:gap-5 gap-2 pb-2 bg-[var(--new-green)] text-white rounded-lg rounded-br-none justify-between pe-4">
-        <section className="flex items-center xl:flex-row lg:flex-col lg:gap-1 sm:flex-row gap-4 justify-center flex-col">
-          <div className="flex flex-col xl:flex-row xl:gap-2 lg:flex-col">
+    <div className={`flex ${className} flex-col col-span-full bg-[var(--milky-white)]  gap-2 rounded-lg shadow-md`}>
+      <div className="ps-4 py-3 flex items-start  min-[1320px]:gap-5 gap-2 pb-2 bg-[var(--new-green)] text-white rounded-lg rounded-br-none  pe-4">
+        <section className="flex w-full items-start 2xl:items-center 2xl:flex-row lg:flex-col lg:gap-1 sm:flex-row gap-4 2xl:justify-between justify-between lg:justify-center flex-col">
+          <div className="flex flex-col gap-2">
             <div className="flex gap-2 items-center">
-              <p className="md:text-[1rem] 2xl:text-base text-xs">ارسال کننده:</p>
-              <p className="md:text-[1rem] 2xl:text-base text-xs text-ellipsis overflow-hidden whitespace-nowrap">{sender} </p>
+              <p className="md:text-sm 2xl:text-base text-xs">ارسال کننده:</p>
+              <p className="md:text-sm 2xl:text-base text-xs text-ellipsis overflow-hidden whitespace-nowrap">{sender} </p>
             </div>
             {receiver ? (
               <div className="flex gap-2 items-center">
-                <p className="w-fit md:text-[1rem] 2xl:text-base text-xs">دریافت کننده:</p>
-                <p className="md:text-[1rem] 2xl:text-base text-xs text-ellipsis overflow-hidden whitespace-nowrap">
+                <p className="w-fit md:text-sm 2xl:text-base text-xs">دریافت کننده:</p>
+                <p className="md:text-sm 2xl:text-base text-xs text-ellipsis overflow-hidden whitespace-nowrap">
                   {userData?.name} {userData?.lastName}{" "}
                 </p>
               </div>
             ) : null}
           </div>
-          <div className="flex gap-2 px-3 min-[1320px]:justify-center items-center flex-row">
+          <div className="flex gap-2  min-[1320px]:justify-center items-center flex-row">
             {" "}
             <div className="flex sm:flex-row gap-4">
               <div className="flex flex-row justify-center items-center gap-2">
