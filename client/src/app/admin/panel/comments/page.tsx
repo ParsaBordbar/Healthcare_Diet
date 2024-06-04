ce7dc9aab844bb26613d36c6a9b7487bb2b39b9d
ParsaBordbar <parsab71@gmail.com>
2024-06-04 10:54:39 +0330

    Add a layout for comments section.

--- client/src/app/admin/panel/comments/page.tsx
+++ client/src/app/admin/panel/comments/page.tsx
@@ -6,21 +6,13 @@ import useFetchComments from '@/hooks/useFetchComments/useFetchComments'
 
 const CommentsPage = () => {
   const allComments = useFetchComments()
-
-  console.log(allComments);
   return (
-    <main>
-        <div className='flex gap-3 flex-col'>
-            <div className='bg-red'>
-                <MainInput parentClassName="!w-1/2 mx-auto" type="search" placeholder="نام بیمار مورد نظر خود را وارد کنید"/>
-            </div>
-                {allComments? allComments.map((data): React.ReactNode => {
-                    return <CommentBox key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali} isDoctor={true}/>
-                }): null
-                }
-        </div>
-    </main>
+      <section className='flex gap-3 flex-col'>
+              {allComments? allComments.map((data): React.ReactNode => {
+                  return <CommentBox key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali} isDoctor={true}/>
+              }): null
+              }
+      </section>
   )
 }
-
 export default CommentsPage
