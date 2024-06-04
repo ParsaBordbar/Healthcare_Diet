34ff0c6f089cfbfca0555aba87ae0be3c1af3f96
MohammadMosavat <42.octmst@gmail.com>
2024-06-04 14:40:21 +0330

    change the bg of counter in quicklinkbox and set a layout for it

--- client/src/components/AdminComponents/QuickLinkBox/index.tsx
+++ client/src/components/AdminComponents/QuickLinkBox/index.tsx
@@ -5,15 +5,13 @@ import React from 'react'
 function QuickLinkBox({tittle, desc, url, counter}: QuickLinkBoxType) {
   return (
     <Link href={`${url}`}>
-            <div className="mt-10 flex justify-center flex-col bg-[var(--milky-white)] ps-10 lg:pe-3 lg:pt-4 pb-5 rounded-2xl relative z-0 shadow-md lg:p-2 scale-90 sm:scale-100 sm:m-0 w-full h-40">
-                <div className="flex flex-col gap-1 ">
-                    <h1 className="sm:text-2xl text-lg ps-3 pb-3 sm:px-4 lg:ps-5">{tittle}</h1>
-                    <p className="2xl:text-lg sm:text-base px-2 text-xs pb-3">{desc}</p>
+            <div className=" flex p-4 py-9 flex-col bg-[var(--milky-white)] rounded-lg relative z-0 shadow-md  w-full h-36">
+                <div className="flex flex-col gap-1 h-[-webkit-fill-available] justify-between">
+                    <h1 className="text-xl ">{tittle}</h1>
+                    <p className="text-base ">{desc}</p>
                 </div>
-                <div className="-top-3 -right-2 rounded-full flex justify-center items-center xl:w-14 xl:h-w-14 h-14 w-16 bg-[var(--orange)] text-white z-10 xl:scale-90 lg:scale-75 absolute xl:text-2xl">
-                    <div>
+                <div className="-top-5 -right-5 rounded-full flex justify-center items-center text-lg w-14 h-14  bg-[var(--new-green)] text-white z-10 shadow-md  shadow-green-300 absolute">
                         {counter}
-                    </div>
                 </div> 
             </div>
     </Link>
