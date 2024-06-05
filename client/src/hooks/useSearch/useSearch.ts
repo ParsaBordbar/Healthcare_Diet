7fb52044c2f1eef6a881d8dbe47f632e670e6adc
ParsaBordbar <parsab71@gmail.com>
2024-06-05 11:46:04 +0330

    Add Code from comments page to useSerach for custome hook for searching on comments.

--- client/src/hooks/useSearch/useSearch.ts
+++ client/src/hooks/useSearch/useSearch.ts
@@ -1,4 +1,62 @@
+'use client'
+import { useEffect, useState } from 'react'
+import useFetchComments from '@/hooks/useFetchComments/useFetchComments'
+import useFilterComments from '@/hooks/useFilterComments/useFilterComments'
+import useFetchSingleBmi from '@/hooks/useFetchSingleBmi/useFetchSingleBmi'
+import api from '@/apis'
 
-const useSearch = () => {
-    
+const useSearchCommentsPage = () => {
+  const allComments = useFetchComments()
+  const [filter, setFilter] = useState(allComments)
+  const oldest = useFilterComments('/doctorsComment/comments?sort=newest')
+  const newest = useFilterComments('/doctorsComment/comments?sort=oldest')
+  const [searchValue, setSearchValue] = useState<string>('')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('Event value:', event.target.value)
+    setSearchValue(event.target.value)
+  }
+
+  useEffect(() => {
+    console.log('Search value updated:', searchValue)
+  }, [searchValue])
+
+  const fetchBmi = useFetchSingleBmi(searchValue)
+  const getPatientId = fetchBmi[0]?.phoneNumber
+
+  const newestFilterHandler = () => {
+    setFilter(newest)
+  }
+
+  const oldestFilterHandler = () => {
+    setFilter(oldest)
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const endpoint = `/doctorsComment/certain/patientId/${getPatientId}`
+    console.log('This is endpoint', endpoint)
+
+    try {
+      const response = await api.get(endpoint)
+      setFilter(response.data)
+    } catch (error) {
+      console.error('Error fetching search results:', error)
+    }
+  }
+
+  useEffect(() => {
+    setFilter(allComments)
+  }, [allComments])
+
+  return {
+    filter,
+    searchValue,
+    handleChange,
+    handleSubmit,
+    newestFilterHandler,
+    oldestFilterHandler,
+  }
 }
+
+export default useSearchCommentsPage
