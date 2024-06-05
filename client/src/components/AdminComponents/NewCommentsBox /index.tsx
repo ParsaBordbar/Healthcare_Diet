0f4b92b29e4ca0d9fe9a1a414188f7af05e5d5f3
MohammadMosavat <42.octmst@gmail.com>
2024-06-05 15:57:22 +0330

    set new icon for comment in main panel admin

--- client/src/components/AdminComponents/NewCommentsBox /index.tsx
+++ client/src/components/AdminComponents/NewCommentsBox /index.tsx
@@ -3,14 +3,17 @@ import useFetchComments from '@/hooks/useFetchComments/useFetchComments'
 import Link from 'next/link'
 import React from 'react'
 import CommentBox from '../CommentBox'
-
+import CommentIcon from '/public/svg/adminPanelSvgs/messages.svg'
 function NewCommentsBox () {
 
   const allComments = useFetchComments()
 
   return (
     <div className='my-3 flex justify-center  flex-col '>
-        <h2 className="text-lg mb-6">جدیدترین پیام‌ها</h2>
+      <section className="mb-6 flex items-center gap-2">
+        <CommentIcon className='[&>path]:stroke-black'/>
+        <h2 className="text-lg ">جدیدترین پیام‌ها</h2>
+      </section>
                 <div className='flex max-h-[590px] gap-3 custom-scroll flex-col-reverse'>
                   {allComments? allComments.slice(-3).map((data): React.ReactNode => {
                       return <CommentBox className=' min-[1320px]:w-[96%] w-11/12' key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali} isDoctor={true}/>
