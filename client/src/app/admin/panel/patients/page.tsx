80af22352b3ca85cc2fc9ae0af899eabaf14c9f5
MohammadMosavat <42.octmst@gmail.com>
2024-06-05 16:15:26 +0330

    set patient icon into patients page

--- client/src/app/admin/panel/patients/page.tsx
+++ client/src/app/admin/panel/patients/page.tsx
@@ -3,12 +3,15 @@ import PatientBox from "@/components/AdminComponents/PatientBox"
 import PatientCard from "@/components/AdminComponents/PatientCard"
 import useFetchBmi from "@/hooks/useFetchBmi"
 import Link from "next/link"
-
+import PatientIcon from '/public/svg/adminPanelSvgs/patients.svg'
 const PatientsPage = () => {
     const data = useFetchBmi()
     return(
         <section className="grid gap-6 grid-cols-2"> 
-            <h1 className="col-span-full text-3xl mb-3">بیمارها:</h1>
+        <section className="mb-3 col-span-full flex items-center gap-2">
+            <PatientIcon className='[&>path]:stroke-black [&>path]:stroke-2'/>
+            <h1 className=" text-3xl">بیمارها:</h1>
+        </section>
             {data.map(d => {
                 return <div className="col-span-full lg:col-span-1" key={d.phoneNumber}>
                     <PatientCard linkTo={`/admin/panel/patients/${d.phoneNumber}`}  name={d.name} lastName={d.lastName} phoneNumber={d.phoneNumber} gender={d.gender}/>
