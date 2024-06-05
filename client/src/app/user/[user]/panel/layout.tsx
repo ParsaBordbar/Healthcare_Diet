013175112dce5f9ef2ff85021d452235b8960ee9
MohammadMosavat <42.octmst@gmail.com>
2024-06-05 20:25:04 +0330

    use radios input in side bar of user panel and styles

--- client/src/app/user/[user]/panel/layout.tsx
+++ client/src/app/user/[user]/panel/layout.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { ReactNode } from "react";
+import { ReactNode, memo } from "react";
 import Link from "next/link";
 import HamburgerNavbar from "@/components/HamburgerMenu";
 import useSpecificFetchBmi from "@/hooks/useFetchName/useFetchName";
 import HomeIcon from "/public/svg/userPanelSvgs/home-2.svg";
-import DietIcon from "/public/svg/userPanelSvgs/diet-svgrepo-com.svg";
-import CustomerFeedIcon from "/public/svg/userPanelSvgs/customer-service-svgrepo-com.svg";
+import DocumentIcon from "/public/svg/adminPanelSvgs/document.svg";
+import PersonalIcon from "/public/svg/userPanelSvgs/personal.svg";
 
 interface LayoutUserProps {
   children: ReactNode;
@@ -17,6 +17,12 @@ interface LayoutUserProps {
 const LayoutUser = ({ children, params }: LayoutUserProps) => {
   const bmiData = useSpecificFetchBmi(params.user);
   const { user } = params;
+  console.log(params.user);
+  let count = 0;
+  setTimeout(() => {
+    count = count + 1;
+    console.log(count + 1);
+  }, 1000);
   return (
     <>
       <div className="flex justify-end w-full top-0 z-40">
@@ -37,36 +43,59 @@ const LayoutUser = ({ children, params }: LayoutUserProps) => {
             </h1>
           </div>
           <ul className="flex flex-col rounded-lg h-screen py-4 ps-4 gap-4 ">
-            <Link
-              href={"/"}
-              className="group text-xl cursor-pointer hover:bg-white hover:bg-full ease-in-out transition-all duration-150 p-4 rounded-s-md"
-            >
-              <li className="flex items-center gap-3 text-white group-hover:text-[var(--new-green)]">
-                <HomeIcon className="[&>path]:group-hover:stroke-[var(--new-green)] [&>path]:stroke-white [&>path]:stroke-2" />
-                <p className="pt-2">خانه</p>
-              </li>
+            <Link href={"/"}>
+              <label
+                // href={"/"}
+                className="group text-xl has-[input:checked]:bg-white cursor-pointer hover:bg-white ease-in-out transition-all duration-150 p-4 flex items-center gap-2 rounded-s-md"
+              >
+                <input
+                  className="hidden peer items-center gap-3 text-white group-hover:text-[var(--new-green)]"
+                  type="radio"
+                  name="radio"
+                />
+                <HomeIcon className="[&>path]:group-hover:stroke-[var(--new-green)] peer-checked:[&>path]:stroke-[var(--new-green)] [&>path]:stroke-white [&>path]:stroke-2" />
+                <p className="group-hover:text-[var(--new-green)] peer-checked:text-[var(--new-green)] pt-2 text-white">
+                  خانه
+                </p>
+              </label>
             </Link>
-            <Link
-              className="group text-xl cursor-pointer hover:bg-white hover:bg-full ease-in-out transition-all duration-150 p-4 rounded-s-md"
-              href={`/user/${user}/panel`}
-            >
-              <li className="flex items-center gap-3 text-white group-hover:text-[var(--new-green)]">
-                <CustomerFeedIcon className="[&>path]:group-hover:fill-[var(--new-green)] " />
-                پیشخوان
-              </li>
+            <Link href={`/user/${user}/panel`}>
+              <label
+                className="group text-xl has-[input:checked]:!bg-white hover:bg-white cursor-pointer   ease-in-out transition-all duration-150 p-4  flex items-center gap-2 rounded-s-md"
+                // href={`/user/${user}/panel`}
+              >
+                <input
+                  className="hidden peer group2 items-center gap-3 text-white group-hover:text-[var(--new-green)]"
+                  type="radio"
+                  name="radio"
+                />
+                <PersonalIcon className="[&>path]:group-hover:stroke-[var(--new-green)] [&>path]:stroke-white [&>path]:group-hover:fill-white [&>path]:stroke-2 peer-checked:[&>path]:stroke-[var(--new-green)] " />
+                <p className="group-hover:text-[var(--new-green)] peer-checked:text-[var(--new-green)] text-white ">
+                  پیشخوان
+                </p>
+              </label>
             </Link>
-            <Link
-              className="group text-xl cursor-pointer hover:bg-white hover:bg-full ease-in-out transition-all duration-150 p-4 rounded-s-md"
-              href={`/user/${user}/panel/diets`}
-            >
-              <li className="flex items-center gap-3 text-white group-hover:text-[var(--new-green)]">
-                <DietIcon className="[&>path]:group-hover:fill-[var(--new-green)] " />
-                <p>رژیم ها</p>
-              </li>
+            <Link href={`/user/${user}/panel/diets`}>
+              <label
+                className="group text-xl has-[input:checked]:!bg-white cursor-pointer hover:bg-white   ease-in-out transition-all duration-150 p-4 flex items-center gap-2  rounded-s-md"
+                // href={`/user/${user}/panel/diets`}
+              >
+                <input
+                  className="hidden peer items-center gap-3 checked:hidden text-white group-hover:text-[var(--new-green)]"
+                  type="radio"
+                  name="radio"
+                />
+                <DocumentIcon className="[&>path]:group-hover:stroke-[var(--new-green)] [&>path]:stroke-white [&>path]:stroke-2 peer-checked:[&>path]:stroke-[var(--new-green)]" />
+                <p className="group-hover:text-[var(--new-green)] peer-checked:text-[var(--new-green)] text-white">
+                  رژیم ها
+                </p>
+              </label>
             </Link>
           </ul>
         </div>
         <main className="w-full md:w-[75%] lg:w-5/6 sm:px-10 px-6 md:px-16 py-20 md:py-16 bg-white">
+          <p className="text-3xl">{count + 1}</p>
+
           <div className="">{children}</div>
         </main>
       </div>
@@ -74,4 +103,4 @@ const LayoutUser = ({ children, params }: LayoutUserProps) => {
   );
 };
 
-export default LayoutUser;
+export default memo(LayoutUser);
