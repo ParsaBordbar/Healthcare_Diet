5dfced5258b41cb3af5fd20d9f315c617bc54c9e
MohammadMosavat <42.octmst@gmail.com>
2024-06-06 18:53:58 +0330

    rename the folder of comments user to commentToUser

---
+++ client/src/components/CommentsToUser/index.tsx
@@ -0,0 +1,35 @@
+"use client";
+import useFetchPatientComments from "@/hooks/useFetchPatientComments/useFetchPatientComments";
+import CommentBox from "../AdminComponents/CommentBox";
+import CommentIcon from "/public/svg/adminPanelSvgs/messages.svg";
+
+const CommentsToUser = ({ userID }: { userID: string }) => {
+  const comments = useFetchPatientComments(userID);
+
+  return (
+    <main className="grid col-span-4 custom-scroll pl-6 grid-cols-1 ">
+      <div className="col-span-full flex flex-col gap-6">
+        <section className="flex items-center gap-2">
+          <CommentIcon className="[&>path]:stroke-black" />
+          <h1>جدیدترین پیام های دریافتی شما</h1>
+        </section>
+        {comments
+          ? comments.map((comment, index) => {
+              if (index >= 3) return;
+              return (
+                <CommentBox
+                  className="col-span-full"
+                  key={comment.createdAtJalali}
+                  sender={comment.sender}
+                  body={comment.body}
+                  receiver={comment.receiver}
+                  createdAtJalali={comment.createdAtJalali}
+                />
+              );
+            })
+          : null}
+      </div>
+    </main>
+  );
+};
+export default CommentsToUser;
