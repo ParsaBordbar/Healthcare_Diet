7ac5b27020a0e5623baaac05f4a219d273755fed
MohammadMosavat <42.octmst@gmail.com>
2024-06-06 14:54:42 +0330

    use sidebaritems component and handle the activation of them and color of them in layout of userpanel

--- client/src/app/user/[user]/panel/layout.tsx
+++ client/src/app/user/[user]/panel/layout.tsx
@@ -1,11 +1,14 @@
 "use client";
-import { ReactNode, memo } from "react";
+import { ReactNode, memo, useEffect } from "react";
 import Link from "next/link";
 import HamburgerNavbar from "@/components/HamburgerMenu";
 import useSpecificFetchBmi from "@/hooks/useFetchName/useFetchName";
 import HomeIcon from "/public/svg/userPanelSvgs/home-2.svg";
 import DocumentIcon from "/public/svg/adminPanelSvgs/document.svg";
 import PersonalIcon from "/public/svg/userPanelSvgs/personal.svg";
+import SideBarItems from "@/components/SidebarItems";
+import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 
 interface LayoutUserProps {
   children: ReactNode;
@@ -17,12 +20,8 @@ interface LayoutUserProps {
 const LayoutUser = ({ children, params }: LayoutUserProps) => {
   const bmiData = useSpecificFetchBmi(params.user);
   const { user } = params;
-  console.log(params.user);
-  let count = 0;
-  setTimeout(() => {
-    count = count + 1;
-    console.log(count + 1);
-  }, 1000);
+  const router = usePathname();
+  console.log("rendering layout of user panel");
   return (
     <>
       <div className="flex justify-end w-full top-0 z-40">
@@ -43,59 +42,28 @@ const LayoutUser = ({ children, params }: LayoutUserProps) => {
             </h1>
           </div>
           <ul className="flex flex-col rounded-lg h-screen py-4 ps-4 gap-4 ">
-            <Link href={"/"}>
-              <label
-                // href={"/"}
-                className="group text-xl has-[input:checked]:bg-white cursor-pointer hover:bg-white ease-in-out transition-all duration-150 p-4 flex items-center gap-2 rounded-s-md"
-              >
-                <input
-                  className="hidden peer items-center gap-3 text-white group-hover:text-[var(--new-green)]"
-                  type="radio"
-                  name="radio"
-                />
-                <HomeIcon className="[&>path]:group-hover:stroke-[var(--new-green)] peer-checked:[&>path]:stroke-[var(--new-green)] [&>path]:stroke-white [&>path]:stroke-2" />
-                <p className="group-hover:text-[var(--new-green)] peer-checked:text-[var(--new-green)] pt-2 text-white">
-                  خانه
-                </p>
-              </label>
-            </Link>
-            <Link href={`/user/${user}/panel`}>
-              <label
-                className="group text-xl has-[input:checked]:!bg-white hover:bg-white cursor-pointer   ease-in-out transition-all duration-150 p-4  flex items-center gap-2 rounded-s-md"
-                // href={`/user/${user}/panel`}
-              >
-                <input
-                  className="hidden peer group2 items-center gap-3 text-white group-hover:text-[var(--new-green)]"
-                  type="radio"
-                  name="radio"
-                />
-                <PersonalIcon className="[&>path]:group-hover:stroke-[var(--new-green)] [&>path]:stroke-white [&>path]:group-hover:fill-white [&>path]:stroke-2 peer-checked:[&>path]:stroke-[var(--new-green)] " />
-                <p className="group-hover:text-[var(--new-green)] peer-checked:text-[var(--new-green)] text-white ">
-                  پیشخوان
-                </p>
-              </label>
-            </Link>
-            <Link href={`/user/${user}/panel/diets`}>
-              <label
-                className="group text-xl has-[input:checked]:!bg-white cursor-pointer hover:bg-white   ease-in-out transition-all duration-150 p-4 flex items-center gap-2  rounded-s-md"
-                // href={`/user/${user}/panel/diets`}
-              >
-                <input
-                  className="hidden peer items-center gap-3 checked:hidden text-white group-hover:text-[var(--new-green)]"
-                  type="radio"
-                  name="radio"
-                />
-                <DocumentIcon className="[&>path]:group-hover:stroke-[var(--new-green)] [&>path]:stroke-white [&>path]:stroke-2 peer-checked:[&>path]:stroke-[var(--new-green)]" />
-                <p className="group-hover:text-[var(--new-green)] peer-checked:text-[var(--new-green)] text-white">
-                  رژیم ها
-                </p>
-              </label>
-            </Link>
+          <SideBarItems
+              router={router}
+              value={"خانه"}
+              href={`/`}
+              icon={HomeIcon}
+            />
+            <SideBarItems
+              router={router}
+              value={"پروفایل شخصی"}
+              href={`/user/${user}/panel`}
+              icon={PersonalIcon}
+            />
+
+            <SideBarItems
+              router={router}
+              value={"رژیم ها"}
+              href={`/user/${user}/panel/diets`}
+              icon={DocumentIcon}
+            />
           </ul>
         </div>
         <main className="w-full md:w-[75%] lg:w-5/6 sm:px-10 px-6 md:px-16 py-20 md:py-16 bg-white">
-          <p className="text-3xl">{count + 1}</p>
-
           <div className="">{children}</div>
         </main>
       </div>
