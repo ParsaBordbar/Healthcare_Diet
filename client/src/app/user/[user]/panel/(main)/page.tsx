56796af0649209afe86c2ab30c59662501a0e52d
MohammadMosavat <42.octmst@gmail.com>
2024-06-06 18:17:05 +0330

    create the main page of user panel have welcome section and sender form and show comments from admin

--- client/src/app/user/[user]/panel/(main)/page.tsx
+++ client/src/app/user/[user]/panel/(main)/page.tsx
@@ -1,9 +1,6 @@
-// app/user/[user]/panel/main.tsx
-"use client";
-import CommentBox from "@/components/AdminComponents/CommentBox";
-import useFetchPatientComments from "@/hooks/useFetchPatientComments/useFetchPatientComments";
 import { FC, memo } from "react";
-import MassegeIcon from "/public/svg/adminPanelSvgs/messages.svg";
+import WelcomeUser from "@/components/WelcomeUser";
+import CommentsUser from "@/components/CommentsUser";
 interface UserPanelProps {
   params: {
     user: string;
@@ -13,28 +10,10 @@ interface UserPanelProps {
 const UserPanel: FC<UserPanelProps> = ({ params }) => {
   const userID = params.user;
   console.log(userID);
-  const comments = useFetchPatientComments(userID);
-
   return (
-    <section>
-      <div className="flex items-center gap-2 mb-6">
-        <MassegeIcon className='[&>path]:stroke-black [&>path]:stroke-2'/>
-        <h1 className="text-3xl">پیام‌های شما:</h1>
-      </div>
-      <main className="grid grid-cols-2 gap-8">
-        {comments
-          ? comments.map((comment) => (
-              <CommentBox
-                className="md:col-span-1 col-span-2"
-                key={comment.createdAtJalali}
-                sender={comment.sender}
-                body={comment.body}
-                receiver={comment.receiver}
-                createdAtJalali={comment.createdAtJalali}
-              />
-            ))
-          : null}
-      </main>
+    <section className="grid min-[1320px]:grid-cols-8 grid-cols-7 gap-8">
+      <WelcomeUser userID={userID} />
+      <CommentsUser userID={userID}/>
     </section>
   );
 };
