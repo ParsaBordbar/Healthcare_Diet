7c5b383f4cd35a6e9bbb395f93b68290c21d807b
ParsaBordbar <parsab71@gmail.com>
2024-06-06 16:40:12 +0330

    Add new attributes of Bmi to it's component in front-end.

--- client/src/components/AdminComponents/PatientBmiForm/index.tsx
+++ client/src/components/AdminComponents/PatientBmiForm/index.tsx
@@ -1,11 +1,12 @@
 import { BmiType } from '@/types'
 import React from 'react'
 
-function PatientBmiForm({name, lastName, phoneNumber, gender, age, height, weight, bmi}: BmiType) {
+function PatientBmiForm({name, lastName, phoneNumber, gender, age, height, weight, bmi, abdominalCircumference,joinedAtGregorian, joinedAtJalali,}: BmiType) {
   return (
     <section className='flex flex-col bg-[var(--milky-white)] py-4 rounded-lg gap-4 shadow-md'>
-        <div className='flex flex-col 2xl:flex-row justify-between text-[var(--text)] mt-3 items-start gap-4 2xl:gap-0 2xl:items-center'>
+        <div className='flex flex-col 2xl:flex-row justify-around text-[var(--text)] mt-3 items-start gap-4 2xl:gap-0 2xl:items-center '>
           <h2 className='flex items-center bg-[var(--new-green)] text-white text-3xl font-semibold rounded-e-full overflow-hidden whitespace-nowrap text-ellipsis ps-6 pe-10 w-fit py-3'>{name}{" "}{lastName}</h2>
+          <p className='flex items-center text-[var(--new-green)] text-lg font-semibold w-fit mx-6 xl:p-2.5 rounded-full'>تاریخ ثبت:{" "} {joinedAtJalali}</p>
           <p className='flex items-center text-[var(--new-green)] text-lg font-semibold w-fit mx-6 2xl:p-2.5 rounded-full'>BMI:{" "} {bmi}</p>
         </div>
         <div className='flex 2xl:flex-row items-start 2xl:items-center flex-col'>
@@ -17,8 +18,11 @@ function PatientBmiForm({name, lastName, phoneNumber, gender, age, height, weigh
             <p>قد:{" "} {height}</p>
             <p>وزن:{" "} {weight}</p>
           </div>
+          <div className='flex  flex-col text-[var(--text)] 2xl:px-10 px-6 text-lg gap-4  '>
+            <p> شماره تماس:  {" "}  {phoneNumber}</p>
+            <p>دور شکم:{" "} {abdominalCircumference}</p>
+          </div>
         </div>
-        <p className='2xl:px-10 px-6  flex items-center 2xl:my-2 text-xl text-[var(--tittle-box)]'> شماره تماس:  {" "}  {phoneNumber}</p>
     </section>
   )
 }
