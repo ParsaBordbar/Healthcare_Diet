136031aae403bf7ecacdf8f51a222cd8b67df2a6
MohammadMosavat <42.octmst@gmail.com>
2024-06-06 14:52:33 +0330

    create side bar items for all side bars

---
+++ client/src/components/SidebarItems/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { FunctionComponent, ReactNode, useEffect, useMemo } from "react";
+type TSideBarItems = {
+  value: string;
+  href: string;
+  icon: FunctionComponent;
+  router: string;
+};
+
+const SideBarItems = (props: TSideBarItems) => {
+  const SelectionStyles = useMemo(() => {
+    if (props.router == props.href && props.value == "رژیم ها") {
+      return true;
+    }
+    if (props.router == props.href && props.value == "پروفایل شخصی") {
+      return true;
+    }
+    if (props.router == props.href && props.value == "خانه") {
+      return true;
+    }
+  }, [props.router]);
+
+  return (
+    <Link href={props.href}>
+      <label
+        className={`group [&>svg>path]:hover:stroke-[var(--new-green)] [&>svg>path]:stroke-white  [&>svg&>path]:stroke-2  text-xl ${
+          SelectionStyles && "bg-white [&>svg>path]:!stroke-[var(--new-green)]"
+        } cursor-pointer hover:bg-white   ease-in-out transition-all duration-150 p-4 flex items-center gap-2  rounded-s-md`}
+      >
+        <input
+          className="hidden peer items-center gap-3 checked:hidden text-white group-hover:text-[var(--new-green)]"
+          type="radio"
+          name="radio"
+        />
+        {<props.icon />}
+        <p
+          className={`group-hover:text-[var(--new-green)] ${
+            SelectionStyles && "!text-[var(--new-green)]"
+          } text-white`}
+        >
+          {props.value}
+        </p>
+      </label>
+    </Link>
+  );
+};
+
+export default SideBarItems;
