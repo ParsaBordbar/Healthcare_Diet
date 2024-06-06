be2754d66569a26f05e5bad83b78bcc596d6e34c
ParsaBordbar <parsab71@gmail.com>
2024-06-07 01:49:12 +0330

    Add SearchBar and a text for PaitentsCount in the page.

--- client/src/app/admin/panel/patients/page.tsx
+++ client/src/app/admin/panel/patients/page.tsx
@@ -1,9 +1,10 @@
 'use client'
-import PatientBox from "@/components/AdminComponents/PatientBox"
 import PatientCard from "@/components/AdminComponents/PatientCard"
 import useFetchBmi from "@/hooks/useFetchBmi"
-import Link from "next/link"
 import PatientIcon from '/public/svg/adminPanelSvgs/patients.svg'
+import MainInput from "@/components/MainInput"
+import SearchIcon from "/public/svg/search-normal.svg"
+
 const PatientsPage = () => {
     const data = useFetchBmi()
     return(
@@ -12,6 +13,17 @@ const PatientsPage = () => {
             <PatientIcon className='[&>path]:stroke-black [&>path]:stroke-2'/>
             <h1 className=" text-3xl">بیمارها:</h1>
         </section>
+        <form className="col-span-full flex justify-start gap-5 mb-6 items-end" action="">
+          <MainInput
+            iconFirst={SearchIcon}
+            parentClassName="!w-3/5"
+            type="search"
+            placeholder="نام بیمار مورد نظر خود را وارد کنید"
+            // value={searchValue}
+            // onChange={handleChange}
+          />
+          <p className="text-lg rounded-xl px-8 py-3">تعداد تمام بیمارها:  {data.length}</p>
+        </form>
             {data.map(d => {
                 return <div className="col-span-full lg:col-span-1" key={d.phoneNumber}>
                     <PatientCard linkTo={`/admin/panel/patients/${d.phoneNumber}`}  name={d.name} lastName={d.lastName} phoneNumber={d.phoneNumber} gender={d.gender}/>
