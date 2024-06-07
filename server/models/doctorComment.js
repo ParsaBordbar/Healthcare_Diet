c7f3868508962e2ded544320a83bedbcaa9cf4d7
ParsaBordbar <parsab71@gmail.com>
2024-06-07 11:22:43 +0330

    Update doctorsComment to handle file metadata.

--- server/models/doctorComment.js
+++ server/models/doctorComment.js
@@ -3,6 +3,15 @@ const moment = require('moment');
 const momentJalaali = require('moment-jalaali');
 const Joi = require('joi');
 
+//Dear Future Parsa if you forget this is the schema for files metaData.
+const fileSchema = new mongoose.Schema({
+  originalName: String,
+  filename: String,
+  path: String,
+  size: Number,
+  mimetype: String,
+});
+
 const doctorsCommentSchema = new mongoose.Schema({
   sender: {
     type: String,
@@ -13,10 +22,11 @@ const doctorsCommentSchema = new mongoose.Schema({
     required: true,
   },
   receiver: {
-    type: String, //mongoose.type.ObjectId
+    type: String,
     ref: "Bmi_form",
     required: true,
   },
+  files: [fileSchema], //This field is the Collection of the meta data for file
   createdAtGregorian: {
     type: Date,
     default: Date.now,
@@ -30,15 +40,22 @@ const doctorsCommentSchema = new mongoose.Schema({
 const doctorsComment = mongoose.model('doctors_comment', doctorsCommentSchema);
 
 const validateDoctorsComment = (doctorsComment) => {
-    const schema = Joi.object({
-      sender: Joi.string().required(),
-      body: Joi.string().required(),
-      receiver: Joi.string().required(),
-      createdAtGregorian: Joi.string().isoDate().required(),
-      createdAtJalali: Joi.string().required(),
-    });
-    return schema.validate(doctorsComment);
-  };
+  const schema = Joi.object({
+    sender: Joi.string().required(),
+    body: Joi.string().required(),
+    receiver: Joi.string().required(),
+    files: Joi.array().items(Joi.object({
+      originalName: Joi.string().required(),
+      filename: Joi.string().required(),
+      path: Joi.string().required(),
+      size: Joi.number().required(),
+      mimetype: Joi.string().required(),
+    })),
+    createdAtGregorian: Joi.string().isoDate().required(),
+    createdAtJalali: Joi.string().required(),
+  });
+  return schema.validate(doctorsComment);
+};
 
 exports.doctorsComment = doctorsComment;
 exports.validateDoctorsComment = validateDoctorsComment;
