be3b1d1f5bd23bb2104a2394dadda14d4a3ce64e
ParsaBordbar <parsab71@gmail.com>
2024-06-09 00:00:12 +0330

    Update uploder route for handling BMI with files. :tada

--- server/routes/uploader.js
+++ server/routes/uploader.js
@@ -1,21 +1,19 @@
 const express = require('express');
-const router =  express.Router();
+const router = express.Router();
 const uploader = require("../middlewares/multer");
 const { doctorsComment } = require('../models/doctorComment');
-const { MediterraneanForm, validateMediterranean } = require('../models/mediterraneanForm');
+const { MediterraneanForm } = require('../models/mediterraneanForm');
+const momentJalaali = require('moment-jalaali');
 
 router.post("/upload/type", uploader.array("document", 5), async (req, res) => {
+    const type = req.query.type;
 
-    const type = req.query.type
-
-    if(type === "comments"){
-
+    if (type === "comments") {
         const { sender, body, receiver } = req.body;
-
         if (!sender || !body || !receiver) {
             return res.status(400).send("Error: sender, body, and receiver are required.");
         }
-    
+
         if (req.files) {
             const filesWithMetadata = req.files.map(file => ({
                 originalName: file.originalname,
@@ -24,21 +22,22 @@ router.post("/upload/type", uploader.array("document", 5), async (req, res) => {
                 size: file.size,
                 mimetype: file.mimetype,
             }));
-    
+
             const comment = new doctorsComment({
                 sender,
                 body,
                 receiver,
                 files: filesWithMetadata
             });
-    
+
             await comment.save();
             res.json(comment);
         } else {
             res.status(400).send("Error: No files uploaded or invalid file types.");
         }
     }
-    if(type === 'mediterranean'){
+
+    if (type === 'mediterranean') {
         if (req.files) {
             const filesWithMetadata = req.files.map(file => ({
                 originalName: file.originalname,
@@ -77,15 +76,25 @@ router.post("/upload/type", uploader.array("document", 5), async (req, res) => {
                 fattyLiver: req.body.fattyLiver,
                 kidneyProblems: req.body.kidneyProblems,
                 thyroid: req.body.thyroid,
-                cancer: req.body.cancer,
-                Migraine: req.body.Migraine,
+                cancer: req.body.cancer === "true", // Convert string to boolean
+                Migraine: req.body.Migraine === "true", // Convert string to boolean
                 otherSickness: req.body.otherSickness,
                 medicine: req.body.medicine,
                 phoneNumber: req.body.phoneNumber,
+                files: filesWithMetadata,
+                createdAtJalali: momentJalaali().format('jYYYY/jM/jD HH:mm:ss')
             });
-            mediterraneanForm = await mediterraneanForm.save();
-            return res.send(mediterraneanForm); 
+
+            try {
+                const savedForm = await mediterraneanForm.save();
+                return res.send(savedForm);
+            } catch (err) {
+                return res.status(400).send(err.message);
+            }
+        } else {
+            res.status(400).send("Error: No files uploaded or invalid file types.");
+        }
     }
-}
 });
+
 module.exports = router;
