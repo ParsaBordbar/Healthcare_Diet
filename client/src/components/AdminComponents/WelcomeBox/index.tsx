52401e95d1ecc75a2e7d9d71b409d4c4bf5bdcba
ParsaBordbar <parsab71@gmail.com>
2024-06-08 12:55:39 +0330

    Repace SearchBar in main panel page for admin with Caleander.

--- client/src/components/AdminComponents/WelcomeBox/index.tsx
+++ client/src/components/AdminComponents/WelcomeBox/index.tsx
@@ -1,20 +1,15 @@
 'use client'
-import MainInput from "@/components/MainInput";
+import Calendar from "@/components/Calendar";
 import { WelcomeBoxType } from "@/types";
 import React from "react";
-import SearchIcon from "/public/svg/search-normal.svg";
+
 function WelcomeBox({ doctorName }: WelcomeBoxType) {
   return (
     <div className="flex items-center justify-between w-full">
       <h1 className="text-[var(--new-green)] text-2xl font-semibold  ">
         خوش آمدید مشاور تغذیه {doctorName}{" "}
       </h1>
-      <MainInput
-        iconFirst={SearchIcon}
-        parentClassName="!w-1/2 "
-        type="search"
-        placeholder="نام بیمار مورد نظر خود را وارد کنید"
-      />
+      <Calendar />
     </div>
   );
 }
