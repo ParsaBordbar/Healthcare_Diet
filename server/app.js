eab5eb42116594897a3af18fbb0c7c7d0014cb4b
ParsaBordbar <parsab71@gmail.com>
2024-06-08 18:28:51 +0330

    Add code to create uploads dirctory when we run the server.

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require("express");
 const app = express();
 const cors = require('cors');
@@ -8,13 +9,21 @@ const adminLogin = require('./routes/admin');
 const doctorsComment = require('./routes/docComments');
 const uploader = require('./routes/uploader'); 
 
+const uploadsDir = path.join(__dirname, "uploads")
+if(!fs.existsSync(uploadsDir)){
+    fs.mkdirSync(uploadsDir, { recursive: true });
+    console.log("uploads was not in file tree, we made it!");
+}else{
+    console.log('uploads exists!');
+}
+
 app.use(cors());
 app.use(express.json());
 app.use('/api/bmi', bmi);
 app.use('/api/mediterranean', mediterranean);
 app.use('/api/admin', adminLogin);
 app.use('/api/doctorsComment', doctorsComment);
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(uploadsDir));
 app.use('/api/uploader', uploader);
 
 module.exports = app;
