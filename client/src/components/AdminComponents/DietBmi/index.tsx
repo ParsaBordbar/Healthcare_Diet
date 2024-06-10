9bad46a3d081b65a9c1874dd564938abfc80350a
ParsaBordbar <parsab71@gmail.com>
2024-06-10 16:31:03 +0330

    Add the Missing prop age.

--- client/src/components/AdminComponents/DietBmi/index.tsx
+++ client/src/components/AdminComponents/DietBmi/index.tsx
@@ -11,9 +11,10 @@ function DietBmi({
   }: BmiType) {
   return (
     <div className="flex justify-start my-2 mx-10">
-        <div className="grid 2xl:grid-cols-4 gap-16">
+        <div className="grid 2xl:grid-cols-5 gap-16">
             <p className='col-span-1'>قد: {height}</p>
             <p className='col-span-1'>وزن: {weight}</p>
+            <p className='col-span-1'>سن: {age}</p>
             <p className='col-span-1'>دور شکم: {abdominalCircumference}</p>
             <p className="col-span-1 text-[var(--new-green)] text-lg font-semibold w-fit ">
             BMI: {bmi}
