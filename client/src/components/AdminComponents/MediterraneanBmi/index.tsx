883aa66fd4d6d7a091268f8555faa9c6f6730a76
ParsaBordbar <parsab71@gmail.com>
2024-06-10 13:15:30 +0330

    Add new component for diet's bmis.

---
+++ client/src/components/AdminComponents/MediterraneanBmi/index.tsx
@@ -0,0 +1,25 @@
+
+import { BmiType } from '@/types'
+import React from 'react'
+
+function MediterraneanBmi({
+    age,
+    height,
+    weight,
+    bmi,
+    abdominalCircumference,
+  }: BmiType) {
+  return (
+    <div className="flex justify-start my-2 mx-10">
+        <div className="grid 2xl:grid-cols-4 gap-16">
+            <p className='col-span-1'>قد: {height}</p>
+            <p className='col-span-1'>وزن: {weight}</p>
+            <p className='col-span-1'>دور شکم: {abdominalCircumference}</p>
+            <p className="col-span-1 text-[var(--new-green)] text-lg font-semibold w-fit ">
+            BMI: {bmi}
+            </p>
+        </div>
+    </div>
+  )
+}
+export default MediterraneanBmi
