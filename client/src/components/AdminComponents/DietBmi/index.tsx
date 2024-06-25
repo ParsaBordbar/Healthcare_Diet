ebcf75752d4b26d35fa2875975cef0de1b4dec1e
MohammadMosavat <42.octmst@gmail.com>
2024-06-25 17:27:04 +0330

    responsive the bmi informations in meditreanl form in user panel

--- client/src/components/AdminComponents/DietBmi/index.tsx
+++ client/src/components/AdminComponents/DietBmi/index.tsx
@@ -10,17 +10,15 @@ function DietBmi({
     abdominalCircumference,
   }: BmiType) {
   return (
-    <div className="flex justify-start my-2 mx-10">
-        <div className="grid 2xl:grid-cols-5 gap-16">
+        <div className="grid grid-cols-5 gap-16">
             <p className='col-span-1'>قد: {height}</p>
             <p className='col-span-1'>وزن: {weight}</p>
             <p className='col-span-1'>سن: {age}</p>
             <p className='col-span-1'>دور شکم: {abdominalCircumference}</p>
-            <p className="col-span-1 text-[var(--new-green)] text-lg font-semibold w-fit ">
+            <p className="col-span-1 text-[var(--new-green)] text-lg w-fit ">
             BMI: {bmi}
             </p>
         </div>
-    </div>
   )
 }
 export default DietBmi
