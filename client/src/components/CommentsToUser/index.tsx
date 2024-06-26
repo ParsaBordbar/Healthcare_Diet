7109f22e31d26b14a4eba65dff164ade0258aae4
ParsaBordbar <parsab71@gmail.com>
2024-06-26 16:42:12 +0330

    Update comments toUser component.

--- client/src/components/CommentsToUser/index.tsx
+++ client/src/components/CommentsToUser/index.tsx
@@ -3,6 +3,8 @@ import useFetchPatientComments from "@/hooks/useFetchPatientComments/useFetchPat
 import CommentBox from "../AdminComponents/CommentBox";
 import CommentIcon from "/public/svg/adminPanelSvgs/messages.svg";
 import Link from "next/link";
+import Calendar from "../Calendar";
+import Tick from '/public/svg/tttick.svg'
 
 const CommentsToUser = ({ userID }: { userID: string }) => {
   const comments = useFetchPatientComments(userID);
@@ -10,9 +12,16 @@ const CommentsToUser = ({ userID }: { userID: string }) => {
   return (
     <main className="grid col-span-full lg:col-span-4 custom-scroll pl-6 grid-cols-1 ">
       <div className="col-span-full flex flex-col gap-6">
-        <section className="flex items-center gap-2">
+        <section className="flex items-center gap-2 justify-between">
+          <div className="flex gap-2 justify-center items-center">
           <CommentIcon className="[&>path]:stroke-black" />
           <h1>جدیدترین پیام های دریافتی شما</h1>
+          </div>
+          <div className="flex gap-2 justify-center items-center">
+          <p className="flex justify-center items-center">امروز</p>  
+          <Calendar /> 
+          </div>
+
         </section>
             {comments? comments.slice(-3).map((data): React.ReactNode => {
               return <CommentBox className=' min-[1320px]:w-[96%] w-11/12' _id="" key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali} isDoctor={false}/>
