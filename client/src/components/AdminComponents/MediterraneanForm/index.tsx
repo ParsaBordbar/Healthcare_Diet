32b396000a6e4b0fb6e0330a15754a9de7c61479
MohammadMosavat <42.octmst@gmail.com>
2024-06-26 18:56:44 +0330

    add age to diets bmi in meditrean form

--- client/src/components/AdminComponents/MediterraneanForm/index.tsx
+++ client/src/components/AdminComponents/MediterraneanForm/index.tsx
@@ -238,7 +238,7 @@ const MediterraneanForm = ({
           <div className="flex flex-row">
             <h4 className="text-2xl ps-12 pe-7 bg-[var(--new-green)] text-white w-fit rounded-e-lg py-2 mb-4">فرم BMI:</h4>
           </div>
-          <DietBmi height={dietBmi?.height} weight={dietBmi?.weight} abdominalCircumference={dietBmi?.abdominalCircumference} bmi={dietBmi?.bmi}/>
+          <DietBmi height={dietBmi?.height} weight={dietBmi?.weight} age={dietBmi?.age} abdominalCircumference={dietBmi?.abdominalCircumference} bmi={dietBmi?.bmi}/>
       </div>
             
         {files.length > 0 && (
