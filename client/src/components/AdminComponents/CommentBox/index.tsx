16c28caaf17b646a745522a3dd8b3be641a034f0
MohammadMosavat <42.octmst@gmail.com>
2024-06-26 19:20:00 +0330

    use download in plan page in user panel for downloading diets

--- client/src/components/AdminComponents/CommentBox/index.tsx
+++ client/src/components/AdminComponents/CommentBox/index.tsx
@@ -4,7 +4,8 @@ import AttachmentIcon from "/public/svg/adminPanelSvgs/attachment-svgrepo-com.sv
 import DateSvg from "/public/svg/adminPanelSvgs/calendar.svg";
 import Link from "next/link";
 import useSpecificFetchBmi from "@/hooks/useFetchName/useFetchName";
-import PatientPageIcon from '/public/svg/User.svg'
+import PatientPageIcon from "/public/svg/User.svg";
+import useDownloader from "react-use-downloader";
 
 const CommentBox = ({
   className,
@@ -13,11 +14,12 @@ const CommentBox = ({
   receiver,
   createdAtJalali,
   isDoctor,
-  files = [], 
+  files = [],
 }: CommentType) => {
   const [userData, setUserData] = useState<BmiType>();
 
-  const fetchedData = useSpecificFetchBmi(receiver);
+  let fetchedData = undefined;
+  if (receiver) fetchedData = useSpecificFetchBmi(receiver);
 
   useEffect(() => {
     if (fetchedData) {
@@ -34,14 +36,21 @@ const CommentBox = ({
     return { __html: text.replace(/\n/g, "<br/>") };
   };
 
+  const { size, elapsed, percentage, download, cancel, error, isInProgress } =
+    useDownloader();
+
   return (
-    <div className={`flex ${className} flex-col col-span-full bg-[var(--milky-white)] gap-2 rounded-lg shadow-md my-3`}>
+    <div
+      className={`flex ${className} flex-col col-span-full bg-[var(--milky-white)] gap-2 rounded-lg shadow-md my-3`}
+    >
       <div className="ps-4 py-3 flex items-start min-[1320px]:gap-5 gap-2 pb-2 bg-[var(--secondary-blue)] text-white rounded-lg rounded-br-none pe-4">
         <section className="flex w-full items-start 2xl:items-center 2xl:flex-row lg:flex-col lg:gap-1 sm:flex-row gap-1 2xl:justify-between justify-between lg:justify-center flex-col">
           <div className="flex flex-col gap-2">
             <div className="flex gap-2 items-center">
               <p className=" text-base">ارسال کننده:</p>
-              <p className=" text-base text-ellipsis overflow-hidden whitespace-nowrap">{sender}</p>
+              <p className=" text-base text-ellipsis overflow-hidden whitespace-nowrap">
+                {sender}
+              </p>
             </div>
             {receiver ? (
               <div className="flex gap-2 items-center">
@@ -55,19 +64,28 @@ const CommentBox = ({
           <div className="flex gap-2  min-[1320px]:justify-center items-center flex-row">
             <div className="flex sm:flex-row gap-4">
               <div className="flex flex-row justify-center items-center gap-2">
-                <DateSvg className="sm:flex [&>path]:stroke-white  hidden" width={24} />
+                <DateSvg
+                  className="sm:flex [&>path]:stroke-white  hidden"
+                  width={24}
+                />
                 <p className="pt-1.5  text-base">{createdAtJalali}</p>
               </div>
               {isDoctor ? (
-                <Link className="px-3 flex items-center" href={`/admin/panel/patients/${receiver}`}>
-                    <PatientPageIcon />
+                <Link
+                  className="px-3 flex items-center"
+                  href={`/admin/panel/patients/${receiver}`}
+                >
+                  <PatientPageIcon />
                 </Link>
               ) : null}
             </div>
           </div>
         </section>
       </div>
-      <p className="p-5 w-full pb-12  text-base " dangerouslySetInnerHTML={formatBody(body)} />
+      <p
+        className="p-5 w-full pb-12  text-base "
+        dangerouslySetInnerHTML={formatBody(body)}
+      />
       {files.length > 0 && (
         <div className="px-8 py-2 flex justify-start flex-col">
           <div className="flex flex-row">
@@ -78,7 +96,16 @@ const CommentBox = ({
               <li className="flex gap-2 items-center my-1" key={file.filename}>
                 <AttachmentIcon />
                 {/* This should be The server host and port */}
-                <a href={`http://localhost:8080/uploads/${file.filename}`} download={file.originalName} className="text-blue-400 hover:underline pt-2 text-sm col-span-1">
+                <a
+                  onClick={() => {
+                    download(
+                      `http://localhost:8080/${file.path}`,
+                      "رژیم مدیترانه ای.png"
+                    );
+                  }}
+                  download={file.originalName}
+                  className="text-blue-400 cursor-pointer hover:underline pt-2 text-sm col-span-1"
+                >
                   {file.originalName}
                 </a>
               </li>
