21abf2e05af999d2e0760f1aae76d48b70e5e5ff
MohammadMosavat <42.octmst@gmail.com>
2024-06-26 19:22:30 +0330

    add _id into new commit

--- client/src/components/AdminComponents/NewCommentsBox /index.tsx
+++ client/src/components/AdminComponents/NewCommentsBox /index.tsx
@@ -14,13 +14,13 @@ function NewCommentsBox () {
         <CommentIcon className='[&>path]:stroke-black'/>
         <h2 className="text-lg ">جدیدترین پیام‌ها</h2>
       </section>
-                <div className='flex max-h-[590px] gap-3 custom-scroll flex-col-reverse'>
-                  {allComments? allComments.slice(-3).map((data): React.ReactNode => {
-                      return <CommentBox className=' min-[1320px]:w-[96%] w-11/12' key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali} isDoctor={true}/>
-                  }): null
-                  }
-                </div>
-        <Link className="pt-3 mx-2 text-lg  text-blue-600 " href={"/admin/panel/comments"}> دیدن‌ همه‌ی پیام‌ها</Link>
+      <div className='flex max-h-[590px] gap-3 custom-scroll flex-col-reverse'>
+        {allComments? allComments.slice(-3).map((data): React.ReactNode => {
+            return <CommentBox className=' min-[1320px]:w-[96%] w-11/12' _id="" key={data.receiver} sender={data.sender}body={data.body} receiver={data.receiver} createdAtJalali={data.createdAtJalali} isDoctor={true}/>
+        }): null
+        }
+      </div>
+      <Link className="pt-3 mx-2 text-lg  text-blue-600 " href={"/admin/panel/comments"}> دیدن‌ همه‌ی پیام‌ها</Link>
     </div>
   )
 }
