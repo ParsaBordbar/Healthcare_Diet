d556a332087089cc1dc1f6e97ba01c5ca8629145
ParsaBordbar <parsab71@gmail.com>
2024-06-30 23:59:03 +0330

    Update DietStatus for better styleing for using it in Plans in user panel.

--- client/src/components/DietStatus/index.tsx
+++ client/src/components/DietStatus/index.tsx
@@ -1,9 +1,9 @@
 import useFetchOneMediterranean from '@/hooks/useFetchOneMediterranean';
-import { LoginDataType } from '@/types';
+import { DietStatusType } from '@/types';
 import React, { useMemo } from 'react'
 import Tick from '/public/svg/adminPanelSvgs/check_ring_round.svg'
 
-function DietStatus({phoneNumber} : LoginDataType) {
+function DietStatus({phoneNumber, direction} : DietStatusType) {
     
     const theMedi = useFetchOneMediterranean(phoneNumber)
 
@@ -12,14 +12,14 @@ function DietStatus({phoneNumber} : LoginDataType) {
       }, [theMedi]);
     
   return (
-    <div className='flex flex-col gap-3 bg-[var(--secondary-blue)] p-4 rounded-lg text-white mt-4 mb-10'>
-        <div className='flex gap-2 items-center justify-center'>
+    <div className={`flex ${direction} justify-center gap-3 bg-[var(--secondary-blue)] p-4 rounded-lg text-white mt-4 mb-10`}>
+        <div className="flex gap-2 items-center justify-center" >
             <h2 className=''>رژیم فعلی شما:</h2>
             <p className='justify-items-center '>  رژیم مدیترانه‌ای</p>
         </div>
         <div className='flex gap-2 items-center justify-center '>
         <Tick />
-            <p> وضعیت رژیم:</p> 
+        <p> وضعیت رژیم:</p> 
             {lastMediterraneanForm?.isChecked?<p className='font-bold'>ویزیت شده</p>:<p className='font-bold'>ویزیت نشده</p>}
         </div>
     </div>
