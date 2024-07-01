1edeaf44fe3dbd9d07676dcbf6cd1b63774e7b2c
ParsaBordbar <parsab71@gmail.com>
2024-07-01 17:28:34 +0330

    Update Validation in front-end for MediterreaneanForm.

--- client/src/validations/login/useMediterraneanForm.ts
+++ client/src/validations/login/useMediterraneanForm.ts
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify';
 import api from '@/apis';
 import { useState } from 'react';
 import { useRouter } from "next/navigation";
+import { MediterraneanFormType } from '@/types';
 
 const getPhoneNumberFromUrl = (): string | null => {
   if (typeof window !== 'undefined') {
@@ -53,7 +54,7 @@ const useMediterraneanForm = () => {
     kidneyProblems: yup.string().required("جواب به این سوال الزامی است"),
     thyroid: yup.string(),
     cancer: yup.string(),
-    supplements: yup.array().of(yup.string()).required("جواب به این سوال الزامی است"),
+    supplements: yup.array().of(yup.string().required("جواب به این سوال الزامی است")),
     Migraine: yup.string(),
     otherSickness: yup.string(),
     medicine: yup.string(),
