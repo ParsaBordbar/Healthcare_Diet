ffd2bc312d466453c637d4ce57f366def84d87bf
MohammadMosavat <42.octmst@gmail.com>
2024-07-02 13:00:25 +0330

    responsive status of diets

--- client/src/components/DietStatus/index.tsx
+++ client/src/components/DietStatus/index.tsx
@@ -12,15 +12,14 @@ function DietStatus({phoneNumber, direction} : DietStatusType) {
       }, [theMedi]);
     
   return (
-    <div className={`flex ${direction} justify-center gap-3 bg-[var(--secondary-blue)] p-4 rounded-lg text-white mt-4 mb-10`}>
-        <div className="flex gap-2 items-center justify-center" >
-            <h2 className=''>رژیم فعلی شما:</h2>
-            <p className='justify-items-center '>  رژیم مدیترانه‌ای</p>
+    <div className={`flex xl:!flex-col !flex-row ${direction} justify-evenly xl:justify-center gap-3 bg-[var(--secondary-blue)] p-4 rounded-lg text-white`}>
+        <div className="flex gap-1 items-center justify-around" >
+            <h2 className='font-bold text-sm xl:text-base'>رژیم فعلی شما:</h2>
+            <p className='justify-items-center '>مدیترانه‌ای</p>
         </div>
-        <div className='flex gap-2 items-center justify-center '>
-        <Tick />
-        <p> وضعیت رژیم:</p> 
-            {lastMediterraneanForm?.isChecked?<p className='font-bold'>ویزیت شده</p>:<p className='font-bold'>ویزیت نشده</p>}
+        <div className='flex gap-1 items-center justify-around '>
+        <p className='font-bold text-sm xl:text-base'> وضعیت رژیم:</p> 
+            {lastMediterraneanForm?.isChecked?<p className=''>ویزیت شده</p>:<p className=''>ویزیت نشده</p>}
         </div>
     </div>
   )
