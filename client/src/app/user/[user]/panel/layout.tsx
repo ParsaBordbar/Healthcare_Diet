8833d25e020585f2f7a0c273da03fad696ff0458
MohammadMosavat <42.octmst@gmail.com>
2024-07-03 00:51:53 +0330

    set new items to humberger menu in user panel and add icons

--- client/src/app/user/[user]/panel/layout.tsx
+++ client/src/app/user/[user]/panel/layout.tsx
@@ -4,11 +4,10 @@ import HamburgerNavbar from "@/components/HamburgerMenu";
 import HomeIcon from "/public/svg/userPanelSvgs/home-2.svg";
 import DocumentIcon from "/public/svg/adminPanelSvgs/document.svg";
 import PersonalIcon from "/public/svg/userPanelSvgs/personal.svg";
-import LogOutIcon from "/public/svg/userPanelSvgs/LogOut.svg"
+import LogOutIcon from "/public/svg/userPanelSvgs/LogOut.svg";
 import SideBarItems from "@/components/SidebarItems";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
-
 
 interface LayoutUserProps {
   children: ReactNode;
@@ -22,15 +21,22 @@ const LayoutUser = ({ children, params }: LayoutUserProps) => {
   const router = usePathname();
 
   const handleLogout = () => {
-    localStorage.removeItem('user');
-  }
+    localStorage.removeItem("user");
+  };
 
   console.log("rendering layout of user panel");
   return (
     <>
       <div className="flex justify-end w-full top-0 z-40">
         <HamburgerNavbar
+          userID={user}
           valueOne="خانه"
+          iconOne={HomeIcon}
+          iconThree={PersonalIcon}
+          iconTwo={DocumentIcon}
+          iconFive={DocumentIcon}
+          iconFour={DocumentIcon}
+          iconSix={DocumentIcon}
           valueTwo="رژیم ها"
           valueThree="پروفایل شخصی"
           valueFour="پیام ها"
@@ -44,15 +50,15 @@ const LayoutUser = ({ children, params }: LayoutUserProps) => {
           linkSix={`/`}
         />
         <div className="fixed hidden pb-6 px-2  top-0 right-0 h-screen bg-[var(--soft-blue)]  md:flex w-[25%] lg:w-[20%]">
-        <Image
+          <Image
             src={"/svg/sideBarBanner.svg"}
             width={80}
             height={1}
             layout="fixed"
             className="object-cover hidden lg:inline rounded-2xl py-2 "
             alt={""}
-        />          
-        <ul className="flex flex-col w-full rounded-lg h-screen py-4 ps-4 gap-4 ">
+          />
+          <ul className="flex flex-col w-full rounded-lg h-screen py-4 ps-4 gap-4 ">
             <SideBarItems
               router={router}
               value={"خانه"}
