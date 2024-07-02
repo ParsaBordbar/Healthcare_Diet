079e45503f8a374cc6f1f0a175bb1a9b9e44871f
MohammadMosavat <42.octmst@gmail.com>
2024-07-02 13:16:52 +0330

    set condition for tick icon in DietStatus

--- client/src/components/DietStatus/index.tsx
+++ client/src/components/DietStatus/index.tsx
@@ -1,28 +1,36 @@
-import useFetchOneMediterranean from '@/hooks/useFetchOneMediterranean';
-import { DietStatusType } from '@/types';
-import React, { useMemo } from 'react'
-import Tick from '/public/svg/adminPanelSvgs/check_ring_round.svg'
+import useFetchOneMediterranean from "@/hooks/useFetchOneMediterranean";
+import { DietStatusType } from "@/types";
+import React, { useMemo } from "react";
+import Tick from "/public/svg/adminPanelSvgs/check_ring_round.svg";
 
-function DietStatus({phoneNumber, direction} : DietStatusType) {
-    
-    const theMedi = useFetchOneMediterranean(phoneNumber)
+function DietStatus({ phoneNumber, direction }: DietStatusType) {
+  const theMedi = useFetchOneMediterranean(phoneNumber);
 
-    const lastMediterraneanForm = useMemo(() => {
-        return theMedi[theMedi.length - 1];
-      }, [theMedi]);
-    
+  const lastMediterraneanForm = useMemo(() => {
+    return theMedi[theMedi.length - 1];
+  }, [theMedi]);
+
   return (
-    <div className={`flex xl:!flex-col !flex-row ${direction} justify-evenly xl:justify-center gap-3 bg-[var(--secondary-blue)] p-4 rounded-lg text-white`}>
-        <div className="flex gap-1 items-center justify-around" >
-            <h2 className='font-bold text-sm xl:text-base'>رژیم فعلی شما:</h2>
-            <p className='justify-items-center '>مدیترانه‌ای</p>
-        </div>
-        <div className='flex gap-1 items-center justify-around '>
-        <p className='font-bold text-sm xl:text-base'> وضعیت رژیم:</p> 
-            {lastMediterraneanForm?.isChecked?<p className=''>ویزیت شده</p>:<p className=''>ویزیت نشده</p>}
-        </div>
+    <div
+      className={`flex  ${direction} justify-evenly xl:justify-center gap-3 bg-[var(--secondary-blue)] p-4 rounded-lg text-white`}
+    >
+      <div className="flex gap-1 items-center justify-around">
+        <h2 className="font-bold text-sm xl:text-base">رژیم فعلی شما:</h2>
+        <p className="justify-items-center ">مدیترانه‌ای</p>
+      </div>
+      <div className="flex gap-1 items-center justify-around ">
+        <section className="flex items-center gap-1">
+          {Tick && <Tick />}
+          <p className="font-bold text-sm xl:text-base"> وضعیت رژیم:</p>
+        </section>
+        {lastMediterraneanForm?.isChecked ? (
+          <p className="">ویزیت شده</p>
+        ) : (
+          <p className="">ویزیت نشده</p>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default DietStatus
+export default DietStatus;
