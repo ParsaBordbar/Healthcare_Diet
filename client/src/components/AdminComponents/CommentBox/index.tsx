8ebcc599a8f09f8029ed052a2811057a6e202844
MohammadMosavat <42.octmst@gmail.com>
2024-07-02 15:00:05 +0330

    change the layout of title of comment in mobile size and fix the padding size

--- client/src/components/AdminComponents/CommentBox/index.tsx
+++ client/src/components/AdminComponents/CommentBox/index.tsx
@@ -39,7 +39,7 @@ const CommentBox = ({
       className={`flex ${className} flex-col col-span-full bg-[var(--milky-white)] gap-2 rounded-lg shadow-md my-3`}
     >
       <div className="ps-4 py-3 flex items-start min-[1320px]:gap-5 gap-2 pb-2 bg-[var(--secondary-blue)] text-white rounded-lg rounded-br-none pe-4">
-        <section className="flex w-full items-start 2xl:items-center 2xl:flex-row lg:flex-col lg:gap-1 sm:flex-row gap-1 2xl:justify-between justify-between lg:justify-center flex-col">
+        <section className={`flex w-full items-start 2xl:items-center 2xl:flex-row lg:flex-col lg:gap-1 sm:flex-row gap-1 2xl:justify-between justify-between lg:justify-center ${!receiver && '!flex-row !items-center'} flex-col `}>
           <div className="flex flex-col gap-2">
             <div className="flex gap-2 items-center">
               <p className="text-base">ارسال کننده:</p>
@@ -60,7 +60,7 @@ const CommentBox = ({
             <div className="flex sm:flex-row gap-4">
               <div className="flex flex-row justify-center items-center gap-2">
                 <DateSvg
-                  className="sm:flex [&>path]:stroke-white hidden"
+                  className="flex [&>path]:stroke-white"
                   width={24}
                 />
                 <p className="pt-1.5 text-base">{createdAtJalali}</p>
@@ -82,7 +82,7 @@ const CommentBox = ({
         dangerouslySetInnerHTML={formatBody(body)}
       />
       {files.length > 0 && (
-        <div className="px-8 py-2 flex justify-start flex-col">
+        <div className="px-5 py-2 flex justify-start flex-col">
           <div className="flex flex-row">
             <h4 className="text-base font-semibold mb-2">پیوست‌ها:</h4>
           </div>
