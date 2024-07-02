a2a0a01f63e4ea2b45bd4fc4fd6be1a1c18344f4
MohammadMosavat <42.octmst@gmail.com>
2024-07-03 00:51:24 +0330

    change the layout of hamburger menu ib user panel and add the some information of user in ned of the menu

--- client/src/components/HamburgerMenu/index.tsx
+++ client/src/components/HamburgerMenu/index.tsx
@@ -1,13 +1,23 @@
 "use client";
-import { useCallback, useMemo, useState } from "react";
+import { FunctionComponent, useCallback, useMemo, useState } from "react";
 import Styles from "./navbar.module.scss";
 import { motion } from "framer-motion";
 import LogoSvg from "/public/svg/Logo.svg";
 import Link from "next/link";
 import SideBarBanner from "/public/svg/sideBarBanner.svg";
+import MaleAvatar from "/public/svg/adminPanelSvgs/Group 108.svg";
+import PhoneNumber from "/public/svg/phone.svg";
+import useSpecificFetchBmi from "@/hooks/useFetchName/useFetchName";
+import LogOutIcon from "/public/svg/userPanelSvgs/LogOut.svg";
 
 type THamMenu = {
   logo?: boolean;
+  iconOne?: FunctionComponent;
+  iconTwo?: FunctionComponent;
+  iconThree?: FunctionComponent;
+  iconFour?: FunctionComponent;
+  iconFive?: FunctionComponent;
+  iconSix?: FunctionComponent;
   className?: string;
   valueOne: string;
   linkOne: string;
@@ -21,10 +31,19 @@ type THamMenu = {
   valueFour?: string;
   valueFive?: string;
   valueSix?: string;
+  valueSeven?: string;
+  iconSeven?: FunctionComponent;
+  linkSeven?: string;
+  userID?: string;
 };
 
 const HamburgerNavbar = (props: THamMenu) => {
   const [burgerMenuActive, setBurgerMenuActive] = useState(false);
+
+  let patients = undefined;
+  if (props.userID) {
+    patients = useSpecificFetchBmi(props.userID);
+  }
 
   const listItemVariants = {
     open: {
@@ -44,13 +63,38 @@ const HamburgerNavbar = (props: THamMenu) => {
     },
   };
 
-  const ValueArray: { value: string; link: string }[] = [
-    { value: props.valueOne, link: props.linkOne },
-    { value: props.valueTwo, link: props.linkTwo },
-    { value: props.valueThree ?? "", link: props.linkThree ?? "" },
-    { value: props.valueFour ?? "", link: props.linkFour ?? "" },
-    { value: props.valueFive ?? "", link: props.linkFive ?? "" },
-    { value: props.valueSix ?? "", link: props.linkSix ?? "" },
+  const ValueArray: {
+    value: string;
+    link: string;
+    icon: FunctionComponent | undefined;
+  }[] = [
+    { value: props.valueOne, link: props.linkOne, icon: props.iconOne },
+    { value: props.valueTwo, link: props.linkTwo, icon: props.iconTwo },
+    {
+      value: props.valueThree ?? "",
+      link: props.linkThree ?? "",
+      icon: props.iconThree,
+    },
+    {
+      value: props.valueFour ?? "",
+      link: props.linkFour ?? "",
+      icon: props.iconFour,
+    },
+    {
+      value: props.valueFive ?? "",
+      link: props.linkFive ?? "",
+      icon: props.iconFive,
+    },
+    {
+      value: props.valueSix ?? "",
+      link: props.linkSix ?? "",
+      icon: props.iconSix,
+    },
+    {
+      value: props.valueSeven ?? "",
+      link: props.linkSeven ?? "",
+      icon: props.iconSeven,
+    },
   ];
 
   const LinkGenerate = useCallback(() => {
@@ -58,13 +102,19 @@ const HamburgerNavbar = (props: THamMenu) => {
       if (!Links.value || !Links.link) return;
       return (
         <Link
-          className="group"
+          className="group hover:bg-[var(--new-green)] transition-all ease-in-out duration-200 p-2 py-3 rounded-lg w-full"
           href={Links.link}
           onClick={() => setBurgerMenuActive(false)}
           key={Links.value}
         >
-          <motion.li className="!p-0" variants={listItemVariants}>
-            <p className="group-hover:text-[var(--new-green)]">{Links.value}</p>
+          <motion.li
+            className="!p-0 flex items-center group-hover:[&>svg>path]:stroke-white gap-2"
+            variants={listItemVariants}
+          >
+            {Links.icon && <Links.icon />}
+            <p className="text-[var(--black-blue)] group-hover:text-white font-bold">
+              {Links.value}
+            </p>
           </motion.li>
         </Link>
       );
@@ -80,6 +130,10 @@ const HamburgerNavbar = (props: THamMenu) => {
       )
     );
   }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+  };
 
   const toggleBurgerMenu = () => {
     setBurgerMenuActive(!burgerMenuActive);
@@ -126,14 +180,47 @@ const HamburgerNavbar = (props: THamMenu) => {
           </div>
           {OptionGenerate}
         </div>
-        <div className={Styles.content + ""}>
+        <div className={Styles.content + " w-full !flex h-[80vh]"}>
           <motion.ul
-            className="flex  gap-6"
+            className="flex w-full gap-6"
             animate={burgerMenuActive ? "open" : "closed"}
             variants={motionVariants}
           >
-            <SideBarBanner className="h-[300px] rounded-2xl" width={40} />
-            <div className="flex flex-col gap-6">{LinkGenerate()}</div>
+            <SideBarBanner className="h-[80vh] rounded-2xl" width={40} />
+            <div className="flex flex-col justify-between w-full gap-6">
+              <section className="flex gap-6">
+                <div className="flex flex-col w-full gap-3 pl-16">
+                  {LinkGenerate()}
+                </div>
+              </section>
+              <main
+                className="p-2 pl-[4.75rem] flex flex-col gap-2
+               "
+              >
+                <ul className="flex flex-col gap-2">
+                  <li className="font-bold cursor-pointer hover:text-white text-[var(--black-blue)] !px-3 rounded-lg transition-all duration-200 ease-in-out !py-2 hover:bg-[var(--new-green)] text-base">
+                    تنطیمات
+                  </li>
+                  <li
+                    onClick={handleLogout}
+                    className="font-bold group cursor-pointer  hover:text-white text-[var(--black-blue)] !px-3 rounded-lg transition-all duration-200 ease-in-out flex items-center gap-2 !py-2 hover:bg-[var(--new-green)] text-base"
+                  >
+                    <LogOutIcon className='group-hover:[&>path]:stroke-white'/>
+                    <span>خروج</span>
+                  </li>
+                </ul>
+                <section className=" flex gap-4 items-center">
+                  <MaleAvatar className="" />
+                  <div className="flex flex-col">
+                    <h1>{patients?.name + " " + patients?.lastName}</h1>
+                    <section className="flex items-center gap-2">
+                      <PhoneNumber className="[&>path]:stroke-[var(--new-green)]" />
+                      <p>{patients?.phoneNumber}</p>
+                    </section>
+                  </div>
+                </section>
+              </main>
+            </div>
           </motion.ul>
         </div>
       </div>
