344f2f3eff78beae47f364ecc471633d9e385592
ParsaBordbar <parsab71@gmail.com>
2024-07-04 02:15:52 +0330

    Fix customCard Component.

--- client/src/components/Dr_Rabiee_Landing_components/CustomCard.jsx
+++ client/src/components/Dr_Rabiee_Landing_components/CustomCard.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import Image from 'next/image';
 
 function CustomCard({ title, description, imageUrl }) {
   return (
     <div className="max-w-xs m-2 bg-[#3531F2] rounded-3xl p-4 text-white">
-      <img
+      <Image
         src={imageUrl}
         alt={title}
         className="rounded-full w-full mb-4"
+        width={5000} 
+        height={5000}
       />
       <div className="text-center">
-        <h5 className="text-xl font-[YekanBakhBold] mb-2">{title}</h5>
-        <p className="text-base font-[YekanBakh]">{description}</p>
+        <h5 className="text-2xl font-extrabold mb-2">{title}</h5>
+        <p className="text-base">{description}</p>
       </div>
     </div>
   );
