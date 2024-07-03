ef4eb0ba8a8d1c16cbe5572c618d4ae8af37f1fd
MohammadMosavat <42.octmst@gmail.com>
2024-07-03 15:04:49 +0330

    set state for update the open or close accorfion compoenent

--- client/src/components/Accordion/index.tsx
+++ client/src/components/Accordion/index.tsx
@@ -1,27 +1,32 @@
-// Accordion.js 
-
-const Accordion = (props:any) => { 
-	return ( 
-		<div className="border rounded-lg mb-1"> 
-			<button 
-				className={`w-full ${!props.isOpen ? 'rounded-lg' : 'rounded-t-lg'}  flex items-center justify-between p-4 text-left hover:bg-blue-300
+'use client'
+import { useState} from "react";
+const Accordion = (props: any) => {
+  console.log(props, "props data");
+  const [isOpen , setIsOpen] = useState<boolean>(false)
+  return (
+    <div className="border rounded-lg mb-1">
+      <button
+        className={`w-full ${
+          !props.isOpen ? "rounded-lg" : "rounded-t-lg"
+        }  flex items-center justify-between p-4 text-left hover:bg-blue-300
 						bg-[var(--new-green)] text-white transition duration-300`}
-				onClick={props.toggleAccordion} 
-			> 
-				{props.title} 
-				<span className={`float-right transform ${props.isOpen ? 
-								'rotate-180' : 'rotate-0'} 
-								transition-transform duration-300`}> 
-					&#9660; 
-				</span> 
-			</button> 
-			{props.isOpen && ( 
-				<div className="p-4 bg-blue-100"> 
-					{props.data} 
-				</div> 
-			)} 
-		</div> 
-	); 
-}; 
+        onClick={() => {
+          setIsOpen(!isOpen)
+        }}
+      >
+        {props.title}
+        <span
+          className={`float-right transform ${
+            isOpen ? "rotate-180" : "rotate-0"
+          } 
+								transition-transform duration-300`}
+        >
+          &#9660;
+        </span>
+      </button>
+      {isOpen && <div className="p-4 bg-[var(--soft-blue)]">{props.data}</div>}
+    </div>
+  );
+};
 
-export default Accordion
+export default Accordion;
