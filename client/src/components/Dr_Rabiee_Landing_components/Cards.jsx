9f306cd4c07d2b6fe08fdf974b240dc962083079
ParsaBordbar <parsab71@gmail.com>
2024-07-04 02:30:25 +0330

    Update cards Tilttle.

--- client/src/components/Dr_Rabiee_Landing_components/Cards.jsx
+++ client/src/components/Dr_Rabiee_Landing_components/Cards.jsx
@@ -28,11 +28,11 @@ const cardsData = [
 function CardsSection() {
   return (
     <div className="flex flex-col items-center h-screen bg-[#FEFDF9] overflow-auto relative" dir="rtl">
-      <div className="container mt-8 mb-4">
-        <h1 className="text-4xl font-extrabold text-[#3A3A97] text-center mb-4">
+      <div className="container mt-8">
+        <h1 className="text-4xl font-extrabold text-[#3A3A97] text-center ">
           برنامه های سلامتی
         </h1>
-        <p className="text-lg text-black text-center mb-4">
+        <p className="text-lg text-black text-center mb-1">
           شما می توانید برنامه های رژیمی مختلف را مشاهده کرده و خریداری نمایید.
         </p>
         <div className="flex justify-center gap-4">
@@ -47,7 +47,7 @@ function CardsSection() {
           ))}
         </div>
       </div>
-      <div className="absolute bottom-0 left-0 w-1/4 h-[60px] bg-[#3531F2] text-white p-2 border-t border-white rounded-tr-full flex items-center justify-center font-[YekanBakhBold]">
+      <div className="absolute bottom-0 left-0 w-1/4 h-[60px] bg-[#3531F2] text-white px-2 border-t border-white rounded-r-full flex items-center justify-center font-extrabold text-xl">
         <h6 className="text-center">
           به سلامتی خود، با هر لقمه، نزدیک‌تر شوید.
         </h6>
