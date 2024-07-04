04cf03032c86f554501e24c0c698a7e0b2fc1e3e
ParsaBordbar <parsab71@gmail.com>
2024-07-04 11:14:02 +0330

    Update Cards comnent with TypesScript.

---
+++ client/src/components/Dr_Rabiee_Landing_components/Cards.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import CustomCard from "./CustomCard";
+
+const cardsData = [
+  {
+    id: 1,
+    title: "پکیج اول رژیم",
+    description: "۲۵۰.۰۰۰ تومان",
+    imageUrl: "/images/PackageOne.png",
+  },
+  {
+    id: 2,
+    title: "پکیج دوم رژیم",
+    description: "۲۵۰.۰۰۰ تومان",
+    imageUrl: "/images/PackageTwo.png",
+  },
+  {
+    id: 3,
+    title: "پکیج سوم رژیم",
+    description: "۲۵۰.۰۰۰ تومان",
+    imageUrl: "/images/PackageThree.png",
+  },
+];
+
+function CardsSection() {
+  return (
+    <div className="flex flex-col items-center h-screen bg-[#FEFDF9] overflow-auto relative" dir="rtl">
+      <div className="container mt-8">
+        <h1 className="text-4xl font-extrabold text-[#3A3A97] text-center ">
+          برنامه های سلامتی
+        </h1>
+        <p className="text-lg text-black text-center mb-1">
+          شما می توانید برنامه های رژیمی مختلف را مشاهده کرده و خریداری نمایید.
+        </p>
+        <div className="flex justify-center gap-4">
+          {cardsData.map((card) => (
+            <div key={card.id} className="flex flex-col items-center">
+              <CustomCard
+                title={card.title}
+                description={card.description}
+                imageUrl={card.imageUrl}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="absolute bottom-0 left-0 w-1/4 h-[60px] bg-[#3531F2] text-white px-2 border-t border-white rounded-r-full flex items-center justify-center font-extrabold text-xl">
+        <h6 className="text-center">
+          به سلامتی خود، با هر لقمه، نزدیک‌تر شوید.
+        </h6>
+      </div>
+    </div>
+  );
+}
+
+export default CardsSection;
