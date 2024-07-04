cf2657719f2c06cd7fa69ea9c1b0150dd7bf4218
ParsaBordbar <parsab71@gmail.com>
2024-07-04 14:40:47 +0330

    Update Pyramid.

--- client/src/components/Dr_Rabiee_Landing_components/Pyramid.tsx
+++ client/src/components/Dr_Rabiee_Landing_components/Pyramid.tsx
@@ -5,13 +5,13 @@ import Image from 'next/image';
 const Pyramid = () => {
   return(
     <section className="relative p-5 bg-[#FEFDF9]">
-    <div className="absolute top-20 right-0 w-1/6 h-16 bg-[#3531F2] text-white p-2 rounded-l-full border-t border-white rtl flex">
-      <h6 className="font-bold flex items-center">
+    <div className="flex justify-center gap-28 items-center">
+    <div className="absolute left-0 w-1/6 top-5 h-20 bg-[#3531F2] text-white p-2 rounded-r-full border-t border-white rtl flex">
+      <h6 className="font-bold flex items-center px-12 ">
       هرم غذایی رژیم مدیترانه‌ای
       </h6>
     </div>
-    <div className="flex justify-between items-center">
-    <div>
+    <div className="mt-40">
         <Image
           src={PyramidImage}
           alt="Mediterranean Diet Pyramid"
@@ -20,25 +20,18 @@ const Pyramid = () => {
           height={500}
         />
       </div>
-      <article className="w-1/2 ml-5 rtl">
-        <h3 className="text-lg">
+      <article className="flex w-2/5 justify-center items-center rtl mt-28">
+      <h3 className="text-lg">
         رژیم غذایی مدیترانه‌ای، الگویی غذایی است که از سنت‌های غذایی مردمان
-      نواحی زیتون‌خیز حوزه مدیترانه در دهه‌های ۱۹۵۰ و ۱۹۶۰ الهام گرفته و
-      شامل مصرف بالای روغن زیتون، حبوبات، غلات کامل، میوه‌ها، سبزی‌ها و ماهی
-      است، در حالی که مصرف گوشت قرمز محدود می‌شود. این رژیم به کاهش خطر
-      بیماری‌های قلبی-عروقی و سایر بیماری‌های مزمن کمک می‌کند. تفاوت‌های
-      فرهنگی و اقتصادی در کشورهای مدیترانه‌ای باعث شده تا تنوعی در این رژیم
-      غذایی وجود داشته باشد. اصول کلی این رژیم در هرم غذایی مدیترانه‌ای نشان
-      داده شده که شامل دستورالعمل‌های روزانه، هفتگی و گاه به گاه برای یک
-      زندگی سالم‌تر است.
-        </h3>
-        <ul className="list-disc pl-5">
-          <li>کارشناس تغذیه از دانشگاه علوم پزشکی شیراز</li>
-          <li>کارشناس ارشد تغذیه ورزشی از دانشگاه شیراز</li>
-          <li>دکتری بیوشیمی و متابولیسم ورزشی از دانشگاه شیراز</li>
-          <li>عضو هیات مدیره انجمن تغذیه استان فارس</li>
-          <li>مدرس دانشگاه زند شیراز</li>
-        </ul>
+        نواحی زیتون‌خیز حوزه مدیترانه در دهه‌های ۱۹۵۰ و ۱۹۶۰ الهام گرفته و
+        شامل مصرف بالای روغن زیتون، حبوبات، غلات کامل، میوه‌ها، سبزی‌ها و ماهی
+        است، در حالی که مصرف گوشت قرمز محدود می‌شود. این رژیم به کاهش خطر
+        بیماری‌های قلبی-عروقی و سایر بیماری‌های مزمن کمک می‌کند. تفاوت‌های
+        فرهنگی و اقتصادی در کشورهای مدیترانه‌ای باعث شده تا تنوعی در این رژیم
+        غذایی وجود داشته باشد. اصول کلی این رژیم در هرم غذایی مدیترانه‌ای نشان
+        داده شده که شامل دستورالعمل‌های روزانه، هفتگی و گاه به گاه برای یک
+        زندگی سالم‌تر است.
+      </h3>
       </article>
       
     </div>
