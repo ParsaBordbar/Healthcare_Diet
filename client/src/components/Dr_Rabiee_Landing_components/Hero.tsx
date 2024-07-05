eeb552843c821746e1bf72d0cf91d3c3c26ac723
ParsaBordbar <parsab71@gmail.com>
2024-07-05 11:13:56 +0330

    Add Ponter svg to Hero.

--- client/src/components/Dr_Rabiee_Landing_components/Hero.tsx
+++ client/src/components/Dr_Rabiee_Landing_components/Hero.tsx
@@ -4,10 +4,11 @@ import HeroImg from '/public/images/Hero.jpg';
 import Kashi from '/public/images/kashi.png'
 import Link from "next/link";
 import MainButton from "../MainButton";
+import PointerSvg from "/public/svg/Dr_Rabiee_Landing/HeroPointer.svg"
 
 function MainSection() {
   return (
-    <div className="flex flex-row md:flex-row w-full h-[670px] bg-[#FEFDF9] px-12">
+    <div className="flex flex-row md:flex-row w-full h-[670px] bg-[#FEFDF9] px-8">
       {/* Left Column */}
       <div className="flex justify-center items-center h-full">
           <Image
@@ -18,20 +19,22 @@ function MainSection() {
           height={5000}
           />
         </div>
-      <div className="flex flex-col md:flex-row w-full md:w-1/2 h-full items-center">
-        <div className="flex flex-col justify-between items-center text-center p-5 text-[#3A3A97] rtl">
-          <h3 className="mb-5 text-5xl font-extrabold">
-            رژیم مدیترانه‌ای، نه فقط غذا بلکه یک سبک زندگی است.
-          </h3>
-          <p className="mb-5 text-base text-black">
-            برنامه شخصی سازشده خود را دریافت کنید، ما در هر مرحله با شماییم.
-          </p>
+      <div className="flex flex-col md:flex-row w-full md:w-1/2 h-full items-end mt-32 ms-28">
+        <div className="flex flex-col items-center justify-center text-right p-5 text-[#3A3A97] rtl mb-32 ">
+          <div className="flex flex-col justify-end px-16">
+            <h3 className="mb-5 text-5xl font-extrabold">
+              رژیم مدیترانه‌ای، نه فقط غذا بلکه یک سبک زندگی است.
+            </h3>
+            <p className="mb-5 text-lg text-[#8A9196] justify-start items-start pe-40">
+            برنامه شخصی سازشده خود را دریافت کنید، ما در هر مرحله با شماییم!</p>
+          </div>
           <Link href="/register/login/enterNumber">
             <MainButton className="border border-[#FFA301] rounded-lg font-extrabold shadow-md px-9 py-2 bg-[#FFA301] hover:border-[var(--secondary-blue)] modern hover:bg-[var(--secondary-blue)] text-white transition-colors duration-300"
               value={"دریافت رژیم"} />
           </Link>
+          <PointerSvg className="relative right-36 top-3" />
         </div>
-        
+
       </div>
       {/* Right Column */}
       <div className="flex justify-center items-center w-full md:w-1/2">
