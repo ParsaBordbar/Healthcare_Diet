a7f60652d6b9e86dd394988b490fb3318567ffb5
MohammadMosavat <42.octmst@gmail.com>
2024-07-05 13:39:30 +0330

    Add slider for StoryBoc section or aritcle section

--- client/src/components/Dr_Rabiee_Landing_components/Article.tsx
+++ client/src/components/Dr_Rabiee_Landing_components/Article.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Slider } from "../slider";
+import { DataStory } from "@/data";
+import { TextBoxType } from "@/types";
+import StoryBox from "../landingPage_components/StoryBox";
 
 function ArticleSlider() {
-  return <div>{/* Slider implementation */}</div>;
+  return (
+    <div className="flex-col w-[75%] sm:w-[65%] mx-auto gap-10 flex items-center">
+      <section className="flex flex-col items-center gap-4 w-3/4">
+        <h1 className="text-[var(--black-blue)] text-4xl font-extrabold">
+          مقالات
+        </h1>
+        <p className="text-[#424242] text-center">
+          با دستور العمل های ایجاد شده (و آزمایش شده) توسط تیم داخلی ما از
+          سرآشپزها و متخصصان تغذیه، می توانیم به شما قول بدهیم که تغذیه سالم
+          خوشمزه است!
+        </p>
+      </section>
+      <Slider className="!py-10 flex flex-col items-center" anotherProp={2}>
+        {DataStory.map((items: TextBoxType) => {
+          return (
+            // <SwiperSlide className="!w-fit !m-0" key={items.id} {...items}>
+            <StoryBox
+              id={items.id}
+              title={items.title}
+              bodyText={items.bodyText}
+              img={items.img}
+            />
+            // </SwiperSlide>
+          );
+        })}
+      </Slider>
+    </div>
+  );
 }
 
 export default ArticleSlider;
