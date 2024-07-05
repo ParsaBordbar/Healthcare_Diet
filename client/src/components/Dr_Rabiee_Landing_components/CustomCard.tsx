6a1ea33c0b57d99fa99ef6800eb11c5185a0d2ff
ParsaBordbar <parsab71@gmail.com>
2024-07-05 10:03:23 +0330

    Update CustomCard with new Props for newPrice and oldPrice.

--- client/src/components/Dr_Rabiee_Landing_components/CustomCard.tsx
+++ client/src/components/Dr_Rabiee_Landing_components/CustomCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from 'next/image';
 import { CustomCardType } from "@/types";
 
-function CustomCard({ title, description, imageUrl }:CustomCardType) {
+function CustomCard({ title, oldPrice, newPrice, imageUrl }:CustomCardType) {
   return (
     <div className="mt-10 flex justify-center flex-col items-center max-w-xs my-6 py-11 mx-2 bg-[var(--soft-blue)] rounded-3xl p-6 text-[var(--black-blue)] border border-transparent hover:ease-in-out delay-100 hover:border-[var(--secondary-blue)] cursor-pointer shadow-md transition-transform">
       <Image
@@ -13,8 +13,9 @@ function CustomCard({ title, description, imageUrl }:CustomCardType) {
         height={120}
       />
       <div className="text-center">
-        <h5 className="text-sm font-extrabold mb-2">{title}</h5>
-        <p className="text-xl font-extrabold">{description}</p>
+        <h5 className="text-xl font-extrabold mb-2">{title}</h5>
+        <p className="text-sm  my-3 line-through">{oldPrice}</p>
+        <p className="font-extrabold my-3">{newPrice} </p>
       </div>
     </div>
   );
