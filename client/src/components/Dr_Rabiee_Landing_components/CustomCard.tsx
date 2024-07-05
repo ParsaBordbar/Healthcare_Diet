bc4ad8b54f58f65e11d91df4771434fbeadf04cf
ParsaBordbar <parsab71@gmail.com>
2024-07-05 19:21:09 +0330

    Add Responsiveness to CustomCard Component for Mobile and Tablet sizes.

--- client/src/components/Dr_Rabiee_Landing_components/CustomCard.tsx
+++ client/src/components/Dr_Rabiee_Landing_components/CustomCard.tsx
@@ -4,18 +4,20 @@ import { CustomCardType } from "@/types";
 
 function CustomCard({ title, oldPrice, newPrice, imageUrl }:CustomCardType) {
   return (
-    <div className="mt-10 flex justify-center flex-col items-center max-w-xs mb-6 py-11 mx-2 bg-[var(--soft-blue)] rounded-3xl p-6 text-[var(--black-blue)] border border-transparent hover:ease-in-out delay-150 hover:border-[var(--secondary-blue)] cursor-pointer shadow-md transition-all">
-      <Image
-        src={imageUrl}
-        alt={title}
-        className="rounded-full mb-4"
-        width={120} 
-        height={120}
-      />
-      <div className="text-center">
-        <h5 className="text-xl font-extrabold mb-2">{title}</h5>
-        <p className="text-sm  my-3 line-through">{oldPrice}</p>
-        <p className="font-extrabold my-3">{newPrice} </p>
+    <div className="mt-10 flex justify-center flex-col items-center max-w-xs mb-6 py-11 mx-2 bg-[var(--soft-blue)] rounded-3xl p-6 text-[var(--black-blue)] border border-transparent hover:ease-in-out delay-150 hover:border-[var(--secondary-blue)] cursor-pointer shadow-md transition-all col-span-1 ">
+      <div className="sm:w-44 flex justify-center items-center md:w-full">
+        <Image
+          src={imageUrl}
+          alt={title}
+          className="rounded-full mb-4"
+          width={120} 
+          height={120}
+        />
+      </div>
+      <div className="text-center flex justify-center items-center flex-col gap-2">
+        <h5 className="md:text-xl font-extrabold mb-2 text-3xl">{title}</h5>
+        <p className="md:text-sm  line-through text-lg mt-2">{oldPrice}</p>
+        <p className="md:text-base font-extrabold text-lg">{newPrice} </p>
       </div>
     </div>
   );
