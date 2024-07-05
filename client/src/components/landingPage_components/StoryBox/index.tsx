728b86232142b711c5dc86a6679c897e26d2b315
MohammadMosavat <42.octmst@gmail.com>
2024-07-05 13:37:59 +0330

    change the styles in storyBox component

--- client/src/components/landingPage_components/StoryBox/index.tsx
+++ client/src/components/landingPage_components/StoryBox/index.tsx
@@ -4,19 +4,19 @@ import Arrow from "/public/svg/Arrow 1.svg";
 
 const StoryBox = ({ title, bodyText, img  , id}: TextBoxType) => {
   return (
-    <div className="flex bg-[var(--box-bg)] flex-col rounded-2xl justify-center items-center w-[394px]">
+    <div className="flex bg-[#e9eff0] flex-col rounded-2xl gap-4 py-4 px-2.5 justify-center items-center w-[394px]">
       {/* <div className="w-fit">{img}</div> */}
-      <img src={img} alt="" />
-      <div className="flex justify-center items-start flex-col m-2 p-4 border-b-2">
-        <h3 className="text-2xl text-[var(--tittle-box)] font-semibold">
-          {title + id}
+      <img src={img} alt="" className="rounded-3xl -mt-10" />
+      <div className="flex justify-center gap -1 items-start flex-col px-2  ">
+        <h3 className="text-2xl text-[var(--black-blue)] font-semibold">
+          {title}
         </h3>
         <p className="text-xl ">{bodyText}</p>
       </div>
       <Link href={"./"} className="self-start px-6 py-2">
         <div className="flex items-center gap-3">
           <Arrow />
-          <p>ادامه</p>
+          <p>ادامه مطلب</p>
         </div>
       </Link>
     </div>
