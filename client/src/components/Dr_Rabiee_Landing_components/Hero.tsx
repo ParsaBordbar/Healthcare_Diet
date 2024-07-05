f654b90b3b8ae4a2114d18455d7de4762e06e932
ParsaBordbar <parsab71@gmail.com>
2024-07-05 15:32:37 +0330

    Add Responsiveness for Laptop and Tablet sizes for Hero.

--- client/src/components/Dr_Rabiee_Landing_components/Hero.tsx
+++ client/src/components/Dr_Rabiee_Landing_components/Hero.tsx
@@ -8,10 +8,10 @@ import PointerSvg from "/public/svg/Dr_Rabiee_Landing/HeroPointer.svg"
 
 function MainSection() {
   return (
-    <div className="flex justify-center items-center flex-row md:flex-row w-full bg-[#FEFDF9] ps-8">
-      <div className="grid xl:grid-cols-7">
+    <div className="flex justify-center items-center flex-row w-full bg-[#FEFDF9] md:px-8">
+      <div className="grid 2xl:grid-cols-7 xl:grid-cols-5 md:grid-cols-2 grid-cols-1 ps-2">
 
-        <div className="flex justify-center items-center w-1/2 h-full xl:col-span-1">
+        <div className="xl:flex justify-center items-center w-1/2 h-full xl:col-span-1 hidden">
             <Image
             src={Kashi}
             alt="Decorative pattern"
@@ -21,13 +21,13 @@ function MainSection() {
             />
         </div>
 
-        <div className="flex flex-col md:flex-row w-full h-full items-end xl:col-span-3 justify-start">
+        <div className="flex flex-col md:flex-row w-full h-full items-end 2xl:col-span-3 xl:col-span-2 col-span-1 justify-start">
           <div className="flex flex-col items-center justify-center text-right p-5 text-[#3A3A97] rtl">
-            <div className="flex flex-col justify-end px-16">
-              <h3 className="mb-5 text-5xl font-extrabold">
+            <div className="flex flex-col justify-end 2xl:px-16 xl:pe-10 md:pe-4 leading-6">
+              <h3 className="mb-5 xl:text-5xl font-extrabold text-3xl">
                 رژیم مدیترانه‌ای، نه فقط غذا بلکه یک سبک زندگی است.
               </h3>
-              <p className="mb-5 text-lg text-[#8A9196] justify-start items-start xl:pe-72">
+              <p className="mb-5 2xl:text-lg lg:text-base text-[#8A9196] justify-start items-start 2xl:pe-72">
               برنامه شخصی سازشده خود را دریافت کنید،  
               ما در هر مرحله با شماییم!</p>
             </div>
@@ -39,8 +39,8 @@ function MainSection() {
           </div>
 
         </div>
-        <div className="flex justify-center items-center w-full xl:col-span-3">
-          <div className="w-[661px] h-[561px] rounded-[170px_0_170px_0] shadow-md overflow-hidden flex justify-center items-center">
+        <div className="flex justify-center items-center w-full 2xl:col-span-3 col-span-1 xl:col-span-2">
+          <div className="md:w-[661px] xl:h-[561px] rounded-[170px_0_170px_0] shadow-md overflow-hidden flex justify-center items-center lg:h-[461px] h-[360px] w-[560px]">
             <Image
             src={HeroImg}
             alt="Hero in Box"          
