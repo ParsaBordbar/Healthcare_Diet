cc59d89240d6dfdf9b5cb81b0a36c90c866fbb43
MohammadMosavat <42.octmst@gmail.com>
2024-07-06 10:45:08 +0330

    responsive footer in different size

--- client/src/components/Dr_Rabiee_Landing_components/Footer.tsx
+++ client/src/components/Dr_Rabiee_Landing_components/Footer.tsx
@@ -8,8 +8,8 @@ import MainButton from "../MainButton";
 const Footer = () => {
   return (
     <div className="flex justify-center items-center bg-[#3233F2] text-white py-10 ">
-      <div className="grid lg:grid-cols-4 grid-cols-5 px-10 gap-10">
-        <section className="grid grid-cols-3 lg:col-span-3 col-span-4 gap-4 lg:pr-16">
+      <div className="grid lg:grid-cols-4 grid-cols-5 px-10 sm:gap-10">
+        <section className="grid grid-cols-3 lg:col-span-3  col-span-full sm:col-span-4 gap-4 lg:pr-16">
           <div className="min-[910px]:flex hidden flex-col min-[910px]:col-span-full lg:col-span-2 justify-start ">
             <h6 className="mb-1 font-extrabold text-2xl ">
               زندگی سالم با رژیم مدیترانه‌ای: استراتژی‌های دکتر ربیعی
@@ -29,28 +29,28 @@ const Footer = () => {
             <div className="rounded-full w-full flex items-center border border-white">
               <input
                 type="text"
-                className="bg-transparent outline-none w-2/3 pr-4 py-2 "
+                className="bg-transparent sm:placeholder:text-base placeholder:text-xs outline-none w-2/3 pr-2 py-2 "
                 placeholder="شماره همراه خود را وارد کنید"
               />
               <MainButton
                 modern
-                className="border w-1/3 [&>span]:text-base py-2 !rounded-full border-white"
+                className="border w-1/3 [&>span]:text-sm sm:[&>span]:text-base py-2 !rounded-full border-white"
                 value={"دریافت رژيم"}
               />
             </div>
           </form>
-          <div className="flex min-[910px]:p-0 pr-5 min-[910px]:flex-row lg:flex-col gap-6 lg:justify-center min-[910px]:justify-between items-center text-right col-span-full lg:col-span-1 ">
+          <div className="flex min-[910px]:p-0 sm:pr-5 min-[910px]:flex-row lg:flex-col gap-6 lg:justify-center min-[910px]:justify-between items-center text-right col-span-full lg:col-span-1 ">
             <h6 className="text-xl min-[910px]:inline-block hidden font-extrabold">
               دسترسی آسان
             </h6>
-            <ul className="flex lg:flex-col flex-col min-[910px]:flex-row lg:gap-3 gap-2.5 min-[910px]:gap-16  ">
-              <li className="text-xl font-extrabold list-disc">درباره ما</li>
-              <li className="text-xl font-extrabold list-disc">مقالات</li>
-              <li className="text-xl font-extrabold list-disc">تماس با ما</li>
+            <ul className="flex lg:flex-col sm:flex-col sm:justify-normal sm:w-auto justify-between w-full min-[910px]:flex-row lg:gap-3 gap-2.5 min-[910px]:gap-16  ">
+              <li className="sm:text-xl text-base list-none font-extrabold sm:list-disc">درباره ما</li>
+              <li className="sm:text-xl text-base list-none font-extrabold sm:list-disc">مقالات</li>
+              <li className="sm:text-xl text-base list-none font-extrabold sm:list-disc">تماس با ما</li>
             </ul>
           </div>
         </section>
-        <div className="flex flex-col items-center text-center col-span-1 mt-4">
+        <div className="flex flex-col items-center text-center col-span-full sm:col-span-1 mt-4">
           <DrGroupLogo />
           <div className="flex gap-3 mt-10">
             <a href="#" className="text-white">
