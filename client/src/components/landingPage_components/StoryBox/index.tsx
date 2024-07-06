6e77c101d49ce9ef61fb4afb38ed4f1e760dbc07
MohammadMosavat <42.octmst@gmail.com>
2024-07-06 11:05:14 +0330

    responsive the story box

--- client/src/components/landingPage_components/StoryBox/index.tsx
+++ client/src/components/landingPage_components/StoryBox/index.tsx
@@ -4,17 +4,17 @@ import Arrow from "/public/svg/Arrow 1.svg";
 
 const StoryBox = ({ title, bodyText, img  , id}: TextBoxType) => {
   return (
-    <div className="flex bg-[#e9eff0] flex-col rounded-2xl gap-4 py-4 px-2.5 justify-center items-center w-[394px]">
+    <div className="flex bg-[#e9eff0] flex-col rounded-2xl gap-4 py-4 px-2.5 justify-center items-center w-[280px] sm:w-[394px]">
       {/* <div className="w-fit">{img}</div> */}
       <img src={img} alt="" className="rounded-3xl -mt-10" />
       <div className="flex justify-center gap -1 items-start flex-col px-2  ">
-        <h3 className="text-2xl text-[var(--black-blue)] font-semibold">
+        <h3 className="sm:text-2xl text-xl text-[var(--black-blue)] font-semibold">
           {title}
         </h3>
-        <p className="text-xl ">{bodyText}</p>
+        <p className="sm:text-xl text-base">{bodyText}</p>
       </div>
       <Link href={"./"} className="self-start px-6 py-2">
-        <div className="flex items-center gap-3">
+        <div className="flex sm:text-base text-sm items-center gap-3">
           <Arrow />
           <p>ادامه مطلب</p>
         </div>
