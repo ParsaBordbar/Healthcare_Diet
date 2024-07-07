4f7156e7aa8d874047ae7dbbc8b6ee9966dd369a
MohammadMosavat <42.octmst@gmail.com>
2024-07-07 14:19:28 +0330

    add top to the parent of bmi form

--- client/src/app/register/diets-bmi/page.tsx
+++ client/src/app/register/diets-bmi/page.tsx
@@ -3,7 +3,7 @@ import BmiFormDiet from "@/components/bmiForms";
 const DietPage = () => {
   return (
     <>
-      <div className="absolute flex flex-col justify-center lg:justify-end items-center gap-5 w-4/5 lg:w-1/3 top-[50%] translate-y-[-45%] lg:right-[7.5rem]">
+      <div className="absolute flex flex-col justify-center lg:justify-end items-center gap-5 w-4/5 lg:w-1/3 top-auto sm:top-[10%]  lg:right-[7.5rem]">
         <BmiFormDiet />
       </div>
     </>
