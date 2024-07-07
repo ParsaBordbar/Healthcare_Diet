0a7696add464c18da0084b8521ba3ebe375f742a
ParsaBordbar <parsab71@gmail.com>
2024-07-07 14:41:55 +0330

    Update IInformation component with Info.

--- client/src/components/Dr_Rabiee_Landing_components/Information.tsx
+++ client/src/components/Dr_Rabiee_Landing_components/Information.tsx
@@ -5,39 +5,36 @@ import Check from "/public/svg/check.svg";
 
 function InformationSection() {
   return (
-    <section className="relative p-5 bg-[#FEFDF9]">
+    <section className="relative p-5 bg-[#FEFDF9] my-10">
       <div className="absolute xl:top-20 top-0  right-0 flex items-center xl:w-1/6 md:w-2/6 w-3/6 h-20 bg-[#3531F2] text-white p-2 rounded-l-full border-t border-white rtl">
-        <h6 className="font-bold w-full text-xl text-center">متخصص تغذیه</h6>
+        <h6 className="font-bold w-full text-xl text-center">محمدرضا ربیعی</h6>
       </div>
-      <div className="flex justify-evenly min-[1109px]:gap-0 gap-4 min-[1109px]:flex-row flex-col-reverse items-center  mt-24">
-        <article className="min-[1109px]:w-1/3 w-3/3 min-[1109px]:m-0 mx-auto flex flex-col gap-4 rtl">
-          <h3 className="text-lg text-justify">
-          دکتر محمدرضا ربیعی، کارشناس تغذیه از دانشگاه علوم پزشکی شیراز، کارشناس ارشد تغذیه ورزشی و دکتری بیوشیمی و متابولیسم ورزشی از دانشگاه شیراز فارغ التحصیل شده است. وی دارای شماره نظام پزشکی ت-1118 و با پنج سال سابقه کاری موفق و همکاری با هنرمندان و ورزشکاران است. محمدرضا ربیعی دارای بالاترین درصد رضایت مردمی در صفحه اینستاگرام خود و بیش از ۱۰۰۰ تجربه موفق از مراجعین خود دارد.
-          </h3>
+      <div className="flex justify-evenly min-[1109px]:gap-0 gap-4 min-[1109px]:flex-row flex-col-reverse items-center mt-24">
+        <article className="min-[1109px]:w-1/3 w-3/3 min-[1109px]:m-0 mx-auto flex flex-col gap-4 rtl md:text-lg xl:text-xl">
           <ul className="text-justify [&>li]:sm:text-base min-[321px]:[&>li]:text-sm [&>li]:text-[0.755rem] flex min-[1109px]:w-full  mx-auto min-[1109px]:m-0 flex-col gap-2 md:pl-5">
             <li className="flex items-center gap-2">
               <Check />
-              <p>کارشناس تغذیه از دانشگاه علوم پزشکی شیراز</p>
+              <p className="md:text-lg xl:text-xl">کارشناس تغذیه از دانشگاه علوم پزشکی شیراز</p>
             </li>
             <li className="flex items-center gap-2">
               <Check />
-              <p>کارشناس ارشد تغذیه ورزشی از دانشگاه شیراز </p>
+              <p className="md:text-lg xl:text-xl">کارشناس ارشد تغذیه ورزشی از دانشگاه شیراز </p>
             </li>
             <li className="flex items-center gap-2">
               <Check />
-              <p>دکتری بیوشیمی و متابولیسم ورزشی از دانشگاه شیراز </p>
+              <p className="md:text-lg xl:text-xl">دکتری بیوشیمی و متابولیسم ورزشی از دانشگاه شیراز </p>
             </li>
             <li className="flex items-center gap-2">
               <Check />
-              <p>عضو هیات مدیره انجمن تغذیه استان فارس</p>
+              <p className="md:text-lg xl:text-xl">عضو هیات مدیره انجمن تغذیه استان فارس</p>
             </li>
             <li className="flex items-center gap-2">
               <Check />
-              <p>مدرس دانشگاه زند شیراز</p>
+              <p className="md:text-lg xl:text-xl">عضو بنیاد ملی نخبگان</p>
             </li>
             <li className="flex items-center gap-2">
               <Check />
-              <p>عضو بنیاد ملی نخبگان</p>
+              <p className="md:text-lg xl:text-xl">مدرس دانشگاه</p>
             </li>
           </ul>
         </article>
