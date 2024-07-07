cff36e3436eac8083152bbad253fa0547023d06a
ParsaBordbar <parsab71@gmail.com>
2024-07-07 13:12:12 +0330

    Update PaitentBmiForm.

--- client/src/components/AdminComponents/PatientBmiForm/index.tsx
+++ client/src/components/AdminComponents/PatientBmiForm/index.tsx
@@ -25,36 +25,42 @@ function PatientBmiForm({
       <div className="flex flex-col 2xl:flex-row justify-between text-[var(--text)] mt-3 items-start gap-4 2xl:gap-0 2xl:items-center ">
         <Link
           href={`/admin/panel/patients/${phoneNumber}`}
-          className="flex items-center bg-[var(--new-green)] text-white text-2xl font-semibold rounded-e-full overflow-hidden whitespace-nowrap text-ellipsis ps-6 pe-10 w-fit py-3"
+          className="flex items-center bg-[var(--new-green)] text-white text-2xl font-semibold rounded-e-full overflow-hidden whitespace-nowrap text-ellipsis ps-6 pe-10 w-fit py-2"
         >
           {name} {lastName}
         </Link>
-        <section className="flex flex-col gap-2 items-end">
-          <p className="text-[var(--new-green)] text-lg w-fit ">
-            BMI: {bmi}
-          </p>
-         <p className="text-[var(--new-green)] text-lg w-fit ">
-            WHR: {whr}
-          </p>  
-        <p className="text-[var(--new-green)] text-lg w-fit ">
+        <section className="flex flex-col items-end">
+          <p className="text-[var(--black-blue)] text-lg w-fit ">
             تاریخ ثبت: {joinedAtJalali}
           </p>
         </section>
+      </div>
+      <div className="pr-6  flex justify-between gap-10">
+          <p className="text-[var(--black-blue)] text-lg w-fit">
+            شماره پرونده: {autoIncrementId}
+          </p>
+          <p className="text-[var(--black-blue)] text-lg w-fit"> شماره تماس: {phoneNumber}</p>
+
       </div>
       <div className="flex 2xl:flex-row pr-6 items-start justify-between 2xl:items-center flex-col">
         <div className="flex flex-col gap-4 text-[var(--text)] text-lg">
           <p>جنسیت: {gender}</p>
           <p>سن: {age}</p>
-          <p> شهر {city} </p>
+          <p> شهر:{city} </p>
         </div>
         <div className="flex  flex-col text-[var(--text)] text-lg gap-4  ">
           <p>قد: {height}</p>
           <p>وزن: {weight}</p>
+          WHR: {whr}
+
         </div>
         <div className="flex  flex-col text-[var(--text)] text-lg gap-4  ">
-          <p> شماره تماس: {phoneNumber}</p>
           <p>دور شکم: {abdominalCircumference}</p>
          <p>دور باسن: {hipcircumference}</p>
+
+         <p className=" text-lg w-fit">
+            BMI: {bmi}
+          </p>
         </div>
       </div>
     </section>
