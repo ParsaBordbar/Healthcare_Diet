fbcdfee4f9ffb2b6b90de6d9e332de3f95359e83
MohammadMosavat <42.octmst@gmail.com>
2024-07-09 00:29:19 +0330

    responsive the bmi section in meditranean form

--- client/src/components/AdminComponents/DietBmi/index.tsx
+++ client/src/components/AdminComponents/DietBmi/index.tsx
@@ -12,15 +12,15 @@ function DietBmi({
     autoIncrementId,
   }: BmiType) {
   return (
-        <div className="grid grid-cols-4 pr-6 gap-4">
-            <p className="min-[1000px]:col-span-3 justify-center col-span-full text-[var(--black-blue)] w-fit ">
+        <div className="grid grid-cols-12 pr-6 gap-4">
+            <p className="min-[1000px]:col-span-6 justify-center col-span-full text-[var(--black-blue)] w-fit ">
             BMI: {bmi}
             </p>
-            <p className='min-[1000px]:col-span-3 text-base col-span-2'>سن: {age}</p>
-            <p className='min-[1000px]:col-span-3 text-base col-span-2'>قد: {height}</p>
-            <p className='min-[1000px]:col-span-3 text-base col-span-2'>وزن: {weight}</p>
-            <p className='min-[1000px]:col-span-3 text-base col-span-2'>دور شکم: {abdominalCircumference}</p>
-            <p className='col-span-full text-base'>دور  باسن: {hipcircumference}</p>
+            <p className='min-[1000px]:col-span-2 text-sm sm:text-base md:col-span-6 col-span-full'>سن: {age}</p>
+            <p className='min-[1000px]:col-span-2 text-sm sm:text-base md:col-span-6 col-span-full'>قد: {height}</p>
+            <p className='min-[1000px]:col-span-2 text-sm sm:text-base md:col-span-6 col-span-full'>وزن: {weight}</p>
+            <p className='min-[1000px]:col-span-2 text-sm sm:text-base md:col-span-6 col-span-full'>دور شکم: {abdominalCircumference}</p>
+            <p className='min-[1000px]:col-span-2 text-sm sm:text-base md:col-span-6 col-span-full'>دور  باسن: {hipcircumference}</p>
         </div>
   )
 }
