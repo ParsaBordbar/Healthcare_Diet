d8262e8a9c1cb2e234b48662277493163d6e88af
ParsaBordbar <parsab71@gmail.com>
2024-07-09 11:35:17 +0330

    Update the goToNextItems to fix the small bug.

--- client/src/components/bmiForms/index.tsx
+++ client/src/components/bmiForms/index.tsx
@@ -3,11 +3,9 @@ import useBmi from "@/validations/bmi/useBmi";
 import MainButton from "../MainButton";
 import MainInput from "../MainInput";
 import { Controller } from "react-hook-form";
-import { useContext, useEffect, useRef } from "react";
-import { AllContextType, useMyContext } from "@/hooks/useContext/Context";
+import { useEffect, useRef } from "react";
 
 const BmiFormDiet = () => {
-  // const { Bmi, setBmi } = useMyContext();
 
   const { control, handelValueInputs, register, errors, handleSubmit } =
     useBmi();
@@ -22,7 +20,6 @@ const BmiFormDiet = () => {
   const weightRef = useRef<HTMLInputElement | null>(null);
   const genderRef = useRef<HTMLSelectElement | null>(null);
 
-  // console.log(phoneNumberRef.current?.value, "value of phone number field");
 
   const inputRefs = [
     nameRef,
@@ -83,7 +80,7 @@ const BmiFormDiet = () => {
       onSubmit={handleSubmit(handelValueInputs)}
       className="flex w-full gap-6 sm:gap-10 flex-col justify-center"
     >
-      <h1 className="text-center text-[var(--black-blue)] font-bold text-2xl">
+      <h1 className="text-center text-[var(--black-blue)] font-extrabold text-2xl">
         اطلاعات خود را وارد کنید
       </h1>
       <section className="grid w-full grid-cols-4 gap-4">
@@ -117,20 +114,6 @@ const BmiFormDiet = () => {
           )}
         />
 
-        <Controller
-          control={control}
-          name="city"
-          render={({ field }) => (
-            <MainInput
-              {...field}
-              error={!!errors.city}
-              ref={cityRef}
-              parentClassName="col-span-4 md:col-span-2 lg:col-span-4"
-              label="شهر"
-              placeholder="شهر خود را وارد کنید"
-            />
-          )}
-        />
 
         <Controller
           control={control}
@@ -140,12 +123,13 @@ const BmiFormDiet = () => {
               {...field}
               error={!!errors.phoneNumber}
               ref={phoneNumberRef}
-              parentClassName="col-span-2 lg:col-span-2"
+              parentClassName="col-span-4 md:col-span-2 lg:col-span-4"
               label="شماره تماس"
               placeholder=" شماره تماس خود را وارد کنید"
             />
           )}
         />
+
         <Controller
           control={control}
           name="abdominalCircumference"
@@ -176,37 +160,7 @@ const BmiFormDiet = () => {
           )}
         />
 
-        <section className="col-span-2 flex flex-col gap-1">
-          <label
-            htmlFor="gender"
-            className={`block text-base sm:text-lg ${
-              errors.gender?.message ? "text-red-600" : "text-[var(--black-blue)]"
-            }`}
-          >
-            جنسیت
-          </label>
-          <Controller
-            control={control}
-            name="gender"
-            render={({ field }) => (
-              <select
-                {...field}
-                ref={genderRef}
-                className={`rounded-lg w-full outline-none   py-3 px-4 border ${
-                  errors.gender?.message
-                    ? "border-red-600"
-                    : "border-[var(--border-color)]"
-                }  flex items-center gap-1 text-base bg-white `}
-              >
-                <option value="null"  selected>
-                  جنسیت
-                </option>
-                <option value="مرد">مرد</option>
-                <option value="زن">زن</option>
-              </select>
-            )}
-          />
-        </section>
+       
 
         <Controller
           control={control}
@@ -240,7 +194,24 @@ const BmiFormDiet = () => {
           )}
         />
 
+
         <Controller
+          control={control}
+          name="city"
+          render={({ field }) => (
+            <MainInput
+              {...field}
+              error={!!errors.city}
+              ref={cityRef}
+              parentClassName="col-span-full sm:col-span-2"
+              label="شهر"
+              placeholder="شهر خود را وارد کنید"
+            />
+          )}
+        />
+
+
+      <Controller
           control={control}
           name="height"
           render={({ field }) => (
@@ -250,12 +221,42 @@ const BmiFormDiet = () => {
               error={!!errors.height}
               ref={heightRef}
               label="قد"
-              parentClassName="col-span-full sm:col-span-2"
+              parentClassName="col-span-2 lg:col-span-2"
               placeholder="(cm) قد خود را وارد کنید"
             />
           )}
         />
-
+ <section className="col-span-2 flex flex-col gap-1">
+          <label
+            htmlFor="gender"
+            className={`block text-base sm:text-lg ${
+              errors.gender?.message ? "text-red-600" : "text-[var(--black-blue)]"
+            }`}
+          >
+            جنسیت
+          </label>
+          <Controller
+            control={control}
+            name="gender"
+            render={({ field }) => (
+              <select
+                {...field}
+                ref={genderRef}
+                className={`rounded-lg w-full outline-none   py-3 px-4 border ${
+                  errors.gender?.message
+                    ? "border-red-600"
+                    : "border-[var(--border-color)]"
+                }  flex items-center gap-1 text-base bg-white `}
+              >
+                <option value="null"  selected>
+                  جنسیت
+                </option>
+                <option value="مرد">مرد</option>
+                <option value="زن">زن</option>
+              </select>
+            )}
+          />
+        </section>
         <MainButton
           modern
           type="submit"
