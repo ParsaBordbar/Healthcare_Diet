a0c4be5d8fc8e1d786096465b9051cce45aa5c69
ParsaBordbar <parsab71@gmail.com>
2024-07-13 12:02:29 +0330

    Update useFetchSpesificBmi

--- client/src/hooks/useFetchName/useFetchName.tsx
+++ client/src/hooks/useFetchName/useFetchName.tsx
@@ -2,29 +2,29 @@ import api from "@/apis";
 import { BmiType } from "@/types";
 import { useState, useEffect } from "react";
 
-const useSpecificFetchBmi = (phoneNumber: string) => {
+const useSpecificFetchBmi = (phoneNumber: string | undefined) => {
+  const [bmiForms, setBmiForms] = useState<BmiType | undefined>(undefined);
 
-    const [bmiForms, setBmiForms] = useState<BmiType>();
-    
-    useEffect(()=>{
-        fetchBmiPatientForms()
-    }, [])
-  
-    const fetchBmiPatientForms = async () => {
-      try{
-        console.log(bmiForms);
-          const response = await api.get(`/bmi/phone${phoneNumber}`);
-          setBmiForms(response.data);
+  useEffect(() => {
+    if (phoneNumber) {
+      fetchBmiPatientForms();
+    }
+  }, [phoneNumber]);
+
+  const fetchBmiPatientForms = async () => {
+    try {
+      const response = await api.get(`/bmi/phone${phoneNumber}`);
+      setBmiForms(response.data);
+    } catch (err:any) {
+      if (err.response) {
+        console.log(err.response.data);
+      } else {
+        console.log(`Error: ${err.message}`);
       }
-      catch(err : any){
-         if (err.response){
-          console.log(err.response.data)
-         }
-         else{
-          console.log(`Error: ${err.message}`);
-         }
-      }
-  }
+    }
+  };
+
   return bmiForms;
-}
+};
+
 export default useSpecificFetchBmi;
