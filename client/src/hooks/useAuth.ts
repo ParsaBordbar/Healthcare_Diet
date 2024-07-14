6e81e043158e91bc5c9474acb9edc973593dc7b1
ParsaBordbar <parsab71@gmail.com>
2024-07-14 19:04:38 +0330

    Update the new useAuth hook for build.

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -4,14 +4,16 @@ import { useEffect } from "react";
 
 const useAuth = () => {
   const nav = useRouter();
-    if (!localStorage.getItem("token")) {
-      console.log("check the token in local");
-      nav.push("/register/login/enterNumber");
-    }else{
-      console.log("check the token in local flase");
-
-    }
-  
+  useEffect(()=>{
+    if (typeof window !== "undefined") {
+      if (!localStorage.getItem("token")) {
+        console.log("check the token in local");
+        nav.push("/register/login/enterNumber");
+      }else{
+        console.log("Check the Token In LocalStorage.");
+      }
+  }
+  },[])
 };
 
 export default useAuth;
