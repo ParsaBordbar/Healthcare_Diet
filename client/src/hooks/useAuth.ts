67534791ac0574206199dce280ae3bd1d165a4da
ParsaBordbar <parsab71@gmail.com>
2024-07-14 19:39:37 +0330

    Update useAuth Hook to chek url with the Auth of user.

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -2,14 +2,31 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
+
+const getPhoneNumberFromUrl = (): string | null => {
+  if (typeof window !== "undefined") {
+    const pathSegments = window.location.pathname.split("/");
+    const phoneNumberIndex = pathSegments.indexOf("user") + 1;
+    return pathSegments[phoneNumberIndex] || null;
+  }
+  return null;
+};
+
 const useAuth = () => {
   const nav = useRouter();
   useEffect(()=>{
-    if (typeof window !== "undefined") {
-      if (!localStorage.getItem("token")) {
+
+    if (typeof window !== "undefined"){
+      const token = localStorage.getItem("token");
+      const user = localStorage.getItem('user');
+      if (!token) {
         console.log("check the token in local");
         nav.push("/register/login/enterNumber");
-      }else{
+      }else if (token && user){
+        if(user != getPhoneNumberFromUrl()){
+          nav.push("/register/login/enterNumber");
+        }
+      }else {
         console.log("Check the Token In LocalStorage.");
       }
   }
