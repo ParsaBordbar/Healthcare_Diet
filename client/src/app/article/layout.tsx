4fce7e7f42ef4fcfb2c37bc35920a7e78a85724f
ParsaBordbar <parsab71@gmail.com>
2024-07-15 10:50:34 +0330

    Clean code Article layout.

--- client/src/app/article/layout.tsx
+++ client/src/app/article/layout.tsx
@@ -1,12 +1,5 @@
 import Header from "@/components/Dr_Rabiee_Landing_components/Header";
 import { ReactNode } from "react";
-import Image from 'next/image';
-import HeaderPic from '/public/images/articleHeaderPic.jpg'
-import DocPhoto from "/public/images/dr.jpg";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import TelegramIcon from "@mui/icons-material/Telegram";
-
 
 const ArticleLayout = ({ children }: { children: ReactNode }) => {
   return (
