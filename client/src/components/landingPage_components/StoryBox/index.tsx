150a9ec393371751aa136675ff8e425a4fc5834c
ParsaBordbar <parsab71@gmail.com>
2024-07-15 10:48:49 +0330

    Update StroyBox component with Image and remove img.

--- client/src/components/landingPage_components/StoryBox/index.tsx
+++ client/src/components/landingPage_components/StoryBox/index.tsx
@@ -1,12 +1,21 @@
 import { TextBoxType } from "@/types";
 import Link from "next/link";
 import Arrow from "/public/svg/Arrow 1.svg";
+import Image from 'next/image';
 
-const StoryBox = ({ title, bodyText, img  , id}: TextBoxType) => {
+
+const StoryBox = ({ title, bodyText, img  , id, url}: TextBoxType) => {
   return (
     <div className="flex bg-[#e9eff0] flex-col rounded-2xl gap-4 py-4 px-2.5 justify-center items-center w-[280px] sm:w-[394px]">
       {/* <div className="w-fit">{img}</div> */}
-      <img src={img} alt="" className="rounded-3xl -mt-10" />
+      {/* <img src={img} alt="" className="rounded-3xl -mt-10" /> */}
+      <Image
+          src={img}
+          alt="Story Box Picture"
+          className="flex object-cover h-64"
+          width={5000}    
+          height={5000}
+      />
       <div className="flex justify-center gap -1 items-start flex-col px-2  ">
         <h3 className="sm:text-2xl text-xl text-[var(--black-blue)] font-semibold">
           {title}
@@ -16,7 +25,7 @@ const StoryBox = ({ title, bodyText, img  , id}: TextBoxType) => {
       <Link href={"./"} className="self-start px-6 py-2">
         <div className="flex sm:text-base text-sm items-center gap-3">
           <Arrow />
-          <Link href={'../../../article/mediterranean/'}>
+          <Link href={url}>
             <p>ادامه مطلب</p>
           </Link>
         </div>
