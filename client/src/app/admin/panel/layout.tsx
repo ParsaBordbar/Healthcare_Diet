5a8c47b193742a8224c28ad36c067c0121a450d4
MohammadMosavat <42.octmst@gmail.com>
2024-07-24 16:23:00 +0330

    when we don not have admin not allow to user or anyone to reach the admin panelg

--- client/src/app/admin/panel/layout.tsx
+++ client/src/app/admin/panel/layout.tsx
@@ -1,16 +1,22 @@
 "use client";
-import { ReactNode } from "react";
-import LogOutIcon from "/public/svg/userPanelSvgs/LogOut.svg"
+import { ReactNode, useEffect } from "react";
+import LogOutIcon from "/public/svg/userPanelSvgs/LogOut.svg";
 import AdminPanelLogo from "/public/svg/adminPanelSvgs/Stethoscope.svg";
 import FormIcon from "/public/svg/adminPanelSvgs/document.svg";
 import MedicalDocIcon from "/public/svg/adminPanelSvgs/Medical-Documentation.svg";
 import HamburgerNavbar from "@/components/HamburgerMenu";
 import SideBarItems from "@/components/SidebarItems";
-import { usePathname } from "next/navigation";
-
+import { usePathname, useRouter } from "next/navigation";
 
 const LayoutAdmin = ({ children }: { children: ReactNode }) => {
   const router = usePathname();
+  const nav = useRouter();
+
+  useEffect(() => {
+    if (!localStorage.getItem("admin")) {
+      nav.push('/admin')
+    }
+  }, []);
 
   return (
     <>
@@ -23,7 +29,7 @@ const LayoutAdmin = ({ children }: { children: ReactNode }) => {
           iconFour={FormIcon}
           iconFive={FormIcon}
           valueTwo="سوابق بیمار"
-          linkOne={"/admin/panel"}  
+          linkOne={"/admin/panel"}
           linkTwo={`/admin/panel/patients`}
           valueThree="فرم BMI"
           linkThree="/admin/panel/bmi"
@@ -35,7 +41,7 @@ const LayoutAdmin = ({ children }: { children: ReactNode }) => {
           linkSix="/admin/panel/comments"
           iconSix={FormIcon}
         />
-        
+
         <div className="fixed md:flex top-0 lg:w-[25%] md:w-[25%] xl:w-[15%] right-0 bg-[var(--soft-blue)] hidden h-screen">
           <section className="w-full flex flex-col gap-6 py-4">
             <h1 className="text-[var(--dark-blue)] text-2xl font-semibold bg-custom-IslamicArt">
@@ -45,47 +51,50 @@ const LayoutAdmin = ({ children }: { children: ReactNode }) => {
 
             <ul className="flex flex-col rounded-lg h-screen py-4 px-3  gap-4">
               <SideBarItems
-              className="lg:text-lg md:text-base"
+                className="lg:text-lg md:text-base"
                 router={router}
                 value={"پنل اصلی"}
                 href={"/admin/panel"}
                 icon={AdminPanelLogo}
               />
               <SideBarItems
-              className="lg:text-lg md:text-base"
+                className="lg:text-lg md:text-base"
                 router={router}
                 value={"سوابق بیمارها"}
                 href={"/admin/panel/patients"}
                 icon={MedicalDocIcon}
               />
               <SideBarItems
-              className="lg:text-lg md:text-base"
+                className="lg:text-lg md:text-base"
                 router={router}
                 value={"فرم های BMI"}
                 href={"/admin/panel/bmi"}
                 icon={FormIcon}
               />
               <SideBarItems
-              className="lg:text-lg md:text-base"
+                className="lg:text-lg md:text-base"
                 router={router}
                 value={"فرم رژیم مدیترانه ای"}
                 href={"/admin/panel/mediterranean"}
                 icon={FormIcon}
               />
               <SideBarItems
-              className="lg:text-lg md:text-base"
+                className="lg:text-lg md:text-base"
                 router={router}
                 value={"فرم مدیترانه ای پاسخ داده نشده"}
                 href={"/admin/panel/unvisited_diets"}
                 icon={FormIcon}
               />
               <SideBarItems
-              className="lg:text-lg md:text-base"
-              router={router}
-              value={"خروج"}
-              href={`/`}
-              icon={LogOutIcon}
-            />
+                onClick={() => {
+                  localStorage.removeItem("admin");
+                }}
+                className="lg:text-lg md:text-base"
+                router={router}
+                value={"خروج"}
+                href={`/`}
+                icon={LogOutIcon}
+              />
             </ul>
           </section>
         </div>
