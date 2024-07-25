6a16bff76f5020b18c943cf7545baed1aa9868e3
MohammadMosavat <42.octmst@gmail.com>
2024-07-25 13:25:18 +0330

    use Imafge from next for banner image in login layout

--- client/src/app/register/layout.tsx
+++ client/src/app/register/layout.tsx
@@ -1,12 +1,20 @@
-// "use client";
+"use client";
 import { ReactNode } from "react";
 import Navbar from "@/components/landingPage_components/Navbar";
-
+import Image from "next/image";
+import BannerImage from 'public/svg/loginBanner.svg'
 const LayoutRegister = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <div className="relative top-0 z-40">
-        <main className="flex !bg-cover md:!bg-contain bg-no-repeat !bg-none  bg-[var(--soft-blue)] lg:!bg-[url('/svg/loginBanner.svg')] h-screen items-center w-full justify-center lg:justify-end">
+        <main className="flex   bg-[var(--soft-blue)] h-screen items-center w-full justify-center lg:justify-end">
+          <Image
+            src={'/svg/loginBanner.svg'}
+            alt="login banner"
+            className="hidden lg:inline-block md:object-contain"
+            width={900}
+            height={0}
+          />
           {children}
         </main>
       </div>
