7e3a8274406b85ddcaf3d507461088b7abfd5ffd
MohammadMosavat <42.octmst@gmail.com>
2024-07-26 14:47:38 +0330

    set navigation thing in use comment hook , used in replay box and now when admin send message to user goes back to the previse page

--- client/src/hooks/useComment/useComment.ts
+++ client/src/hooks/useComment/useComment.ts
@@ -1,15 +1,16 @@
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useCallback } from 'react';
-import { toast } from 'react-toastify';
-import * as yup from 'yup';
-import api from '@/apis'; 
-import { CommentType, CommentTypeHook } from '@/types';
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useCallback } from "react";
+import { toast } from "react-toastify";
+import * as yup from "yup";
+import api from "@/apis";
+import { CommentType, CommentTypeHook } from "@/types";
+import { useRouter } from "next/navigation";
 
 const CommentSchema = yup.object().shape({
-  sender: yup.string().required('Sender is required'),
-  body: yup.string().required('لطفا متن پیام را اضافه کنید'),
-  receiver: yup.string().required('Receiver is required'),
+  sender: yup.string().required("Sender is required"),
+  body: yup.string().required("لطفا متن پیام را اضافه کنید"),
+  receiver: yup.string().required("Receiver is required"),
 });
 
 const useComment = () => {
@@ -22,30 +23,37 @@ const useComment = () => {
   } = useForm<CommentTypeHook>({
     resolver: yupResolver(CommentSchema),
   });
+  const nav = useRouter();
+  const handleValueInputs: SubmitHandler<CommentType> = useCallback(
+    async (formData) => {
+      try {
+        const response = await api.post(
+          "/uploader/upload/type?type=comments",
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
 
-  const handleValueInputs: SubmitHandler<CommentType> = useCallback(async (formData) => {
-    
-    try {
-      const response = await api.post('/uploader/upload/type?type=comments', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-
-      if (response.status === 200) {
-        toast.success('پیام با موفقیت ارسال شد');
-      } else {
-        throw new Error('Upload failed');
+        if (response.status === 200) {
+          toast.success("پیام با موفقیت ارسال شد");
+          nav.back()
+        } else {
+          throw new Error("Upload failed");
+        }
+      } catch (error) {
+        console.error("مشکلی در ارسال پیام پیش آمد", error);
+        toast.error("مشکلی در ارسال پیام پیش آمد");
       }
-    } catch (error) {
-      console.error('مشکلی در ارسال پیام پیش آمد', error);  
-      toast.error('مشکلی در ارسال پیام پیش آمد');
-    }
-  }, []);
+    },
+    []
+  );
 
   return {
     control,
-    handleValueInputs,  
+    handleValueInputs,
     register,
     errors,
     handleSubmit,
