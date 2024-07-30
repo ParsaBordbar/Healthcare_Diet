90fa5605665a1c497a7d1abc82b7e87b4eb2a040
ParsaBordbar <parsab71@gmail.com>
2024-07-31 00:21:22 +0330

    Update QucickLink Section Counters.

--- client/src/components/AdminComponents/QuickLinkSection/index.tsx
+++ client/src/components/AdminComponents/QuickLinkSection/index.tsx
@@ -26,7 +26,7 @@ function QuickLinSection() {
             <QuickLinkBox tittle={'بیمارها'} desc={'سوابق و مشخصات تمام بیمارها'} url={"/admin/panel/patients"} counter={bmiCount} /> 
         </li>
         <li className='lg:col-span-1 md:col-span-2 col-span-full' >
-            <QuickLinkBox tittle={'فایل‌ها'} desc={'فایل‌های آپلود شده شما'} url={"/admin/panel/comments"} counter={0} />
+            <QuickLinkBox tittle={'فایل‌ها'} desc={'فایل‌های آپلود شده شما'} url={"/admin/panel/comments"} counter={commentCount} />
         </li>
     </ul>
   )
