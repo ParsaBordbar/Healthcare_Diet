d790032839f1100ad6b14a1145539705fb2c5086
ParsaBordbar <parsab71@gmail.com>
2024-07-30 23:39:20 +0330

    Update UseAuth Bugs and Redirct Bug.

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,6 +1,7 @@
-"use client";
+'use client'
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+
 
 const getPhoneNumberFromUrl = (): string | null => {
   if (typeof window !== "undefined") {
@@ -13,26 +14,25 @@ const getPhoneNumberFromUrl = (): string | null => {
 
 const useAuth = () => {
   const nav = useRouter();
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const token = localStorage.getItem("token");
-      const user = localStorage.getItem("user");
-      const newUser = localStorage.getItem("new_user");
+  useEffect(()=>{
 
-      if (!token) {
+    if (typeof window !== "undefined"){
+      const token = localStorage.getItem("token");
+      const user = localStorage.getItem('user');
+      const tempToken = localStorage.getItem('temp_token')
+
+      if (!token || tempToken) {
         console.log("check the token in local");
         nav.push("/register/login/enterNumber");
-      } else if (token && (user || newUser)) {
-        if (user != getPhoneNumberFromUrl()) {
+      }else if (token && user){
+        if(user != getPhoneNumberFromUrl()){
           nav.push("/register/login/enterNumber");
         }
-        if (newUser != getPhoneNumberFromUrl()) {
-          nav.push("/register/login/enterNumber");
-        }
-      } else {
+      }else {
         console.log("Check the Token In LocalStorage.");
       }
-    }
-  }, []);
+  }
+  },[])
 };
+
 export default useAuth;
