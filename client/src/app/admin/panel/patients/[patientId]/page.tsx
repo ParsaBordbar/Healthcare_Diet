566829952636bc18da003b033f3d4893e3ea25dd
MohammadMosavat <42.octmst@gmail.com>
2024-07-30 13:34:34 +0330

    add the perivies version of the patient page in admin panel

--- client/src/app/admin/panel/patients/[patientId]/page.tsx
+++ client/src/app/admin/panel/patients/[patientId]/page.tsx
@@ -112,26 +112,22 @@ function PatientId({ params }: { params: { patientId: string } }) {
         <h2 className="text-xl md:text-2xl lg:text-3xl mt-8">
           پیام‌های ارسال شده:
         </h2>
-        <div className="grid grid-cols-2 gap-4">
-          {commentData ? (
-            commentData.map((comment) => (
-              <div className="lg:col-span-1 col-span-full">
-                <CommentBox
-                  key={comment.receiver}
-                  sender={comment.sender}
-                  body={comment.body}
-                  receiver={comment.receiver}
-                  createdAtJalali={comment.createdAtJalali}
-                  isDoctor={true}
-                  files={comment.files}
-                  _id={comment._id}
-                />
-              </div>
-            ))
-          ) : (
-            <p>هنوز پیامی وجود ندارد</p>
-          )}
-        </div>
+        {commentData ? (
+          commentData.map((comment) => (
+            <CommentBox
+              key={comment.receiver}
+              sender={comment.sender}
+              body={comment.body}
+              receiver={comment.receiver}
+              createdAtJalali={comment.createdAtJalali}
+              isDoctor={true}
+              files={comment.files}
+              _id={comment._id}
+            />
+          ))
+        ) : (
+          <p>هنوز پیامی وجود ندارد</p>
+        )}
         <ReplyBox receiver={params.patientId} />
       </section>
     </main>
