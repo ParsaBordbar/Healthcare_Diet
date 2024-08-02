221a5fa91b7460905e5b312e57b4d916c00507f6
MohammadMosavat <42.octmst@gmail.com>
2024-08-02 18:14:53 +0330

    slice the comment and medi form in patient page of admin panel and add link to go to specific page of comment and medi form

--- client/src/app/admin/panel/patients/[patientId]/page.tsx
+++ client/src/app/admin/panel/patients/[patientId]/page.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+"use client";;
 import CommentBox from "@/components/AdminComponents/CommentBox";
 import PatientBmiFormLazy from "@/components/AdminComponents/lazyLoadingComponents/PatientBmiForm";
 import PatientCardLazy from "@/components/AdminComponents/lazyLoadingComponents/PatientCardLazy";
@@ -10,7 +9,7 @@ import ReplyBox from "@/components/AdminComponents/ReplyBox";
 import useSpecificFetchBmi from "@/hooks/useFetchName/useFetchName";
 import useFetchOneMediterranean from "@/hooks/useFetchOneMediterranean";
 import useFetchPatientComments from "@/hooks/useFetchPatientComments/useFetchPatientComments";
-import React from "react";
+import Link from "next/link";
 
 function PatientId({ params }: { params: { patientId: string } }) {
   const bmiData = useSpecificFetchBmi(params.patientId);
@@ -58,75 +57,94 @@ function PatientId({ params }: { params: { patientId: string } }) {
             <PatientBmiFormLazy />
           </>
         )}
-
-        <h2 className="text-xl md:text-2xl lg:text-3xl mt-8">رژیم‌ها:</h2>
-        {medData.length > 0 ? (
-          medData.map((form) => (
-            <MediterraneanForm
-              key={`${params.patientId} ${form.createdAtJalali}`}
-              phoneNumber={params.patientId}
-              isChecked={form?.isChecked}
-              dailyFruit={form.dailyFruit}
-              dailyVegetable={form.dailyVegetable}
-              Cereals={form.Cereals}
-              dailyCereals={form.dailyCereals}
-              potatoAndStarchWeekly={form.potatoAndStarchWeekly}
-              oliveAndOliveOilDaily={form.oliveAndOliveOilDaily}
-              nutsDaily={form.nutsDaily}
-              dairyDaily={form.dairyDaily}
-              beans={form.beans}
-              eggWeekly={form.eggWeekly}
-              fishWeekly={form.fishWeekly}
-              chickensWeekly={form.chickensWeekly}
-              sugarWeekly={form.sugarWeekly}
-              alcoholWeekly={form.alcoholWeekly}
-              fermentationWeekly={form.fermentationWeekly}
-              supplements={form.sugarWeekly}
-              physicalActivity={form.physicalActivity}
-              diabetes={form.diabetes}
-              anemia={form.anemia}
-              bloodPressure={form.bloodPressure}
-              digestiveProblems={form.digestiveProblems}
-              selfSafety={form.selfSafety}
-              stroke={form.stroke}
-              fattyLiver={form.fattyLiver}
-              kidneyProblems={form.kidneyProblems}
-              thyroid={form.thyroid}
-              cancer={form.cancer}
-              Migraine={form.Migraine}
-              otherSickness={form.otherSickness}
-              medicine={form.medicine}
-              foodAllergies={form.foodAllergies}
-              pregnancyWeeks={form.pregnancyWeeks}
-              breastfeeding={form.breastfeeding}
-              weightBeforePregnancy={form.weightBeforePregnancy}
-              createdAtJalali={form.createdAtJalali}
-              files={form.files}
-              redMeatWeekly={form.redMeatWeekly}
-              dietBmi={form.dietBmi}
-              payment={form.payment}
-              _id={form._id}
-            />
-          ))
+        <section className="flex items-center  mt-8 justify-between">
+          <h2 className="text-xl md:text-2xl lg:text-3xl">رژیم‌ها:</h2>
+          <Link
+            href={`/admin/panel/patients/${params.patientId}/mediterranean`}
+            className="text-[var(--new-green)] text-xl cursor-pointer"
+          >
+            دیدن همه ی رژیم ها
+          </Link>
+        </section>
+        {medData?.length > 0 ? (
+          medData
+            .slice(0, 2)
+            .map((form) => (
+              <MediterraneanForm
+                key={`${params.patientId} ${form.createdAtJalali}`}
+                phoneNumber={params.patientId}
+                isChecked={form?.isChecked}
+                dailyFruit={form.dailyFruit}
+                dailyVegetable={form.dailyVegetable}
+                Cereals={form.Cereals}
+                dailyCereals={form.dailyCereals}
+                potatoAndStarchWeekly={form.potatoAndStarchWeekly}
+                oliveAndOliveOilDaily={form.oliveAndOliveOilDaily}
+                nutsDaily={form.nutsDaily}
+                dairyDaily={form.dairyDaily}
+                beans={form.beans}
+                eggWeekly={form.eggWeekly}
+                fishWeekly={form.fishWeekly}
+                chickensWeekly={form.chickensWeekly}
+                sugarWeekly={form.sugarWeekly}
+                alcoholWeekly={form.alcoholWeekly}
+                fermentationWeekly={form.fermentationWeekly}
+                supplements={form.sugarWeekly}
+                physicalActivity={form.physicalActivity}
+                diabetes={form.diabetes}
+                anemia={form.anemia}
+                bloodPressure={form.bloodPressure}
+                digestiveProblems={form.digestiveProblems}
+                selfSafety={form.selfSafety}
+                stroke={form.stroke}
+                fattyLiver={form.fattyLiver}
+                kidneyProblems={form.kidneyProblems}
+                thyroid={form.thyroid}
+                cancer={form.cancer}
+                Migraine={form.Migraine}
+                otherSickness={form.otherSickness}
+                medicine={form.medicine}
+                foodAllergies={form.foodAllergies}
+                pregnancyWeeks={form.pregnancyWeeks}
+                breastfeeding={form.breastfeeding}
+                weightBeforePregnancy={form.weightBeforePregnancy}
+                createdAtJalali={form.createdAtJalali}
+                files={form.files}
+                redMeatWeekly={form.redMeatWeekly}
+                dietBmi={form.dietBmi}
+                payment={form.payment}
+                _id={form._id}
+              />
+            ))
         ) : (
           <p className="text-xl text-center">رژیم ندارد</p>
         )}
-        <h2 className="text-xl md:text-2xl lg:text-3xl mt-8">
-          پیام‌های ارسال شده:
-        </h2>
-        {commentData ? (
-          commentData.map((comment) => (
-            <CommentBox
-              key={comment._id}
-              sender={comment.sender}
-              body={comment.body}
-              receiver={comment.receiver}
-              createdAtJalali={comment.createdAtJalali}
-              isDoctor={true}
-              files={comment.files}
-              _id={comment._id}
-            />
-          ))
+        <section className="flex items-center mt-8 justify-between">
+          <h2 className="text-xl md:text-2xl lg:text-3xl ">
+            پیام‌های ارسال شده:
+          </h2>
+          <Link
+            href={`/admin/panel/patients/${params.patientId}/comments`}
+            className="text-[var(--new-green)] text-xl cursor-pointer"
+          >
+            دیدن همه ی پیام ها
+          </Link>
+        </section>
+        {commentData?.length > 0 ? (
+          commentData
+            .slice(0, 2)
+            .map((comment) => (
+              <CommentBox
+                key={comment._id}
+                sender={comment.sender}
+                body={comment.body}
+                receiver={comment.receiver}
+                createdAtJalali={comment.createdAtJalali}
+                isDoctor={true}
+                files={comment.files}
+                _id={comment._id}
+              />
+            ))
         ) : (
           <p>هنوز پیامی وجود ندارد</p>
         )}
