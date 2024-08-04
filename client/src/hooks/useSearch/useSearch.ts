1482c61011980559b4591ef02d941e1343ec8169
MohammadMosavat <42.octmst@gmail.com>
2024-08-04 21:35:57 +0330

    chunking array of result of useSearch hook that hook using for search patient i think

--- client/src/hooks/useSearch/useSearch.ts
+++ client/src/hooks/useSearch/useSearch.ts
@@ -1,56 +1,67 @@
-'use client'
-import { useEffect, useState } from 'react'
-import useFetchComments from '@/hooks/useFetchComments/useFetchComments'
-import useFilterComments from '@/hooks/useFilterComments/useFilterComments'
-import useFetchSingleBmi from '@/hooks/useFetchSingleBmi/useFetchSingleBmi'
-import api from '@/apis'
+"use client";
+import { useEffect, useState } from "react";
+import useFetchComments from "@/hooks/useFetchComments/useFetchComments";
+import useFilterComments from "@/hooks/useFilterComments/useFilterComments";
+import useFetchSingleBmi from "@/hooks/useFetchSingleBmi/useFetchSingleBmi";
+import api from "@/apis";
+import { CommentType } from "@/types";
+import useFetchPatientComments from "../useFetchPatientComments/useFetchPatientComments";
+import { chunkingArray } from "../chunkingArray";
 
 const useSearchCommentsPage = () => {
-  const allComments = useFetchComments()
-  const [filter, setFilter] = useState(allComments)
-  const oldest = useFilterComments('/doctorsComment/comments?sort=newest')
-  const newest = useFilterComments('/doctorsComment/comments?sort=oldest')
-  const [searchValue, setSearchValue] = useState<string>('')
+  const allComments = useFetchComments();
+  const allComments2 = useFetchComments();
+  const allComments3 = useFetchComments();
+  const [filter, setFilter] = useState<any>(allComments);
+  const oldest = useFilterComments("/doctorsComment/comments?sort=newest");
+  const newest = useFilterComments("/doctorsComment/comments?sort=oldest");
+  const [searchValue, setSearchValue] = useState<string>("");
+  console.log(oldest);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log('Event value:', event.target.value)
-    setSearchValue(event.target.value)
-  }
+    console.log("Event value:", event.target.value);
+    setSearchValue(event.target.value);
+  };
 
   useEffect(() => {
-    console.log('Search value updated:', searchValue)
-  }, [searchValue])
+    console.log("Search value updated:", searchValue);
+  }, [searchValue]);
 
-  const fetchBmi = useFetchSingleBmi(searchValue)
-  
-  
-  const newestFilterHandler = () => {
-    setFilter(newest)
+  const fetchBmi = useFetchSingleBmi(searchValue);
+
+  function newestFilterHandler(fetchFunction: CommentType[]) {
+    console.log("this is newset array ", newest, fetchFunction.length);
+    const newArrayComment = chunkingArray(newest, 10);
+    setFilter(newArrayComment);
   }
 
-  const oldestFilterHandler = () => {
-    setFilter(oldest)
+  function oldestFilterHandler(fetchFunction: CommentType[]) {
+    console.log("this is olders array ", oldest, fetchFunction.length);
+    const newArrayComment = chunkingArray(oldest, 10);
+    setFilter(newArrayComment);
   }
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const results = []
+    event.preventDefault();
+    const results = [];
     for (const patient of fetchBmi) {
-      const endpoint = `/doctorsComment/certain/patientId/${patient.phoneNumber}`
-      console.log('This is endpoint', endpoint)
+      const endpoint = `/doctorsComment/certain/patientId/${patient.phoneNumber}`;
+      console.log("This is endpoint", endpoint);
       try {
-        const response = await api.get(endpoint)
-        results.push(...response.data)
+        const response = await api.get(endpoint);
+        results.push(...response.data);
       } catch (error) {
-        console.error('Error fetching search results:', error)
+        console.error("Error fetching search results:", error);
       }
     }
-    setFilter(results)
-  }
+    const newArrayComment = chunkingArray(results.toReversed(), 10);
+    setFilter(newArrayComment);
+  };
 
   useEffect(() => {
-    setFilter(allComments)
-  }, [allComments])
+    const newArrayComment = chunkingArray(allComments.toReversed(), 10);
+    setFilter(newArrayComment);
+  }, [allComments]);
 
   return {
     filter,
@@ -59,7 +70,7 @@ const useSearchCommentsPage = () => {
     handleSubmit,
     newestFilterHandler,
     oldestFilterHandler,
-  }
-}
+  };
+};
 
-export default useSearchCommentsPage
+export default useSearchCommentsPage;
