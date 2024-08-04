a411345085a543aa0d9ff680464855ffcc8e97ab
MohammadMosavat <42.octmst@gmail.com>
2024-08-04 21:34:39 +0330

    chunking the result of the hook of useSearchBMI and use it for admin panel

--- client/src/hooks/useSearchBmi/useSearchBmi.ts
+++ client/src/hooks/useSearchBmi/useSearchBmi.ts
@@ -1,81 +1,86 @@
-'use client'
-import { useEffect, useState } from 'react'
-import useFetchBmi from '../useFetchBmi'
-import api from '@/apis'
-import { BmiType } from '@/types'
+"use client";
+import { useEffect, useState } from "react";
+import useFetchBmi from "../useFetchBmi";
+import api from "@/apis";
+import { BmiType } from "@/types";
+import { chunkingArray } from "../chunkingArray";
 
 const useSearchBmi = (filterUrl: string) => {
-  const bmi = useFetchBmi()
+  const bmi = useFetchBmi();
   const [filter, setFilter] = useState<BmiType[]>(bmi);
-  const [searchValue, setSearchValue] = useState<string>('')
+  const [searchValue, setSearchValue] = useState<string>("");
 
   useEffect(() => {
     const fetchInitialData = async () => {
-      const data: BmiType[] = await fetchFilteredData(`${filterUrl}?sort=newest`);
-      setFilter(data);
+      const data: BmiType[] = await fetchFilteredData(
+        `${filterUrl}?sort=newest`
+      );
+      const newArrayChunk = chunkingArray(data, 12);
+      setFilter(newArrayChunk);
     };
     fetchInitialData();
   }, []);
 
-  useEffect(() => {
-    console.log(filter);
-  }, [filter]);
-
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log('Event value:', event.target.value)
-    setSearchValue(event.target.value)
-  }
+    console.log("Event value:", event.target.value);
+    setSearchValue(event.target.value);
+  };
 
   const fetchFilteredData = async (url: string) => {
     try {
-      const response = await api.get(url)
-      return response.data
+      const response = await api.get(url);
+      return response.data;
     } catch (error) {
-      console.error('Error fetching filtered data:', error)
-      return []
+      console.error("Error fetching filtered data:", error);
+      return [];
     }
-  }
+  };
 
   const fetchSearchData = async (query: string) => {
     try {
-      const response = await api.get(`/bmi/search?query=${query}`)
-      return response.data
+      const response = await api.get(`/bmi/search?query=${query}`);
+      return response.data;
     } catch (error) {
-      console.error('Error fetching search data:', error)
-      return []
+      console.error("Error fetching search data:", error);
+      return [];
     }
-  }
+  };
 
   const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+    event.preventDefault();
     const trimmedSearchValue = searchValue.trim();
-    if (!trimmedSearchValue) {  
-      newestFilterHandler()
+    if (!trimmedSearchValue) {
+      newestFilterHandler();
       return;
     }
-    const searchData = await fetchSearchData(trimmedSearchValue)
-    setFilter(searchData)
-  }
+    const searchData = await fetchSearchData(trimmedSearchValue);
+    const newArrayChunk = chunkingArray(searchData, 12);
+    setFilter(newArrayChunk);
+  };
 
   const newestFilterHandler = async () => {
-    const data: BmiType[] = await fetchFilteredData(`${filterUrl}?sort=newest`)
-    setFilter(data)
-  }
+    const data: BmiType[] = await fetchFilteredData(`${filterUrl}?sort=newest`);
+    const newArrayChunk = chunkingArray(data, 12);
+    setFilter(newArrayChunk);
+  };
 
   const oldestFilterHandler = async () => {
-    const data = await fetchFilteredData(`${filterUrl}?sort=oldest`)
-    setFilter(data)
-  }
+    const data = await fetchFilteredData(`${filterUrl}?sort=oldest`);
+    const newArrayChunk = chunkingArray(data, 12);
+    setFilter(newArrayChunk);
+  };
 
   const maleFilterHandler = async () => {
-    const data = await fetchFilteredData(`${filterUrl}?sort=male`)
-    setFilter(data)
-  }
+    const data = await fetchFilteredData(`${filterUrl}?sort=male`);
+    const newArrayChunk = chunkingArray(data, 12);
+    setFilter(newArrayChunk);
+  };
 
   const femaleFilterHandler = async () => {
-    const data = await fetchFilteredData(`${filterUrl}?sort=female`)
-    setFilter(data)
-  }
+    const data = await fetchFilteredData(`${filterUrl}?sort=female`);
+    const newArrayChunk = chunkingArray(data, 12);
+    setFilter(newArrayChunk);
+  };
 
   return {
     filter,
@@ -86,7 +91,7 @@ const useSearchBmi = (filterUrl: string) => {
     maleFilterHandler,
     femaleFilterHandler,
     submitHandler,
-  }
-}
+  };
+};
 
-export default useSearchBmi
+export default useSearchBmi;
