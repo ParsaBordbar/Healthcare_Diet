1160aa777da4e6a723100ed0d0ed404d4adaf412
MohammadMosavat <42.octmst@gmail.com>
2024-08-04 21:47:17 +0330

    add pagination to the commet page of patient in admin panel

--- client/src/app/admin/panel/comments/page.tsx
+++ client/src/app/admin/panel/comments/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import CommentBox from "@/components/AdminComponents/CommentBox";
 import MainInput from "@/components/MainInput";
 import SearchIcon from "/public/svg/search-normal.svg";
@@ -7,6 +7,8 @@ import useSearchCommentsPage from "@/hooks/useSearch/useSearch";
 import MainButton from "@/components/MainButton";
 import FilterIcon from "/public/svg/adminPanelSvgs/filter.svg";
 import DocumentIcon from "/public/svg/adminPanelSvgs/document.svg";
+import { CommentType } from "@/types";
+import useFetchComments from "@/hooks/useFetchComments/useFetchComments";
 
 const CommentsPage = () => {
   const {
@@ -17,6 +19,33 @@ const CommentsPage = () => {
     newestFilterHandler,
     oldestFilterHandler,
   } = useSearchCommentsPage();
+  const allComments = useFetchComments();
+  const [arrayItemsComment, setArrayItemsComment] = useState<number>(0);
+
+  const paginationComment = useCallback(() => {
+    console.log("filter in pagination func", filter);
+    return filter.map((item: any, index: number) => {
+      return (
+        item.length > 0 &&
+        filter[1]?.length > 0 && (
+          <button
+            type="button"
+            autoFocus={index == 0 ? true : false}
+            onClick={(e) => {
+              console.log(e.target);
+              setArrayItemsComment(index);
+            }}
+            className={`p-4 hover:underline bg-[var(--milky-white)] hover:bg-[var(--new-green)] border-none outline-none hover:text-[var(--milky-white)] ${
+              arrayItemsComment + 1 == index + 1 &&
+              "bg-[var(--new-green)] text-[var(--milky-white)] underline"
+            } `}
+          >
+            {index + 1}
+          </button>
+        )
+      );
+    });
+  }, [filter, arrayItemsComment]);
 
   return (
     <section className="flex gap-3 flex-col">
@@ -47,40 +76,56 @@ const CommentsPage = () => {
           <section className=" col-span-full md:col-span-2 grid grid-cols-2 gap-8 w-full">
             <MainButton
               modern
-              onClick={newestFilterHandler}
+              type="button"
+              onClick={() => {
+                console.log("run un newsest");
+                setArrayItemsComment(0);
+                oldestFilterHandler(allComments);
+              }}
               className="rounded-lg col-span-1 py-3 px-4 !text-sm"
-              value={"قدیمی ترین"}
+              value={"جدید ترین"}
             />
 
             <MainButton
               modern
-              onClick={oldestFilterHandler}
+              type="button"
+              onClick={() => {
+                setArrayItemsComment(0);
+                newestFilterHandler(allComments);
+              }}
               className="rounded-lg col-span-1 py-3 px-4 !text-sm"
-              value={" جدیدترین"}
+              value={"قدیمی ترین"}
             />
           </section>
         </div>
       </header>
       <main className="grid grid-cols-2 gap-8">
         {filter
-          ? filter.map((data): React.ReactNode => {
-              console.log(data);
-              return (
-                <CommentBox
-                  _id={data._id}
-                  className="sm:!col-span-1"
-                  key={data._id}
-                  sender={data.sender}
-                  body={data.body}
-                  receiver={data.receiver}
-                  createdAtJalali={data.createdAtJalali}
-                  isDoctor={true}
-                  files={data.files}
-                />
-              );
-            })
+          ? filter[arrayItemsComment]?.map(
+              (data: CommentType): React.ReactNode => {
+                return (
+                  <CommentBox
+                    _id={data._id}
+                    className="sm:!col-span-1"
+                    key={data._id}
+                    sender={data.sender}
+                    body={data.body}
+                    receiver={data.receiver}
+                    createdAtJalali={data.createdAtJalali}
+                    isDoctor={true}
+                    files={data.files}
+                  />
+                );
+              }
+            )
           : null}
       </main>
+      <div
+        role="group"
+        className="flex items-center w-fit custom-scroll-x justify-center mx-auto overflow-hidden rounded-lg"
+      >
+        {paginationComment()}
+      </div>
     </section>
   );
 };
