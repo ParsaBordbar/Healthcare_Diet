6021c18a367719564d9c2358680d40825efa5dd1
MohammadMosavat <42.octmst@gmail.com>
2024-08-05 18:48:11 +0330

    Add pagination into the unvisited med form page in admin panel

--- client/src/app/admin/panel/unvisited_diets/page.tsx
+++ client/src/app/admin/panel/unvisited_diets/page.tsx
@@ -5,92 +5,14 @@ import React, { useCallback, useState } from "react";
 import DocumentIcon from "/public/svg/adminPanelSvgs/document.svg";
 import { chunkingArray } from "@/hooks/chunkingArray";
 import { MediterraneanFormType } from "@/types";
+import Pagination from "@/components/Pagination";
 
 function UnvisitedDiets() {
   const mediterraneanForms = useFetchMediterranean("/sort?sort=unVisited");
+  const newArray = chunkingArray(mediterraneanForms, 2);
+  const [calcPlues, setCalcPlues] = useState<number>(0);
   const [arrayItemsComment, setArrayItemsComment] = useState<number>(0);
   console.log("medi array", mediterraneanForms);
-
-  const paginationComment = useCallback(() => {
-    return mediterraneanForms.map((item: any, index: number) => {
-      return (
-        item.length > 0 &&
-        mediterraneanForms[1]?.length > 0 && (
-          <button
-            type="button"
-            autoFocus={index == 0 ? true : false}
-            onClick={(e) => {
-              console.log(e.target);
-              setArrayItemsComment(index);
-            }}
-            className={`p-4 hover:underline bg-[var(--milky-white)] hover:bg-[var(--new-green)] border-none outline-none hover:text-[var(--milky-white)] ${
-              arrayItemsComment + 1 == index + 1 &&
-              "bg-[var(--new-green)] text-[var(--milky-white)] underline"
-            } `}
-          >
-            {index + 1}
-          </button>
-        )
-      );
-    });
-  }, [mediterraneanForms]);
-
-  // const renderMedi = useCallback(() => {
-  //   return (
-  //     mediterraneanForms.length > 0 &&
-  //     mediterraneanForms[arrayItemsComment].map(
-  //       (form: MediterraneanFormType) => {
-  //         return (
-  //           <MediterraneanForm
-  //             dailyFruit={form.dailyFruit}
-  //             dailyVegetable={form.dailyVegetable}
-  //             Cereals={form.Cereals}
-  //             dailyCereals={form.dailyCereals}
-  //             potatoAndStarchWeekly={form.potatoAndStarchWeekly}
-  //             oliveAndOliveOilDaily={form.oliveAndOliveOilDaily}
-  //             nutsDaily={form.nutsDaily}
-  //             dairyDaily={form.dairyDaily}
-  //             beans={form.beans}
-  //             eggWeekly={form.eggWeekly}
-  //             fishWeekly={form.fishWeekly}
-  //             chickensWeekly={form.chickensWeekly}
-  //             sugarWeekly={form.sugarWeekly}
-  //             alcoholWeekly={form.alcoholWeekly}
-  //             fermentationWeekly={form.fermentationWeekly}
-  //             supplements={form.sugarWeekly}
-  //             physicalActivity={form.physicalActivity}
-  //             diabetes={form.diabetes}
-  //             anemia={form.anemia}
-  //             bloodPressure={form.bloodPressure}
-  //             digestiveProblems={form.digestiveProblems}
-  //             selfSafety={form.selfSafety}
-  //             stroke={form.stroke}
-  //             fattyLiver={form.fattyLiver}
-  //             kidneyProblems={form.kidneyProblems}
-  //             thyroid={form.thyroid}
-  //             cancer={form.cancer}
-  //             Migraine={form.Migraine}
-  //             otherSickness={form.otherSickness}
-  //             medicine={form.medicine}
-  //             phoneNumber={form.phoneNumber}
-  //             key={form.phoneNumber}
-  //             isChecked={form.isChecked}
-  //             files={form.files}
-  //             redMeatWeekly={form.redMeatWeekly}
-  //             createdAtJalali={form.createdAtJalali}
-  //             payment={form.payment}
-  //             dietBmi={form.dietBmi}
-  //             foodAllergies={form.foodAllergies}
-  //             pregnancyWeeks={form.pregnancyWeeks}
-  //             breastfeeding={form.breastfeeding}
-  //             _id={form._id}
-  //             weightBeforePregnancy={form.weightBeforePregnancy}
-  //           />
-  //         );
-  //       }
-  //     )
-  //   );
-  // }, [arrayItemsComment]);
 
   return (
     <>
@@ -101,8 +23,8 @@ function UnvisitedDiets() {
             فرم‌های رژیم مدیترانه‌ای پاسخ داده نشده
           </h1>
         </section>
-        {mediterraneanForms.length > 0 &&
-          mediterraneanForms[arrayItemsComment].map(
+        {newArray.length > 0 &&
+          newArray[arrayItemsComment].map(
             (form: MediterraneanFormType) => {
               return (
                 <MediterraneanForm
@@ -154,12 +76,15 @@ function UnvisitedDiets() {
             }
           )}
       </main>
-      <div
-        role="group"
-        className="flex items-center w-fit custom-scroll-x justify-center mx-auto overflow-hidden rounded-lg"
-      >
-        {paginationComment()}
-      </div>
+      <Pagination
+        arr={newArray}
+        itemNumber={arrayItemsComment}
+        setItemNumber={setArrayItemsComment}
+        setCalc={setCalcPlues}
+        calc={calcPlues}
+        limitNumber={20}
+        lastCalc={3}
+      />
     </>
   );
 }
