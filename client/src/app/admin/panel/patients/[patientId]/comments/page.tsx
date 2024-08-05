e3b19f704e262c27d8c7342f28d95ac979bf89d2
MohammadMosavat <42.octmst@gmail.com>
2024-08-05 18:49:46 +0330

    Add pagination into the comment section and page in admin planel

--- client/src/app/admin/panel/patients/[patientId]/comments/page.tsx
+++ client/src/app/admin/panel/patients/[patientId]/comments/page.tsx
@@ -1,46 +1,24 @@
 "use client";
 import CommentBox from "@/components/AdminComponents/CommentBox";
 import MediterraneanForm from "@/components/AdminComponents/MediterraneanForm";
+import Pagination from "@/components/Pagination";
 import { chunkingArray } from "@/hooks/chunkingArray";
 import useFetchOneMediterranean from "@/hooks/useFetchOneMediterranean";
 import useFetchPatientComments from "@/hooks/useFetchPatientComments/useFetchPatientComments";
 import { CommentType } from "@/types";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const CommentsPatientPage = ({ params }: { params: { patientId: string } }) => {
   const commentData = useFetchPatientComments(params.patientId).toReversed();
   const [arrayItemsComment, setArrayItemsComment] = useState<number>(0);
-  const newArrayComment = chunkingArray(commentData, 4);
-
-  const paginationComment = useCallback(() => {
-    return newArrayComment.map((item:any, index:number) => {
-      return (
-        item.length > 0 &&
-        newArrayComment[1]?.length > 0 && (
-          <button
-            type="button"
-            autoFocus={index == 0 ? true : false}
-            onClick={(e) => {
-              console.log(e.target);
-              setArrayItemsComment(index);
-            }}
-            className={`p-4 hover:underline bg-[var(--milky-white)] hover:bg-[var(--new-green)] border-none outline-none hover:text-[var(--milky-white)] ${
-              arrayItemsComment + 1 == index + 1 &&
-              "bg-[var(--new-green)] text-[var(--milky-white)] underline"
-            } `}
-          >
-            {index + 1}
-          </button>
-        )
-      );
-    });
-  }, [newArrayComment]);
+  const [calcPlues, setCalcPlues] = useState<number>(0);
+  const newArrayComment = chunkingArray(commentData, 6);
 
   return (
     <div className="flex flex-col mb-6 gap-6">
       <h2 className="text-xl md:text-2xl lg:text-3xl">پیام ها:</h2>
       {newArrayComment?.length > 0 ? (
-        newArrayComment[arrayItemsComment].map((comment:CommentType) => (
+        newArrayComment[arrayItemsComment].map((comment: CommentType) => (
           <CommentBox
             key={comment._id}
             sender={comment.sender}
@@ -55,12 +33,15 @@ const CommentsPatientPage = ({ params }: { params: { patientId: string } }) => {
       ) : (
         <p className="text-xl text-center">پیام ندارد</p>
       )}
-      <div
-        role="group"
-        className="flex items-center w-fit custom-scroll-x justify-center mx-auto overflow-hidden rounded-lg"
-      >
-        {paginationComment()}
-      </div>
+      <Pagination
+        arr={newArrayComment}
+        itemNumber={arrayItemsComment}
+        setCalc={setCalcPlues}
+        calc={calcPlues}
+        lastCalc={4}
+        setItemNumber={setArrayItemsComment}
+        limitNumber={30}
+      />
     </div>
   );
 };
