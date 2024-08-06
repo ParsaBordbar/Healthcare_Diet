535953709f8b04e7ab8a1053fd4bb29a43d0fc5a
MohammadMosavat <42.octmst@gmail.com>
2024-08-06 23:55:01 +0330

    update the bmi section in medi form use the question and answer component

--- client/src/components/AdminComponents/DietBmi/index.tsx
+++ client/src/components/AdminComponents/DietBmi/index.tsx
@@ -1,27 +1,61 @@
-
-import { BmiType } from '@/types'
-import React from 'react'
+import QuestionAndAnswerMediForm from "@/components/QAMediForm";
+import { BmiType } from "@/types";
+import React from "react";
 
 function DietBmi({
-    age,
-    height,
-    hipcircumference,
-    weight,
-    bmi,
-    abdominalCircumference,
-    autoIncrementId,
-  }: BmiType) {
+  age,
+  height,
+  hipcircumference,
+  weight,
+  bmi,
+  abdominalCircumference,
+  autoIncrementId,
+}: BmiType) {
   return (
-        <div className="grid grid-cols-12 pr-6 gap-4">
-            <p className="min-[1000px]:col-span-full justify-center col-span-full text-[var(--black-blue)] w-fit ">
-            BMI: {bmi}
-            </p>
-            <p className='min-[1000px]:col-span-2 text-sm sm:text-base md:col-span-6 col-span-full'>سن: {age}</p>
-            <p className='min-[1000px]:col-span-2 text-sm sm:text-base md:col-span-6 col-span-full'>قد: {height}</p>
-            <p className='min-[1000px]:col-span-2 text-sm sm:text-base md:col-span-6 col-span-full'>وزن: {weight}</p>
-            <p className='min-[1000px]:col-span-2 text-sm sm:text-base md:col-span-6 col-span-full'>دور شکم: {abdominalCircumference}</p>
-            <p className='min-[1000px]:col-span-2 text-sm sm:text-base md:col-span-6 col-span-full'>دور  باسن: {hipcircumference}</p>
-        </div>
-  )
+    <div className="grid grid-cols-12 pr-6 gap-4">
+      <QuestionAndAnswerMediForm
+        question={":BMI "}
+        className={
+          "min-[1000px]:col-span-full justify-center col-span-full gap-1 flex flex-row-reverse text-[var(--black-blue)] w-fit "
+        }
+        answer={bmi}
+      />
+      <QuestionAndAnswerMediForm
+        question={"سن:"}
+        className={
+          "min-[1000px]:col-span-2 md:col-span-6 justify-center [&>p]:!text-black text-sm sm:text-base  col-span-full gap-1 flex  w-fit "
+        }
+        answer={age}
+      />
+      <QuestionAndAnswerMediForm
+        question={"قد:"}
+        className={
+          "min-[1000px]:col-span-2 md:col-span-6 justify-center [&>p]:!text-black text-sm sm:text-base  col-span-full gap-1 flex w-fit "
+        }
+        answer={height}
+      />
+      <QuestionAndAnswerMediForm
+        question={"وزن:"}
+        className={
+          "min-[1000px]:col-span-2 md:col-span-6 justify-center [&>p]:!text-black text-sm sm:text-base  col-span-full gap-1 flex w-fit "
+        }
+        answer={weight}
+      />
+      <QuestionAndAnswerMediForm
+        question={"دور شکم:"}
+        className={
+          "min-[1000px]:col-span-2 md:col-span-6 justify-center [&>p]:!text-black text-sm sm:text-base  col-span-full gap-1 flex w-fit "
+        }
+        answer={abdominalCircumference}
+      />
+      <QuestionAndAnswerMediForm
+        question={"دور باسن:"}
+        className={
+          "min-[1000px]:col-span-2 md:col-span-6 justify-center [&>p]:!text-black text-sm sm:text-base  col-span-full gap-1 flex w-fit "
+        }
+        answer={hipcircumference}
+      />
+    </div>
+  );
 }
-export default DietBmi
+export default DietBmi;
