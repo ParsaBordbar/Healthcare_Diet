f21b09afae7416844053caca7d81e0fb60d48bc8
ParsaBordbar <parsab71@gmail.com>
2024-08-08 17:17:15 +0330

    Update Layout for Doctors card.

--- client/src/components/DrCardDetails/index.tsx
+++ client/src/components/DrCardDetails/index.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 
 function DoctorsCardDetails() {
   return (
-    <div className='bg-[var(--black-blue)] text-white font-extrabold text-3xl flex justify-center flex-col  gap-4 p-10 rounded-xl'>
-        <div className='grid grid-cols-8 gap-1 justify-center items-center mt-20'>
-            <span className='text-6xl col-start-3 col-end-4'>5041</span>
-            <span className='text-6xl col-start-4 col-end-5'>7212</span>
-            <span className='text-6xl col-start-5 col-end-6'>2124</span>
-            <span className='text-6xl col-start-6 col-end-7'>2597</span>
+    <div className='bg-[var(--black-blue)] text-white font-extrabold text-3xl flex justify-center flex-col  gap-4 p-8 rounded-xl items-center cursor-pointer'>
+        <div className='grid grid-cols-6 gap-12 justify-center items-center mt-20'>
+            <div className='flex justify-end hover:scale-110'><button className='text-sm border rounded-full p-1 w-1/3 h-1/2'>کپی</button></div>
+            <span className='text-6xl col-span-1 col-start-2'>5041</span>
+            <span className='text-6xl col-span-1 col-start-3'>7212</span>
+            <span className='text-6xl col-span-1 col-start-4'>2124</span>
+            <span className='text-6xl col-span-1 col-start-5'>2597</span>
         </div>
-        <div className='grid grid-cols-4'>
+        <div className='grid grid-cols-5'>
         <h2 className='text-2xl col-start-2'>
             به نام محمدرضا ربیعی    
         </h2>
