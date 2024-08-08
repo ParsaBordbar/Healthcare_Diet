4180a920711c775bf1d6e7cadb87daed6da4b795
ParsaBordbar <parsab71@gmail.com>
2024-08-08 21:04:46 +0330

    Update DrCardDetails styles.

--- client/src/components/DrCardDetails/index.tsx
+++ client/src/components/DrCardDetails/index.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 
 function DoctorsCardDetails() {
   return (
-    <div className='bg-[var(--black-blue)] text-white font-extrabold text-3xl flex justify-center flex-col  gap-4 p-8 rounded-xl items-center cursor-pointer'>
-        <div className='grid grid-cols-4 gap-12 justify-center items-center md:mt-20'>
-           <button className='sm:text-sm text-xs border rounded-full p-2' onClick={() => CopyToClipboard("5041721221242507")}>کپی</button>
+    <div className='bg-[var(--black-blue)] text-white font-extrabold text-3xl flex justify-center flex-col  gap-4 p-8 rounded-2xl items-center cursor-pointer shadow-lg'>
+        <div className='grid grid-cols-4 gap-12 justify-center items-center md:pt-20'>
+           <button className='sm:text-sm text-xs border rounded-2xl p-2' onClick={() => CopyToClipboard("5041721221242507")}>کپی</button>
             <span className='xl:text-4xl md:text-3xl  text-xl col-span-1 col-start-2'>2507</span>
             <span className='xl:text-4xl md:text-3xl  text-xl col-span-1 col-start-3'>2124</span>
             <span className='xl:text-4xl md:text-3xl  text-xl col-span-1 col-start-4'>7212</span>
             <span className='xl:text-4xl md:text-3xl  text-xl col-span-1 col-start-5'>5041</span>
         </div>
-        <div className='grid grid-cols-5'>
-        <h2 className='xl:text-2xl text-lg lg:col-start-2 col-span-full'>
+        <div className='grid grid-cols-4 ps-10 pt-3'>
+        <h2 className='xl:text-2xl text-lg lg:col-span-1 col-span-full'>
              محمدرضا ربیعی    
         </h2>
         </div>
--- client/src/components/Dr_Rabiee_Landing_components/Cards.tsx
+++ client/src/components/Dr_Rabiee_Landing_components/Cards.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CustomCard from "./CustomCard";
+import DoctorsCardDetails from "../DrCardDetails";
 
 const cardsData = [
   {
