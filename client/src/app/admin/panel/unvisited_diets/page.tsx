23f63fd78bb2aad773b72a739937aba57b075916
MohammadMosavat <42.octmst@gmail.com>
2024-08-08 10:45:40 +0330

    add missed props for medi component in unvisited page and add new pagination

--- client/src/app/admin/panel/unvisited_diets/page.tsx
+++ client/src/app/admin/panel/unvisited_diets/page.tsx
@@ -6,13 +6,15 @@ import DocumentIcon from "/public/svg/adminPanelSvgs/document.svg";
 import { chunkingArray } from "@/hooks/chunkingArray";
 import { MediterraneanFormType } from "@/types";
 import Pagination from "@/components/Pagination";
+import ResponsivePagination from "react-responsive-pagination";
+import "react-responsive-pagination/themes/classic.css";
 
 function UnvisitedDiets() {
   const mediterraneanForms = useFetchMediterranean("/sort?sort=unVisited");
   const newArray = chunkingArray(mediterraneanForms, 2);
-  const [calcPlues, setCalcPlues] = useState<number>(0);
-  const [arrayItemsComment, setArrayItemsComment] = useState<number>(0);
   console.log("medi array", mediterraneanForms);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = newArray.length;
 
   return (
     <>
@@ -24,66 +26,65 @@ function UnvisitedDiets() {
           </h1>
         </section>
         {newArray.length > 0 &&
-          newArray[arrayItemsComment].map(
-            (form: MediterraneanFormType) => {
-              return (
-                <MediterraneanForm
-                  dailyFruit={form.dailyFruit}
-                  dailyVegetable={form.dailyVegetable}
-                  Cereals={form.Cereals}
-                  dailyCereals={form.dailyCereals}
-                  potatoAndStarchWeekly={form.potatoAndStarchWeekly}
-                  oliveAndOliveOilDaily={form.oliveAndOliveOilDaily}
-                  nutsDaily={form.nutsDaily}
-                  dairyDaily={form.dairyDaily}
-                  beans={form.beans}
-                  eggWeekly={form.eggWeekly}
-                  fishWeekly={form.fishWeekly}
-                  chickensWeekly={form.chickensWeekly}
-                  sugarWeekly={form.sugarWeekly}
-                  alcoholWeekly={form.alcoholWeekly}
-                  fermentationWeekly={form.fermentationWeekly}
-                  supplements={form.sugarWeekly}
-                  physicalActivity={form.physicalActivity}
-                  diabetes={form.diabetes}
-                  anemia={form.anemia}
-                  bloodPressure={form.bloodPressure}
-                  digestiveProblems={form.digestiveProblems}
-                  selfSafety={form.selfSafety}
-                  stroke={form.stroke}
-                  fattyLiver={form.fattyLiver}
-                  kidneyProblems={form.kidneyProblems}
-                  thyroid={form.thyroid}
-                  cancer={form.cancer}
-                  Migraine={form.Migraine}
-                  otherSickness={form.otherSickness}
-                  medicine={form.medicine}
-                  phoneNumber={form.phoneNumber}
-                  key={form.phoneNumber}
-                  isChecked={form.isChecked}
-                  files={form.files}
-                  redMeatWeekly={form.redMeatWeekly}
-                  createdAtJalali={form.createdAtJalali}
-                  payment={form.payment}
-                  dietBmi={form.dietBmi}
-                  foodAllergies={form.foodAllergies}
-                  pregnancyWeeks={form.pregnancyWeeks}
-                  breastfeeding={form.breastfeeding}
-                  _id={form._id}
-                  weightBeforePregnancy={form.weightBeforePregnancy}
-                />
-              );
-            }
-          )}
+          newArray[currentPage - 1].map((form: MediterraneanFormType) => {
+            return (
+              <MediterraneanForm
+                dailyFruit={form.dailyFruit}
+                dailyVegetable={form.dailyVegetable}
+                Cereals={form.Cereals}
+                dailyCereals={form.dailyCereals}
+                potatoAndStarchWeekly={form.potatoAndStarchWeekly}
+                oliveAndOliveOilDaily={form.oliveAndOliveOilDaily}
+                nutsDaily={form.nutsDaily}
+                dairyDaily={form.dairyDaily}
+                beans={form.beans}
+                eggWeekly={form.eggWeekly}
+                fishWeekly={form.fishWeekly}
+                chickensWeekly={form.chickensWeekly}
+                sugarWeekly={form.sugarWeekly}
+                alcoholWeekly={form.alcoholWeekly}
+                fermentationWeekly={form.fermentationWeekly}
+                supplements={form.supplements}
+                physicalActivity={form.physicalActivity}
+                diabetes={form.diabetes}
+                anemia={form.anemia}
+                bloodPressure={form.bloodPressure}
+                digestiveProblems={form.digestiveProblems}
+                selfSafety={form.selfSafety}
+                stroke={form.stroke}
+                fattyLiver={form.fattyLiver}
+                kidneyProblems={form.kidneyProblems}
+                thyroid={form.thyroid}
+                cancer={form.cancer}
+                Migraine={form.Migraine}
+                otherSickness={form.otherSickness}
+                medicine={form.medicine}
+                phoneNumber={form.phoneNumber}
+                key={form.phoneNumber}
+                isChecked={form.isChecked}
+                files={form.files}
+                redMeatWeekly={form.redMeatWeekly}
+                createdAtJalali={form.createdAtJalali}
+                payment={form.payment}
+                dietBmi={form.dietBmi}
+                foodAllergies={form.foodAllergies}
+                pregnancyWeeks={form.pregnancyWeeks}
+                breastfeeding={form.breastfeeding}
+                _id={form._id}
+                weightBeforePregnancy={form.weightBeforePregnancy}
+                appetite={form.appetite}
+                physicalActivityHours={form.physicalActivityHours}
+                sport={form.sport}
+                preferredDrink={form.preferredDrink}
+              />
+            );
+          })}
       </main>
-      <Pagination
-        arr={newArray}
-        itemNumber={arrayItemsComment}
-        setItemNumber={setArrayItemsComment}
-        setCalc={setCalcPlues}
-        calc={calcPlues}
-        limitNumber={20}
-        lastCalc={3}
+      <ResponsivePagination
+        linkHref="omit"
+        current={currentPage}
+        total={totalPages}
+        onPageChange={setCurrentPage}
       />
     </>
   );
