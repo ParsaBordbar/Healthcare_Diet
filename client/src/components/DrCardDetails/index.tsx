5355f027e60054e97070fca124d0f78aaaa9a386
ParsaBordbar <parsab71@gmail.com>
2024-08-08 21:09:26 +0330

    Fix CopyButton Style.

--- client/src/components/DrCardDetails/index.tsx
+++ client/src/components/DrCardDetails/index.tsx
@@ -5,7 +5,7 @@ function DoctorsCardDetails() {
   return (
     <div className='bg-[var(--black-blue)] text-white font-extrabold text-3xl flex justify-center flex-col  gap-4 p-8 rounded-2xl items-center cursor-pointer shadow-lg'>
         <div className='grid grid-cols-4 gap-12 justify-center items-center md:pt-20'>
-           <button className='sm:text-sm text-xs border rounded-2xl p-2' onClick={() => CopyToClipboard("5041721221242507")}>کپی</button>
+           <button className='sm:text-sm text-xs px-4 py-2 border rounded-full flex justify-center items-center' onClick={() => CopyToClipboard("5041721221242507")}>کپی</button>
             <span className='xl:text-4xl md:text-3xl  text-xl col-span-1 col-start-2'>2507</span>
             <span className='xl:text-4xl md:text-3xl  text-xl col-span-1 col-start-3'>2124</span>
             <span className='xl:text-4xl md:text-3xl  text-xl col-span-1 col-start-4'>7212</span>
